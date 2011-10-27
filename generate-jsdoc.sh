0eaa4eedf52b9f63048fab25aa56dfe6a407c5f6
Matthijs Laan <matthijsln@gmail.com>
2011-10-27 14:37:40 +0000

    geen volledige dirname in output

--- generate-jsdoc.sh
+++ generate-jsdoc.sh
@@ -4,6 +4,6 @@
 # Script om met jsdoc-toolkit documentatie te genereren. 
 # Zie http://code.google.com/p/jsdoc-toolkit/
 #
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-/opt/jsdoc-toolkit/jsrun.sh -a -r -t=/opt/jsdoc-toolkit/templates/jsdoc -d=$DIR/jsdoc -v -E="jquery" -E="swfobject" -p -s $DIR
+cd $( dirname "$0" )
+/opt/jsdoc-toolkit/jsrun.sh -a -r -t=/opt/jsdoc-toolkit/templates/jsdoc -d=jsdoc -v -E="jquery" -E="swfobject" -p -s .
 
