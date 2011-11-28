60a91f027334371468fd1900a444bb84bb542b09
Matthijs Laan <matthijsln@gmail.com>
2011-11-28 18:18:28 +0000

    eerste opzet leeg web project voor viewer

---
+++ src/java/nl/b3p/viewer/admin/stripes/GeoServiceRegistryActionBean.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2011 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.viewer.admin.stripes;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import net.sourceforge.stripes.action.*;
+import net.sourceforge.stripes.controller.LifecycleStage;
+import nl.b3p.viewer.config.services.GeoService;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.stripesstuff.stripersist.Stripersist;
+
+/**
+ *
+ * @author Matthijs Laan
+ */
+@UrlBinding("/action/geoserviceregistry/{$event}")
+public class GeoServiceRegistryActionBean implements ActionBean {
+
+    private ActionBeanContext context;
+
+    private String registryTreeJson;
+
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+
+    public ActionBeanContext getContext() {
+        return context;
+    }
+
+    public String getRegistryTreeJson() {
+        return registryTreeJson;
+    }
+
+    public void setRegistryTreeJson(String registryTreeJson) {
+        this.registryTreeJson = registryTreeJson;
+    }
+
+    @After(stages=LifecycleStage.EventHandling)
+    public void loadRegistryTree() throws JSONException {
+        EntityManager em = Stripersist.getEntityManager();
+
+        List<GeoService> services = em.createQuery("from GeoService order by name").getResultList();
+
+        JSONArray a = new JSONArray();
+        for(GeoService s: services) {
+            JSONObject j = new JSONObject();
+            j.put("id", s.getId());
+            j.put("name", s.getName());
+            a.put(j);
+        }
+        registryTreeJson = a.toString(4);
+    }
+
+    public Resolution view() throws JSONException {
+
+
+        return new ForwardResolution("/WEB-INF/jsp/geoserviceregistry.jsp");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,32 @@
+<%--
+Copyright (C) 2011 B3Partners B.V.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@include file="/WEB-INF/jsp/taglibs.jsp"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+
+        <p><stripes:link beanclass="nl.b3p.viewer.admin.stripes.GeoServiceRegistryActionBean">Gegevensregister</stripes:link></p>
+    </body>
+</html>
