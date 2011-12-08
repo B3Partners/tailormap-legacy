ef8caebba914cb992613facecfaec21dcc969130
Matthijs Laan <matthijsln@gmail.com>
2011-12-08 09:02:20 +0000

    toegevoegd

---
+++ src/nl/b3p/viewer/config/services/FeatureSource.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2011 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.viewer.config.services;
+
+import java.util.*;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Matthijs Laan
+ */
+@Entity
+@DiscriminatorColumn(name="protocol")
+public abstract class FeatureSource {
+    @Id
+    private Long id;
+
+    @Column(unique=true)
+    @Basic(optional=false)
+    private String name;
+
+    @Basic(optional=false)
+    private String url;
+
+    private String username;
+    private String password;
+
+    /**
+     * Extra information to configure the feature source, for example WFS
+     * FeatureType name, ArcXML layer, username, password etc.
+     *
+     * Stored in a JSON format specific for the concrete subclass of GeoService.
+     */
+    @Lob
+    private String featureSourceConfig;
+    
+    @OneToMany(orphanRemoval=true, mappedBy="featureSource")
+    @OrderColumn
+    private List<SimpleFeatureType> featureTypes = new ArrayList<SimpleFeatureType>();
+
+    //<editor-fold defaultstate="collapsed" desc="getters en setters">
+    public String getFeatureSourceConfig() {
+        return featureSourceConfig;
+    }
+
+    public void setFeatureSourceConfig(String featureSourceConfig) {
+        this.featureSourceConfig = featureSourceConfig;
+    }
+
+    public List<SimpleFeatureType> getFeatureTypes() {
+        return featureTypes;
+    }
+
+    public void setFeatureTypes(List<SimpleFeatureType> featureTypes) {
+        this.featureTypes = featureTypes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    //</editor-fold>
+
+}
