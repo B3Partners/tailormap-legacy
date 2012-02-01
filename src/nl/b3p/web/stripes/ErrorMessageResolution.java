304aa364de8a498e531fff6d704469123c4f7704
Matthijs Laan <matthijsln@gmail.com>
2012-02-01 10:07:35 +0000

    added ErrorMessageResolution for Ajax handlers

---
+++ src/nl/b3p/web/stripes/ErrorMessageResolution.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2012 Expression organization is undefined on line 4, column 61 in Templates/Licenses/license-gpl30.txt.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.web.stripes;
+
+import javax.servlet.http.HttpServletResponse;
+import net.sourceforge.stripes.action.StreamingResolution;
+import org.apache.commons.httpclient.HttpStatus;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author matthijsln
+ */
+public class ErrorMessageResolution extends StreamingResolution {
+
+    public ErrorMessageResolution(String message) {
+        super("text/plain", message);
+        setCharacterEncoding("UTF-8");
+    }
+
+    public ErrorMessageResolution(JSONObject json) {
+        super("application/json", json.toString());
+        setCharacterEncoding("UTF-8");
+    }
+
+    @Override
+    protected void stream(HttpServletResponse response) throws Exception {
+        response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+        super.stream(response);
+    }
+}
