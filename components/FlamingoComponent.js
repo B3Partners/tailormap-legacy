d7613c183a009d46f7fb74eb66d4786dff8c15b6
Roy Braam <braam.roy@gmail.com>
2012-02-09 08:29:13 +0000

    Added custom flamingo component.

---
+++ components/FlamingoComponent.js
@@ -0,0 +1,35 @@
+/* 
+ * Copyright (C) 2012 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * FlamingoComponent
+ * Creates a Custom Flamingo component with the given xml.
+ * With this all custom Flamingo-mc objects can be added.
+ * @author <a href="mailto:roybraam@b3partners.nl">Roy Braam</a>
+ */
+Ext.define ("viewer.components.FlamingoComponent",{
+    extend: "viewer.components.Component",
+    config:{
+        name: "FlamingoComponent",
+        xml : ""
+    },
+    constructor: function (conf){        
+        viewer.components.tools.Legend.superclass.constructor.call(this, conf);
+        this.initConfig(conf);
+        return this;
+    }
+});
+
