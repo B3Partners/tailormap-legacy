cfcb9d5808932f2ff721873c06d363cdf568f3e3
Meine Toonen <toonen.meine@gmail.com>
2012-02-09 08:01:39 +0000

    After saving the component config, return the correct resolution to set all the necessary variables Use shortname in left menu TOC has own config (WIP) Bookmark added Related docs calls correct superclass

---
+++ components/TOC-config.js
@@ -0,0 +1,52 @@
+/* 
+ * Copyright (C) 2012 Expression organization is undefined on line 4, column 61 in Templates/Licenses/license-gpl30.txt.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// bouw van componentspecifieke configuratievelden
+var form;
+function ConfigSource(parentId, config){
+    if(config == undefined || config == null){
+        config = new Object();
+    }
+    form = new Ext.form.FormPanel({ //(1)
+        renderTo: parentId, //(2)
+        url: 'Home/SubmitForm',
+        frame: false,
+        title: 'Configureer dit component',
+        width: 300,
+        items: [{ //(3)
+            xtype: 'textfield',
+            fieldLabel: 'Naam',
+            name: 'naam',
+            value: config.naam
+        }, {
+            xtype: 'numberfield',
+            fieldLabel: 'Nummer',
+            name: 'nummer',
+            value: config.nummer
+        }, {
+            xtype: 'checkbox',
+            fieldLabel: 'Aan',
+            inputValue: true,
+            name: 'aan',
+            value: config.aan
+        }]
+    });      
+}
+
+function getConfigObject() {
+    return form.getValues();
+}
