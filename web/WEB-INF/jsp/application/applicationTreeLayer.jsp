0b23360b8edf7da2196e5b88af40be50b4161b55
Geert Plaisier <geert.plaisier@gmail.nl>
2012-02-13 17:39:34 +0000

    Created the Edit and Filtering/Selection tabs for editing an application layer

--- web/WEB-INF/jsp/application/applicationTreeLayer.jsp
+++ web/WEB-INF/jsp/application/applicationTreeLayer.jsp
@@ -28,9 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         <stripes:errors/>
         <stripes:messages/>
         <p>
-        <stripes:form beanclass="nl.b3p.viewer.admin.stripes.ApplicationTreeLayerActionBean">
+        <stripes:form beanclass="nl.b3p.viewer.admin.stripes.ApplicationTreeLayerActionBean" id="apptreelayerform">
             <stripes:hidden name="applicationLayer" value="${actionBean.applicationLayer.id}"/>
-            <stripes:hidden name="attributesJSON" value="${actionBean.attributesJSON}"/>
+            <stripes:hidden name="attributesJSON" id="attributesJSON" value="${actionBean.attributesJSON}"/>
             <c:if test="${actionBean.context.eventName == 'edit'}">
                 <stripes:submit name="save" value="Opslaan" />
                 <stripes:submit name="cancel" value="Annuleren"/>
@@ -99,51 +99,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                         </table>
                     </div>
                     <div id="edit-tab" class="x-hide-display">
-                        <script type="text/javascript">
-                            var items = [];
-                        </script>
-                        <div id="accordionDiv"></div>
-                        <c:choose>
-                            <c:when test="${not empty actionBean.attributesList && editable}">
-                                <c:forEach var="attribute" items="${actionBean.attributesList}">
-                                    <div id="attribuut${attribute.id}" class="accordiondiv">
-                                        ${attribute.name}<br>
-                                    </div>
-                                <script type="text/javascript">
-                                    var attributes = '${actionBean.attributesJSON}';
-
-                                    items.push({
-                                        contentEl:'attribuut${attribute.id}',
-                                        title: '${attribute.name}'
-                                    });
-                                </script>
-                                </c:forEach>
-                            </c:when>
-                            <c:otherwise>
-                                <c:choose>
-                                    <c:when test="${!editable}">
-                                        De attribuutbron van deze kaartlaag is niet van het type JDBC en is daarom niet editbaar.
-                                    </c:when>
-                                    <c:otherwise>
-                                        Er zijn geen attributen voor deze kaartlaag geconfigureerd.
-                                    </c:otherwise>
-                                </c:choose>
-                            </c:otherwise>
-                        </c:choose>
+                        Er zijn geen attributen voor deze kaartlaag geconfigureerd.
                     </div>
                     <div id="filter-tab" class="x-hide-display">
-                        <c:choose>
-                            <c:when test="${not empty actionBean.attributesList}">
-
-                            </c:when>
-                            <c:otherwise>
-                                Er zijn geen attributen voor deze kaartlaag geconfigureerd.
-                            </c:otherwise>
-                        </c:choose>
+                        Er zijn geen attributen voor deze kaartlaag geconfigureerd.
                     </div>
                 </div>
+                <script type="text/javascript">
+                    var attributes = ${actionBean.attributesJSON};
+                    var getDBValuesUrl = '';
+                </script>
+                <script type="text/javascript" src="${contextPath}/resources/js/application/applicationTreeLayer.js"></script>
             </c:if>
         </stripes:form>
-        <script type="text/javascript" src="${contextPath}/resources/js/application/applicationTreeLayer.js"></script>
     </stripes:layout-component>
 </stripes:layout-render>
--- web/resources/js/application/applicationTreeLayer.js
+++ web/resources/js/application/applicationTreeLayer.js
@@ -24,6 +24,89 @@ Ext.require([
 ]);
 
 Ext.onReady(function() {
+   
+    var collapsed = false;
+    var editPanelItems = [];
+    var filterPanelItems = [];
+    var defaults = {
+        width: '100%',
+        animCollapse: false,
+        collapsible: true,
+        titleCollapse: true,
+        hideCollapseTool: true,
+        fieldDefaults: {
+            labelWidth: 150,
+            size: 40
+        },
+        listeners: {
+            beforeexpand: function(panel){
+                Ext.Array.each(panel.findParentByType('container').items.items, function(item) {
+                    item.collapse();
+                });
+            }
+        }
+    };
+    var editAllowed = false;
+    var filterAllowed = false;
+    if(Ext.isArray(attributes) && attributes.length > 0) {
+        editAllowed = true;
+        filterAllowed = true;
+        Ext.Array.each(attributes, function(attribute) {
+            var name = attribute.alias || attribute.name;
+            editPanelItems.push(Ext.create('Ext.form.Panel', Ext.apply(defaults, {
+                id: 'edit' + attribute.id,
+                title: name + (attribute.editable ? ' (&times;)' : ''),
+                collapsed: collapsed,
+                items: [
+                    { fieldLabel: 'Bewerkbaar', name: 'editable', inputValue: 1, checked: attribute.editable, xtype: 'checkbox', listeners: {
+                        change: function(field, newval) {
+                            editPanelTitle(field.findParentByType('form'), name, newval)
+                        }
+                    }},
+                    { fieldLabel: 'Alias', name: 'editalias', value: attribute.editalias, xtype: 'textfield' },
+                    {
+                        xtype: 'container',
+                        layout: 'hbox',
+                        items: [
+                            { fieldLabel: 'Mogelijke waarden', name: 'editvalues', id: 'editvalues' + attribute.id, value: attribute.editvalues, xtype: 'textfield', size: 100 },
+                            { xtype: 'button', text: 'DB', style: { marginLeft: '10px' }, listeners: {
+                                click: function() {
+                                    getDBValues(attribute.id);
+                                }
+                            }},
+                        ]
+                    },
+                    { fieldLabel: 'Hoogte', name: 'editheight', value: attribute.editheight, xtype: 'textfield' }
+                ]
+            })));
+            var isEnabled = (attribute.filterable || attribute.selectable) || false;
+            filterPanelItems.push(Ext.create('Ext.form.Panel', Ext.apply(defaults, {
+                id: 'filter' + attribute.id,
+                title: name,
+                collapsed: collapsed,
+                items: [
+                    { fieldLabel: 'Filterbaar / Selecteerbaar', name: 'filterable_selectable', inputValue: 1, checked: isEnabled, xtype: 'checkbox',  labelWidth: 150, listeners: {
+                        change: function(field, newval) {
+                            var panel = field.findParentByType('form');
+                            Ext.getCmp('filterable' + attribute.id).setDisabled(!newval);
+                            Ext.getCmp('selectable' + attribute.id).setDisabled(!newval);
+                            editPanelTitle(panel, name, newval);
+                        }
+                    }},
+                    {
+                        xtype: 'container',
+                        layout: 'hbox',
+                        items: [
+                            { xtype: 'displayfield', fieldLabel: 'Attribuut gebruiken bij', labelWidth: 'auto' },
+                            { id: 'filterable' + attribute.id, fieldLabel: 'Filteren', name: 'filterable', inputValue: 'filter', checked: attribute.filterable, disabled: !isEnabled, xtype: 'radio', labelWidth: 'auto', labelAlign: 'right' },
+                            { id: 'selectable' + attribute.id, fieldLabel: ' &nbsp;Dataselectie', name: 'filterable', inputValue: 'select', checked: attribute.selectable, disabled: !isEnabled, xtype: 'radio', labelWidth: 'auto', labelAlign: 'right' }
+                        ]
+                    }
+                ]
+            })));
+            collapsed = true;
+        });
+    }
     var tabconfig = [{
         contentEl:'rights-tab', 
         title: 'Rechten'
@@ -33,13 +116,41 @@ Ext.onReady(function() {
     },{
         contentEl:'settings-tab', 
         title: 'Instellingen'
-    },{
-        contentEl:'edit-tab', 
-        title: 'Edit'
-    },{
-        contentEl:'filter-tab', 
-        title: 'Selectie/Filter'
     }];
+    if(editAllowed) {
+        tabconfig.push({
+            xtype: 'container',
+            width: '100%',
+            title: 'Edit',
+            padding: 10,
+            height: 550,
+            layout: 'auto',
+            autoScroll: true,
+            items: editPanelItems
+        });
+    } else {
+        tabconfig.push({
+            contentEl:'edit-tab', 
+            title: 'Edit'
+        });
+    }
+    if(filterAllowed) {
+        tabconfig.push({
+            xtype: 'container',
+            width: '100%',
+            title: 'Filter / Selectie',
+            padding: 10,
+            height: 550,
+            layout: 'auto',
+            autoScroll: true,
+            items: filterPanelItems
+        });
+    } else {
+        tabconfig.push({
+            contentEl:'filter-tab', 
+            title: 'Filter / Selectie'
+        });
+    }
 
     Ext.createWidget('tabpanel', {
         renderTo: 'tabs',
@@ -48,21 +159,48 @@ Ext.onReady(function() {
         defaults :{
             bodyPadding: 10
         },
-        layoutOnTabChange: true,
+        layoutOnTabChange: false,
         items: tabconfig
     });
     
-    /*Ext.select('.accordiondiv', true).removeCls('accordiondiv').addCls('x-panel-default-collapsed');
-    Ext.create('Ext.panel.Panel', {
-        width: '100%',
-        height: '100%',
-        layout:'accordion',
-        layoutConfig: {
-            titleCollapse: false,
-            animate: true,
-            activeOnTop: true
-        },
-        items: items,
-        renderTo: 'accordionDiv'
-    });*/
+    Ext.get('apptreelayerform').on('submit', function(e) {
+        Ext.get('attributesJSON').dom.value = getJson();
+    });
+    
 });
+
+function editPanelTitle(panel, name, checked) {
+    panel.setTitle(name + (checked ? ' (&times;)' : ''));
+}
+
+function getJson() {
+    var currentAttributes = [];
+    Ext.Array.each(attributes, function(attribute) {
+        var newAttribute = attribute;
+        Ext.getCmp('edit' + attribute.id).getForm().getFields().each(function(field) {
+            newAttribute[field.getName()] = field.getValue();
+        });
+        newAttribute.filterable = Ext.getCmp('filterable' + attribute.id).getValue();
+        newAttribute.selectable = Ext.getCmp('selectable' + attribute.id).getValue();
+        currentAttributes.push(newAttribute);
+    });
+    return JSON.stringify(currentAttributes);
+}
+
+function getDBValues(attributeid) {
+    if(getDBValuesUrl != '') {
+        Ext.Ajax.request({ 
+            url: getDBValuesUrl, 
+            params: { 
+                attributeid: attributeid
+            }, 
+            success: function ( result, request ) { 
+                var result = JSON.parse(result.responseText);
+                Ext.getCmp('editvalues' + attributeid).setValue(result.join(','));
+            }, 
+            failure: function ( result, request) { 
+                Ext.MessageBox.alert('Foutmelding', result.responseText); 
+            } 
+        });
+    }
+}
