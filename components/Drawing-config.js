8bc38e21bbcba4266779aa38f55c01ecb9625ea2
Meine Toonen <toonen.meine@gmail.com>
2012-02-13 08:47:50 +0000

    Colorfield added

---
+++ components/Drawing-config.js
@@ -0,0 +1,80 @@
+/* 
+ * Copyright (C) 2012 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Custom configuration object for HTML configuration
+ * @author <a href="mailto:roybraam@b3partners.nl">Roy Braam</a>
+ */
+Ext.define("viewer.components.CustomConfiguration",{
+    extend: "viewer.components.ConfigObject",
+    form: null,
+    constructor: function (parentId,configObject){
+        viewer.components.CustomConfiguration.superclass.constructor.call(this, parentId,configObject);        
+        //createForm();                
+        this.createForm(this.configObject);
+
+    },
+    createForm: function(config){
+        //to make this accessible in object
+        var colorPicker = Ext.create('Ext.menu.ColorPicker', {
+    value: '000000'
+});
+        var me=this;
+        this.form=new Ext.form.FormPanel({
+            frame: false,
+            bodyPadding: me.formPadding,
+            width: me.formWidth,
+            defaults: {
+                anchor: '100%'
+            },
+            items: [{ 
+                xtype: 'textfield',
+                fieldLabel: 'Titel',
+                name: 'title',
+                value: config.title,
+                labelWidth:me.labelWidth
+            },{ 
+                xtype: 'textfield',
+                fieldLabel: 'Titelbalk icoon',
+                name: 'iconUrl',
+                value: config.iconUrl,
+                labelWidth:me.labelWidth
+            },{ 
+                xtype: 'textfield',
+                fieldLabel: 'Tooltip',
+                name: 'toolTip',
+                value: config.toolTip,
+                labelWidth:me.labelWidth
+            },{ 
+                xtype: 'colorfield',
+                fieldLabel: 'Kleur',
+                name: 'color',
+                value: config.color,
+                labelWidth:me.labelWidth
+            }],
+            renderTo: this.parentId//(2)
+        });     
+    },
+       
+    getConfiguration: function(){
+        var config = new Object();    
+        for( var i = 0 ; i < this.form.items.length ; i++){
+            config[this.form.items.get(i).name] = this.form.items.get(i).value;
+        }
+        return config;
+    }
+});
+
