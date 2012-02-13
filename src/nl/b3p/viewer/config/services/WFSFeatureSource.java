e75fda3fce496983241b362d761c52c91ff4a149
Matthijs Laan <matthijsln@gmail.com>
2012-02-13 18:42:50 +0000

    added loading of WFS service
    added use of DescribeLayer to automatically load WFS service type name linked to WMS layers

--- src/nl/b3p/viewer/config/services/WFSFeatureSource.java
+++ src/nl/b3p/viewer/config/services/WFSFeatureSource.java
@@ -17,8 +17,20 @@
 package nl.b3p.viewer.config.services;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.*;
+import nl.b3p.web.WaitPageStatus;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.wfs.WFSDataStoreFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.opengis.feature.type.AttributeType;
 
 /**
  *
@@ -27,8 +39,145 @@ import javax.persistence.*;
 @Entity
 @DiscriminatorValue(WFSFeatureSource.PROTOCOL)
 public class WFSFeatureSource extends FeatureSource {
+    private static final Log log = LogFactory.getLog(WFSFeatureSource.class);
+    
     public static final String PROTOCOL = "wfs";
+    
+    public WFSFeatureSource() {
+        super();
+    }
+    
+    public WFSFeatureSource(Map params) throws JSONException {
+        super();
 
+        String wfsUrl = params.get(WFSDataStoreFactory.URL.key).toString();
+        if(!wfsUrl.endsWith("&") && !wfsUrl.endsWith("?")){
+            wfsUrl += wfsUrl.indexOf("?") >= 0 ? "&" : "?";
+        }                    
+        wfsUrl = wfsUrl + "REQUEST=GetCapabilities&SERVICE=WFS&VERSION=1.0.0";        
+        
+        setUrl(wfsUrl);
+        // XXX username and password keys
+    }    
+
+    public void loadFeatureTypes() throws Exception {
+        loadFeatureTypes(new WaitPageStatus());
+    }
+    
+    public void loadFeatureTypes(WaitPageStatus status) throws Exception {
+        status.setCurrentAction("Ophalen informatie...");
+
+        DataStore store = null;
+        try {
+            store = createDataStore();
+            status.setProgress(10);
+            status.setCurrentAction("Lijst van type-namen ophalen...");
+            String[] typeNames = store.getTypeNames();
+            status.setProgress(20);        
+
+            /* XXX generalize this code and exact same code in JDBCFeatureSource */
+            if(typeNames.length != 0) {
+                double progress = 20.0;
+                double progressPerTypeName = (80.0/typeNames.length);
+                for(String typeName: typeNames) {
+                    status.setCurrentAction("Inladen schema van type \"" + typeName + "\"...");
+                    log.debug("Loading feature source " + typeName + " for WFS url " + getUrl());
+
+                    SimpleFeatureSource gtFs = store.getFeatureSource(typeName);
+
+                    SimpleFeatureType sft = new SimpleFeatureType();
+                    sft.setTypeName(typeName);
+                    sft.setFeatureSource(this);
+                    sft.setWriteable(true);
+                    if(gtFs.getInfo() != null) {
+                        sft.setDescription(gtFs.getInfo().getDescription());
+                    }
+
+                    org.opengis.feature.simple.SimpleFeatureType gtFt = gtFs.getSchema();
+
+                    for(org.opengis.feature.type.AttributeDescriptor gtAtt: gtFt.getAttributeDescriptors()) {
+                        AttributeDescriptor att = new AttributeDescriptor();
+                        sft.getAttributes().add(att);
+                        att.setName(gtAtt.getLocalName());
+
+                        AttributeType gtType = gtAtt.getType();
+                        String binding = gtType.getBinding().getName();
+
+                        /* XXX use instanceof... */                        
+                        String type = "";
+                        if(binding.equals("com.vividsolutions.jts.geom.MultiPolygon")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY_MPOLYGON;
+                        }else if(binding.equals("com.vividsolutions.jts.geom.Polygon")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY_POLYGON;
+                        }else if(binding.equals("com.vividsolutions.jts.geom.Geometry")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY;
+                        }else if(binding.equals("com.vividsolutions.jts.geom.LineString")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY_LINESTRING;
+                        }else if(binding.equals("com.vividsolutions.jts.geom.Point")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY_POINT;
+                        }else if(binding.equals("com.vividsolutions.jts.geom.MultiLineString")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY_MLINESTRING;
+                        }else if(binding.equals("com.vividsolutions.jts.geom.MultiPoint")){
+                            type = AttributeDescriptor.TYPE_GEOMETRY_MPOINT;
+                        }else if(binding.equals("java.lang.Boolean")){
+                            type = AttributeDescriptor.TYPE_BOOLEAN;
+                        }else if(binding.equals("java.lang.Long")){
+                            type = AttributeDescriptor.TYPE_INTEGER;
+                        }else if(binding.equals("java.lang.String")){
+                            type = AttributeDescriptor.TYPE_STRING;
+                        }else if(binding.equals("java.lang.Integer")){
+                            type = AttributeDescriptor.TYPE_INTEGER;
+                        }else if(binding.equals("java.lang.Short")){
+                            type = AttributeDescriptor.TYPE_INTEGER;
+                        }else if(binding.equals("java.lang.Double")){
+                            type = AttributeDescriptor.TYPE_DOUBLE;
+                        }else if(binding.equals("java.lang.Float")){
+                            type = AttributeDescriptor.TYPE_DOUBLE;
+                        }else if(binding.equals("java.sql.Timestamp")){
+                            type = AttributeDescriptor.TYPE_TIMESTAMP;
+                        }else if(binding.equals("java.sql.Date")){
+                            type = AttributeDescriptor.TYPE_DATE;
+                        }else if(binding.equals("java.math.BigDecimal")){
+                            type = AttributeDescriptor.TYPE_DOUBLE;
+                        }
+                        
+                        if(sft.getGeometryAttribute() == null && type.startsWith("com.vividsolutions.jts.geom")) {
+                            sft.setGeometryAttribute(att.getName());
+                        }
+                        
+                        att.setType(type);
+                    }
+                    this.getFeatureTypes().add(sft);         
+                    progress += progressPerTypeName;
+                    status.setProgress((int)progress);
+                }
+            }            
+        } finally {
+            status.setProgress(100);
+            status.setCurrentAction("Service ingeladen");
+            status.setFinished(true);
+            if(store != null) {
+                store.dispose();
+            }
+        }        
+    }
+    
+    public DataStore createDataStore() throws Exception {
+        Map params = new HashMap();
+        params.put(WFSDataStoreFactory.URL.key, getUrl());
+        
+        log.debug("Opening datastore using parameters: " + params);
+        try {
+            DataStore ds = DataStoreFinder.getDataStore(params);      
+            if(ds == null) {
+                throw new Exception("Cannot open datastore using parameters " + params);
+            }
+            return ds;
+        } catch(Exception e) {
+            throw new Exception("Cannot open datastore using parameters " + params, e);
+        }
+    }    
+    
     @Override
     List<String> calculateUniqueValues(SimpleFeatureType sft, String attributeName, int maxFeatures) throws IOException {
         throw new UnsupportedOperationException("Not supported yet.");
--- src/nl/b3p/viewer/config/services/WMSService.java
+++ src/nl/b3p/viewer/config/services/WMSService.java
@@ -17,11 +17,18 @@
 package nl.b3p.viewer.config.services;
 
 import java.net.URL;
-import java.util.Map;
+import java.util.*;
 import javax.persistence.*;
 import nl.b3p.web.WaitPageStatus;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.geotools.data.ServiceInfo;
+import org.geotools.data.ows.LayerDescription;
+import org.geotools.data.wfs.WFSDataStoreFactory;
 import org.geotools.data.wms.*;
+import org.geotools.data.wms.request.DescribeLayerRequest;
+import org.geotools.data.wms.response.DescribeLayerResponse;
+import org.stripesstuff.stripersist.Stripersist;
 
 /**
  *
@@ -30,6 +37,8 @@ import org.geotools.data.wms.*;
 @Entity
 @DiscriminatorValue(WMSService.PROTOCOL)
 public class WMSService extends GeoService {
+    private static final Log log = LogFactory.getLog(WMSService.class);
+    
     public static final String PROTOCOL = "wms";
 
     public static final String PARAM_OVERRIDE_URL = "overrideUrl";
@@ -49,6 +58,7 @@ public class WMSService extends GeoService {
         try {
             status.setCurrentAction("Ophalen informatie...");
             
+            // XXX username / password by overriding HTTPClient?
             WebMapServer gtwms = new WebMapServer(new URL(url));
 
             WMSService wms = new WMSService();
@@ -57,7 +67,7 @@ public class WMSService extends GeoService {
             wms.setName(si.getTitle());
 
             wms.setOverrideUrl(Boolean.TRUE.equals(params.get(PARAM_OVERRIDE_URL)));
-            if(overrideUrl) {
+            if(wms.getOverrideUrl()) {
                 wms.setUrl(url);
             } else {
                 wms.setUrl(si.getSource().toString());
@@ -67,16 +77,106 @@ public class WMSService extends GeoService {
 
             status.setProgress(30);
             status.setCurrentAction("Inladen layers...");
-            status.setProgress(70);
+            status.setProgress(50);
 
-            org.geotools.data.ows.Layer l = gtwms.getCapabilities().getLayer();
-            wms.setTopLayer(new Layer(l, wms));
+            org.geotools.data.ows.Layer rl = gtwms.getCapabilities().getLayer();
+            wms.setTopLayer(new Layer(rl, wms));
+            
+            status.setProgress(60);
+            status.setCurrentAction("Gerelateerde WFS bronnen opzoeken...");
+            
+            StringBuffer layers = new StringBuffer();
+            try {
+                getAllNonVirtualLayers(layers, wms.getTopLayer());
+
+                DescribeLayerRequest dlreq = gtwms.createDescribeLayerRequest();
+                dlreq.setLayers(layers.toString());
+                log.debug("Issuing DescribeLayer request for WMS " + url + " with layers=" + layers);
+                DescribeLayerResponse dlr = gtwms.issueRequest(dlreq);
+                
+                Map<String,List<LayerDescription>> layerDescByWfs = new HashMap<String,List<LayerDescription>>();
+                
+                for(LayerDescription ld: dlr.getLayerDescs()) {
+                    log.debug(String.format("DescribeLayer response, name=%s, wfs=%s, typeNames=%s",
+                            ld.getName(),
+                            ld.getWfs(),
+                            Arrays.toString(ld.getQueries())
+                    ));
+                    if(ld.getWfs() != null && ld.getQueries() != null && ld.getQueries().length != 0) {
+                        if(ld.getQueries().length != 1) {
+                            log.debug("Cannot handle multiple typeNames for this layer, only using the first");
+                        }
+                        List<LayerDescription> lds = layerDescByWfs.get(ld.getWfs().toString());
+                        if(lds == null) {
+                            lds = new ArrayList<LayerDescription>();
+                            layerDescByWfs.put(ld.getWfs().toString(), lds);
+                        }
+                        lds.add(ld);
+                    }                                
+                }
+                
+                status.setProgress(70);
+                String action = "Gerelateerde WFS bron inladen";
+                String[] wfses = (String[])layerDescByWfs.keySet().toArray(new String[] {});
+                for(int i = 0; i < wfses.length; i++) {
+                    String wfsUrl = wfses[i];
+
+                    String thisAction = action + (wfses.length > 1 ? " (" + (i+1) + " van " + wfses.length + ")" : "");
+                    status.setCurrentAction(thisAction + ": GetCapabilities...");
+                    
+                    Map p = new HashMap();
+                    p.put(WFSDataStoreFactory.URL.key, wfsUrl);
+                    // XXX use same password for WMS, maybe only if URLs have same hostname?
+                    
+                    try {
+                        WFSFeatureSource wfsFs = new WFSFeatureSource(p);
+                        wfsFs.loadFeatureTypes();
+                        
+                        boolean used = false;
+                        for(LayerDescription ld: layerDescByWfs.get(wfsUrl)) {
+                            Layer l = wms.getLayer(ld.getName());
+                            if(l != null) {
+                                SimpleFeatureType sft = wfsFs.getFeatureType(ld.getQueries()[0]);
+                                l.setFeatureType(sft);
+                                log.debug("Feature type for layer " + l.getName() + " set to feature type " + sft.getTypeName());
+                                used = true;
+                            }                            
+                        }
+                        if(used) {
+                            log.debug("Type from WFSFeatureSource with url " + wfsUrl + " used by layer of WMS, persisting after finding unique name");
+                            
+                            wfsFs.setName(FeatureSource.findUniqueName(wms.getName()));
+                            log.debug("Unique name found for WFSFeatureSource: " + wfsFs.getName());
+
+                            Stripersist.getEntityManager().persist(wfsFs);
+                        } else {
+                            log.debug("No type from WFSFeatureSource with url " + wfsUrl + " used, not persisting!");
+                        }
+                    } catch(Exception e) {
+                        log.error("Error loading WFS from url " + wfsUrl, e);
+                    }                    
+                }
+            } catch(Exception e) {
+                log.warn("DescribeLayer request failed for layers " + layers + " on service " + url, e);
+            }            
             
             return wms;
         } finally {
-            status.setCurrentAction("");
             status.setProgress(100);
+            status.setCurrentAction("Service ingeladen");
             status.setFinished(true);
+        }
+    }
+    
+    private void getAllNonVirtualLayers(StringBuffer sb, Layer l) {
+        if(!l.isVirtual()) {
+            if(sb.length() > 0) {
+                sb.append(",");
+            }
+            sb.append(l.getName());
+        }
+        for(Layer child: l.getChildren()) {
+            getAllNonVirtualLayers(sb, child);
         }
     }
 
