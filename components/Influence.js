4db23cfc33e92cdb6e2a639d62f9726e1d7ec6cb
Meine Toonen <toonen.meine@gmail.com>
2012-02-13 11:04:00 +0000

    Influence added

---
+++ components/Influence.js
@@ -0,0 +1,29 @@
+/* 
+ * Copyright (C) 2012 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Buffer component
+ * Creates a Drawing component
+ * @author <a href="mailto:meinetoonen@b3partners.nl">Meine Toonen</a>
+ */
+Ext.define ("viewer.components.Influence",{
+    extend: "viewer.components.Component",    
+    constructor: function (conf){        
+        viewer.components.Influence.superclass.constructor.call(this, conf);
+        this.initConfig(conf);        
+        return this;
+    }      
+});
