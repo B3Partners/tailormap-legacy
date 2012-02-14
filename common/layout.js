445cecf8ab883b69354bc831b6f1d7b18fc5249b
Geert Plaisier <geert.plaisier@gmail.nl>
2012-02-14 16:24:24 +0000

    LayoutManager v0.9

--- common/layout.js
+++ common/layout.js
@@ -17,16 +17,16 @@
 
 Ext.define('viewer.LayoutManager', {
     defaultRegionSettings: {
-        header: {region: 'north', useTabs: false, defaultLayout: {height: 150}},
-        leftmargin_top: {region:'west', subregion:'center', columnOrientation: 'vertical', useTabs: true, defaultLayout: {width: 250}},
-        leftmargin_bottom: {region:'west', subregion:'south', columnOrientation: 'vertical', useTabs: true, defaultLayout: {height: 250}},
-        left_menu: {region:'center', subregion:'west', columnOrientation: 'horizontal', useTabs: false, defaultLayout: {width: 150}},
+        header: {region: 'north', columnOrientation: 'horizontal', useTabs: false, defaultLayout: {height: 150}},
+        leftmargin_top: {region:'west', subregion:'center', columnOrientation: 'vertical', subRegionOrientation: 'vertical', useTabs: true, defaultLayout: {width: 250}},
+        leftmargin_bottom: {region:'west', subregion:'south', columnOrientation: 'vertical', subRegionOrientation: 'vertical', useTabs: true, defaultLayout: {height: 250}},
+        left_menu: {region:'center', subregion:'west', columnOrientation: 'horizontal', subRegionOrientation: 'vertical', singleComponentBlock: true, useTabs: false, defaultLayout: {width: 150}},
         top_menu: {region:'none'},
-        content: {region:'center', subregion:'center', columnOrientation: 'horizontal', useTabs: false, defaultLayout: {}},
+        content: {region:'center', subregion:'center', columnOrientation: 'horizontal', subRegionOrientation: 'vertical', useTabs: false, defaultLayout: {}},
         popupwindow: {},
-        rightmargin_top: {region:'east', subregion:'center', columnOrientation: 'vertical', useTabs: true, defaultLayout: {width: 250}},
-        rightmargin_bottom: {region:'east', subregion:'south', columnOrientation: 'vertical', useTabs: true, defaultLayout: {height: 250}},
-        footer: {region:'south', useTabs: false, defaultLayout: {height: 150}}
+        rightmargin_top: {region:'east', subregion:'center', columnOrientation: 'vertical', subRegionOrientation: 'vertical', useTabs: true, defaultLayout: {width: 250}},
+        rightmargin_bottom: {region:'east', subregion:'south', columnOrientation: 'vertical', subRegionOrientation: 'vertical', useTabs: true, defaultLayout: {height: 250}},
+        footer: {region:'south', columnOrientation: 'horizontal', useTabs: false, defaultLayout: {height: 150}}
     },
     layout: {},
     layoutItems: {},
@@ -44,11 +44,11 @@ Ext.define('viewer.LayoutManager', {
 
     createLayout: function() {
         var me = this;
-        //console.log('LAYOUTMANAGER: ', me);
+        console.log('LAYOUTMANAGER: ', me);
         var regionList = me.createRegionList();
-        //console.log('REGIONLIST: ', regionList);
+        console.log('REGIONLIST: ', regionList);
         var viewportItems = me.buildLayoutRegions(regionList);
-        //console.log('VIEWPORTITEMS: ', viewportItems);
+        console.log('VIEWPORTITEMS: ', viewportItems);
         me.renderLayout(viewportItems);
     },
 
@@ -67,14 +67,12 @@ Ext.define('viewer.LayoutManager', {
                 }
                 // Push the layout to the array
                 layoutItems[me.defaultRegionSettings[regionid].region].push({
-                    // Region holds the defaultConfig region
-                    region: defaultConfig,
-                    // Regionconfig holds the regionconfig from the layoutmanager
-                    regionconfig: regionconfig,
                     // Region name
                     name: regionid,
-                    // Layout of the region (widths, heights, etc.)
-                    layout: defaultConfig.defaultLayout
+                    // regionConfig holds the regionconfig from the layoutmanager
+                    regionConfig: regionconfig,
+                    // regionDefaultConfig holds the defaultConfig region
+                    regionDefaultConfig: defaultConfig
                 });
             }
         });
@@ -90,59 +88,67 @@ Ext.define('viewer.LayoutManager', {
         return viewportItems;
     },
     
-    getLayoutRegion: function(region, value) {
+    getLayoutRegion: function(regionid, regionitems) {
         var me = this;
         var layout = {
             width: 0,
             height: 0
         };
         var regionlayout = null;
-        if(value.length > 1) {
-            var items = me.getSubLayoutRegion(value);
-            var centerItem = me.getSubRegionCenterItem(value);
+        var extLayout = '';
+        if(regionitems.length > 1) {
+            var items = me.getSubLayoutRegion(regionitems);
+            var centerItem = me.getSubRegionCenterItem(regionitems);
             if(items.length > 0 && centerItem != null) {
-                var extLayout = 'vbox';
-                if(centerItem.region.columnOrientation == 'horizontal') extLayout = 'hbox';
-                if(region != 'center') {
-                    layout.width = centerItem.layout.width;
-                    if(centerItem.regionconfig.layout) {
-                        regionlayout = centerItem.regionconfig.layout;
-                        if(regionlayout.width != '' && regionlayout.widthmeasure == 'px') {
-                            layout.width = parseInt(regionlayout.width);
-                        } else if(regionlayout.width != '' && regionlayout.widthmeasure == '%') {
-                            layout.flex = parseInt(regionlayout.width) / 100;
-                        }
-                    }
-                }
-                return Ext.apply({
-                    xtype: 'container',
-                    region: region,
-                    layout: extLayout,
-                    items: items
-                }, layout);
-            }
-        } else {
-            var componentItems = me.createComponents(value[0].regionconfig.components, value[0].region);
-            componentItems = me.getRegionContent(value[0].region, componentItems);
-            if(value[0].region.region != "none") {
-                layout = value[0].layout;
-                if(value[0].regionconfig.layout) {
-                    regionlayout = value[0].regionconfig.layout;
+                extLayout = { type: 'vbox', align: 'stretch' };
+                if(centerItem.regionDefaultConfig.columnOrientation == 'horizontal') extLayout = { type: 'hbox', align: 'stretch' };
+                if(regionid != 'center') {
+                    layout.width = centerItem.regionDefaultConfig.defaultLayout.width;
+                    regionlayout = centerItem.regionConfig.layout;
                     if(regionlayout.width != '' && regionlayout.widthmeasure == 'px') {
                         layout.width = parseInt(regionlayout.width);
                     } else if(regionlayout.width != '' && regionlayout.widthmeasure == '%') {
                         layout.flex = parseInt(regionlayout.width) / 100;
                     }
-                    if(regionlayout.height != '' && regionlayout.heightmeasure == 'px') {
-                        layout.height = parseInt(regionlayout.height);
-                    } else if(regionlayout.height != '' && regionlayout.heightmeasure == '%') {
-                        layout.flex = parseInt(regionlayout.height) / 100;
+                } else {
+                    layout.flex = 1;
+                    layout.height = '100%';
+                }
+                return Ext.apply({
+                    xtype: 'container',
+                    region: regionid,
+                    layout: extLayout,
+                    items: items
+                }, layout);
+            }
+        } else {
+            regionlayout = regionitems[0].regionConfig.layout;
+            var componentItems = me.createComponents(regionitems[0].regionConfig.components, regionitems[0].regionDefaultConfig, regionlayout);
+            componentItems = me.getRegionContent(componentItems, regionlayout);
+            if(regionitems[0].regionDefaultConfig.region != "none") {
+                layout = regionitems[0].regionDefaultConfig.defaultLayout;
+                if(regionlayout.width != '' && regionlayout.widthmeasure == 'px') {
+                    layout.width = parseInt(regionlayout.width);
+                } else if(regionlayout.width != '' && regionlayout.widthmeasure == '%') {
+                    layout.flex = parseInt(regionlayout.width) / 100;
+                }
+                if(regionlayout.height != '' && regionlayout.heightmeasure == 'px') {
+                    layout.height = parseInt(regionlayout.height);
+                } else if(regionlayout.height != '' && regionlayout.heightmeasure == '%') {
+                    layout.flex = parseInt(regionlayout.height) / 100;
+                }
+                
+                extLayout = 'fit';
+                if(regionlayout.useTabs == false && componentItems.length > 1 && !Ext.isDefined(regionitems[0].regionDefaultConfig.singleComponentBlock) && !regionitems[0].regionDefaultConfig.singleComponentBlock) {
+                    extLayout = { type: 'vbox', align: 'stretch' };
+                    if(regionitems[0].regionDefaultConfig.columnOrientation == 'horizontal') {
+                        extLayout = { type: 'hbox', align: 'stretch' };
                     }
                 }
                 return Ext.apply({
                     xtype: 'container',
-                    region: region,
-                    layout: 'vbox',
+                    region: regionid,
+                    layout: extLayout,
                     items: componentItems
                 }, layout);
             }
@@ -150,87 +156,107 @@ Ext.define('viewer.LayoutManager', {
         return {};
     },
     
-    getSubLayoutRegion: function(value) {
+    getSubLayoutRegion: function(regionitems) {
         var me = this;
-        var items = [];
-        Ext.Array.each(value, function(item, index) {
+        var items = {};
+        Ext.Array.each(regionitems, function(item, index) {
             var sublayout = {};
-            var componentItems = me.createComponents(item.regionconfig.components, item.region);
-            componentItems = me.getRegionContent(item.region, componentItems);
-            if(item.regionconfig.layout) {
-                var regionlayout = item.regionconfig.layout;
-                if(item.region.columnOrientation == 'vertical') {
-                    if(item.region.subregion != 'center') {
-                        sublayout = Ext.apply({
-                            width: 0
-                        }, item.layout);
-                        if(regionlayout.height != '' && regionlayout.heightmeasure == 'px') {
-                            sublayout.height = parseInt(regionlayout.height);
-                        } else if(regionlayout.height != '' && regionlayout.heightmeasure == '%') {
-                            sublayout.flex = parseInt(regionlayout.height) / 100;
-                        }
-                    } else {
-                        sublayout.flex = 1;
+            var regionlayout = item.regionConfig.layout;
+            var componentItems = me.createComponents(item.regionConfig.components, item.regionDefaultConfig, regionlayout);
+            componentItems = me.getRegionContent(componentItems, regionlayout);
+            if(item.regionDefaultConfig.columnOrientation == 'vertical') {
+                if(item.regionDefaultConfig.subregion != 'center') {
+                    sublayout = Ext.apply({
+                        width: 0
+                    }, item.regionDefaultConfig.defaultLayout);
+                    if(regionlayout.height != '' && regionlayout.heightmeasure == 'px') {
+                        sublayout.height = parseInt(regionlayout.height);
+                    } else if(regionlayout.height != '' && regionlayout.heightmeasure == '%') {
+                        sublayout.flex = parseInt(regionlayout.height) / 100;
                     }
-                    sublayout.width = '100%';
+                } else {
+                    sublayout.flex = 1;
                 }
-                if(item.region.columnOrientation == 'horizontal') {
-                    if(item.region.subregion != 'center') {
-                        sublayout = Ext.apply({
-                            height: 0
-                        }, item.layout);
-                        if(regionlayout.width != '' && regionlayout.widthmeasure == 'px') {
-                            sublayout.width = parseInt(regionlayout.width);
-                        } else if(regionlayout.width != '' && regionlayout.widthmeasure == '%') {
-                            sublayout.flex = parseInt(regionlayout.width) / 100;
-                        }
-                    } else {
-                        sublayout.flex = 1;
-                    }
-                    sublayout.height = '100%';
-                }
+                sublayout.width = '100%';
             }
+            if(item.regionDefaultConfig.columnOrientation == 'horizontal') {
+                if(item.regionDefaultConfig.subregion != 'center') {
+                    sublayout = Ext.apply({
+                        height: 0
+                    }, item.regionDefaultConfig.defaultLayout);
+                    if(regionlayout.width != '' && regionlayout.widthmeasure == 'px') {
+                        sublayout.width = parseInt(regionlayout.width);
+                    } else if(regionlayout.width != '' && regionlayout.widthmeasure == '%') {
+                        sublayout.flex = parseInt(regionlayout.width) / 100;
+                    }
+                } else {
+                    sublayout.flex = 1;
+                }
+                sublayout.height = '100%';
+            }
+
             var extLayout = 'fit';
-            if(item.region.useTabs == false && componentItems.length > 1) {
-                extLayout = 'vbox';
-                if(item.region.orientation == 'horizontal') {
-                    extLayout = 'hbox';
+            if(regionlayout.useTabs == false && componentItems.length > 1 && !Ext.isDefined(item.regionDefaultConfig.singleComponentBlock) && !item.regionDefaultConfig.singleComponentBlock) {
+                extLayout = { type: 'vbox', align: 'stretch' };
+                if(item.regionDefaultConfig.subRegionOrientation == 'horizontal') {
+                    extLayout = { type: 'hbox', align: 'stretch' };
                 }
             }
-            if(item.region.subregion != "none") {
-                items.push(Ext.apply({
+            if(item.regionDefaultConfig.subregion != "none") {
+                items[item.regionDefaultConfig.subregion] = Ext.apply({
                     xtype: 'container',
                     items: componentItems,
                     layout: extLayout
-                }, sublayout));
+                }, sublayout);
+            }
+        });
+        return me.reorderSubRegions(items);
+    },
+    
+    reorderSubRegions: function(subregions) {
+        var order = ['north','west','center','east','south'];
+        var items = [];
+        Ext.Array.each(order, function(regionname) {
+            if(Ext.isDefined(subregions[regionname])) {
+                items.push(subregions[regionname]);
             }
         });
         return items;
     },
     
-    getSubRegionCenterItem: function(value) {
+    getSubRegionCenterItem: function(regionitems) {
         var centerItem = null;
-        Ext.Array.each(value, function(item, index) {
-            if(item.region.subregion == 'center') {
+        Ext.Array.each(regionitems, function(item, index) {
+            if(item.regionDefaultConfig.subregion == 'center') {
                 centerItem =  item;
             }
         });
         return centerItem;
     },
 
-    createComponents: function(components, region) {
+    createComponents: function(components, regionDefaultConfig, regionlayout) {
         var componentItems = [];
         var cmpId = null;
         var me = this;
-
+        var first = true;
+        var singleBlock = (Ext.isDefined(regionDefaultConfig.singleComponentBlock) && regionDefaultConfig.singleComponentBlock);
+        
         Ext.Array.each(components, function(component) {
-            cmpId = Ext.id();
+            if(!singleBlock || (singleBlock && first)) {
+                cmpId = Ext.id();
+            }
             var compStyle = {width: '100%',height: '100%'};
             var compFlex = 0;
-            if(region.useTabs == false) {
+            if(regionlayout.useTabs == false && !singleBlock) {
                 compStyle = {width: '100%'};
-                if(region.orientation == 'horizontal') {
-                    compStyle = {height: '100%'};
+                if(Ext.isDefined(regionDefaultConfig.subRegionOrientation)) {
+                    if(regionDefaultConfig.subRegionOrientation == 'horizontal') {
+                        compStyle = {height: '100%'};
+                    }
+                } else {
+                    if(regionDefaultConfig.columnOrientation == 'horizontal') {
+                        compStyle = {height: '100%'};
+                    }
                 }
                 compFlex = 1;
             }
@@ -249,7 +275,9 @@ Ext.define('viewer.LayoutManager', {
                 style: compStyle,
                 flex: compFlex
             };
-            componentItems.push(cmpView);
+            if(!singleBlock || (singleBlock && first)) {
+                componentItems.push(cmpView);
+            }
             me.componentList.push({
                 htmlId: cmpId,
                 componentName: component.name,
@@ -258,13 +286,14 @@ Ext.define('viewer.LayoutManager', {
             if(component.componentClass == "FlamingoMap") {
                 me.mapId = cmpId;
             }
+            first = false;
         });
 
         return componentItems;
     },
 
-    getRegionContent: function(region, componentItems) {
-        if(Ext.isDefined(region.useTabs) && region.useTabs && componentItems.length > 1) {
+    getRegionContent: function(componentItems, regionlayout) {
+        if(Ext.isDefined(regionlayout.useTabs) && regionlayout.useTabs && componentItems.length > 1) {
             var cmpId = Ext.id();
             var tabcomponent = {
                 xtype: 'tabpanel',
