ee04ea9b26359ae36e2a7b52ead5be02906e66e0
Matthijs Laan <matthijsln@gmail.com>
2012-02-28 10:53:06 +0000

    added actionbean for geoservice registry tree store

---
+++ src/java/nl/b3p/viewer/stripes/GeoServiceRegistryActionBean.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2012 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.viewer.stripes;
+
+import java.io.StringReader;
+import javax.persistence.EntityManager;
+import net.sourceforge.stripes.action.*;
+import net.sourceforge.stripes.validation.Validate;
+import nl.b3p.viewer.config.services.Category;
+import nl.b3p.viewer.config.services.GeoService;
+import nl.b3p.viewer.config.services.Layer;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.stripesstuff.stripersist.Stripersist;
+
+/**
+ *
+ * @author Matthijs Laan
+ */
+@UrlBinding("/action/geoserviceregistry/")
+@StrictBinding
+public class GeoServiceRegistryActionBean implements ActionBean {
+    
+    private ActionBeanContext context;
+
+    @Validate
+    private String nodeId;
+    
+    //<editor-fold defaultstate="collapsed" desc="getters and setters">
+    public ActionBeanContext getContext() {
+        return context;
+    }
+    
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+    
+    public String getNodeId() {
+        return nodeId;
+    }
+    
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+    //</editor-fold>
+    
+    public Resolution load() throws JSONException {
+
+        EntityManager em = Stripersist.getEntityManager();
+        
+        final JSONArray children = new JSONArray();
+        
+        String type = nodeId.substring(0, 1);
+        int id = Integer.parseInt(nodeId.substring(1));
+        
+        if(type.equals("c")) {
+            Category c = em.find(Category.class, new Long(id));
+            
+            // TODO check readers            
+            for(Category sub: c.getChildren()) {
+                JSONObject j = new JSONObject();
+                j.put("id", "c" + sub.getId());
+                j.put("name", sub.getName());
+                j.put("type", "category");
+                j.put("isLeaf", sub.getChildren().isEmpty() && sub.getServices().isEmpty());
+                if(sub.getParent() != null) {
+                    j.put("parentid", sub.getParent().getId());
+                }
+                children.put(j);
+            }
+
+            for(GeoService service: c.getServices()) {
+                JSONObject j = new JSONObject();
+                j.put("id", "s" + service.getId());
+                j.put("name", service.getName());
+                j.put("type", "service");
+                j.put("isLeaf", service.getTopLayer() == null);
+                j.put("status", "ok");//Math.random() > 0.5 ? "ok" : "error");
+                j.put("parentid", nodeId);
+                children.put(j);
+            }
+        } else if(type.equals("s")) {
+            
+            GeoService gs = em.find(GeoService.class, new Long(id));
+            // GeoService may be invalid and not have a top layer
+            if(gs.getTopLayer() != null) {
+                for(Layer sublayer: gs.getTopLayer().getChildren()) {
+                    JSONObject j = new JSONObject();
+                    j.put("id", "l" + sublayer.getId());
+                    if(sublayer.getTitleAlias() != null){
+                        j.put("name", sublayer.getTitleAlias());
+                    }else if(sublayer.getTitle() != null){
+                        j.put("name", sublayer.getTitle());
+                    }else{
+                        j.put("name", sublayer.getName());
+                    }
+                    j.put("type", "layer");
+                    j.put("isLeaf", sublayer.getChildren().isEmpty());
+                    j.put("parentid", nodeId);
+                    j.put("isVirtual", sublayer.isVirtual());
+                    children.put(j);
+                }
+            }
+        } else if(type.equals("l")) {
+            Layer layer = em.find(Layer.class, new Long(id));
+            for(Layer sublayer: layer.getChildren()) {
+                
+                // TODO check readers
+                
+                JSONObject j = new JSONObject();
+                j.put("id", "l" + sublayer.getId());
+                if(sublayer.getTitleAlias() != null){
+                    j.put("name", sublayer.getTitleAlias());
+                }else if(sublayer.getName() != null){
+                    j.put("name", sublayer.getName());
+                }else{
+                    j.put("name", sublayer.getTitle());
+                }
+                j.put("type", "layer");
+                j.put("isLeaf", sublayer.getChildren().isEmpty());
+                j.put("parentid", nodeId);
+                j.put("isVirtual", sublayer.isVirtual());
+                children.put(j);
+            }
+        }
+        
+        return new StreamingResolution("application/json", new StringReader(children.toString()));          
+    }    
+}
