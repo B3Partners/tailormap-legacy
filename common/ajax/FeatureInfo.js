4df070c608c9243847517e7dc819044621fc4448
Matthijs Laan <matthijsln@gmail.com>
2012-03-27 15:36:56 +0000

    added FeatureInfo wrapper

---
+++ common/ajax/FeatureInfo.js
@@ -0,0 +1,71 @@
+/* 
+ * Copyright (C) 2012 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Ext.define("viewer.FeatureInfo", {
+    config: {
+        actionBeanUrl: null,
+        viewerController: null
+    },
+    constructor: function(config) {        
+        this.initConfig(config);      
+        if(this.config.actionbeanUrl == null) {
+            this.config.actionbeanUrl = actionBeans["featureinfo"];
+        }        
+    },
+    getVisibleAppLayers: function() {
+        var visibleLayerIds = viewerController.getVisibleLayerIds();
+        
+        var visibleAppLayers = {};
+        
+        for(var i in visibleLayerIds) {
+            var id = visibleLayerIds[i].split("_")[0];
+            
+            for(var appLayerId in viewerController.app.appLayers) {
+                var appLayer = viewerController.app.appLayers[appLayerId];
+
+                if(appLayer.serviceId == id) {
+                    visibleAppLayers[appLayer.id] = true;
+                }
+            }
+        }
+        return visibleAppLayers;
+
+    },
+    featureInfo: function(x, y, distance, successFunction, failureFunction) {
+        var visibleAppLayers = this.getVisibleAppLayers();
+        
+        var queries = [];
+        for(var id in visibleAppLayers) {
+            var query = { appLayer: id };
+            queries.push(query);
+        }
+        
+        Ext.Ajax.request({
+            url: this.config.actionbeanUrl,
+            params: {featureInfo: true, x: x, y: y, distance: distance, queryJSON: Ext.JSON.encode(queries)},
+            success: function(result) {
+                var response = Ext.JSON.decode(result.responseText);
+                successFunction(response);
+            },
+            failure: function(result) {
+                if(failureFunction != undefined) {
+                    failureFunction("Ajax request failed with status " + result.status + " " + result.statusText + ": " + result.responseText);
+                }
+            }
+        });        
+    }
+});
