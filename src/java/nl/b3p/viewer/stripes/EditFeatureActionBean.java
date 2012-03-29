de4e16961f0735194cc6bf067839f7cb4ce719b1
Matthijs Laan <matthijsln@gmail.com>
2012-03-29 08:50:34 +0000

    parse WKT for geometry attribute and new features

--- src/java/nl/b3p/viewer/stripes/EditFeatureActionBean.java
+++ src/java/nl/b3p/viewer/stripes/EditFeatureActionBean.java
@@ -16,10 +16,11 @@
  */
 package nl.b3p.viewer.stripes;
 
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.WKTReader;
 import net.sourceforge.stripes.action.ActionBean;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import net.sourceforge.stripes.action.*;
@@ -28,6 +29,7 @@ import nl.b3p.viewer.config.app.ApplicationLayer;
 import nl.b3p.viewer.config.services.Layer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.geotools.data.DataUtilities;
 import org.geotools.data.DefaultTransaction;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.Transaction;
@@ -36,8 +38,12 @@ import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.identity.FeatureIdImpl;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.type.AttributeDescriptor;
+import org.opengis.feature.type.GeometryType;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.identity.FeatureId;
 
 /**
  *
@@ -57,6 +63,10 @@ public class EditFeatureActionBean  implements ActionBean {
 
     @Validate
     private ApplicationLayer appLayer;
+    
+    private Layer layer;
+    private SimpleFeatureStore store;
+    private JSONObject jsonFeature;
     
     //<editor-fold defaultstate="collapsed" desc="getters and setters">
     public ActionBeanContext getContext() {
@@ -96,64 +106,35 @@ public class EditFeatureActionBean  implements ActionBean {
                     error = "App layer or service not found";
                     break;
                 }
-                Layer l = appLayer.getService().getLayer(appLayer.getLayerName());
+                layer = appLayer.getService().getLayer(appLayer.getLayerName());
 
-                if(l == null) {
+                if(layer == null) {
                     error = "Layer not found";
                     break;
                 }
 
-                if(l.getFeatureType() == null) {
+                if(layer.getFeatureType() == null) {
                     error ="No feature type";
                     break;
                 }
 
-                JSONObject jf = new JSONObject(feature);
-                // TODO new feature if no FID
-                String fid = jf.getString(FID);
-                
-                FeatureSource fs = l.getFeatureType().openGeoToolsFeatureSource();
+                FeatureSource fs = layer.getFeatureType().openGeoToolsFeatureSource();
                 
                 if(!(fs instanceof SimpleFeatureStore)) {
                     error = "Feature source does not support editing";
                     break;
                 }
-                SimpleFeatureStore fstore = (SimpleFeatureStore)fs;
+                store = (SimpleFeatureStore)fs;
                 
-                Transaction transaction = new DefaultTransaction("edit");
-                fstore.setTransaction(transaction);
-        
-                FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
-                Filter filter = ff.id(new FeatureIdImpl(fid));
-                
-                List<String> attributes = new ArrayList<String>();
-                List values = new ArrayList();
-                for(Iterator<String> it = jf.keys(); it.hasNext();) {
-                    String attribute = it.next();
-                    if(!FID.equals(attribute)) {
-                        attributes.add(attribute);
-                        // TODO: if value is geometry parse WKT
-                        values.add(jf.get(attribute));
-                    }
+                jsonFeature = new JSONObject(feature);
+                String fid = jsonFeature.optString(FID, null);
+
+                if(fid == null) {
+                    json.put(FID, addNewFeature());
+                } else {
+                    editFeature(fid);
                 }
                 
-                log.debug(String.format("Modifying feature source #%d fid=%s, attributes=%s, values=%s",
-                        l.getFeatureType().getId(),
-                        fid,
-                        attributes.toString(),
-                        values.toString()));
-
-                try {
-                    fstore.modifyFeatures(attributes.toArray(new String[] {}), values.toArray(), filter);
-
-                    transaction.commit();
-                } catch (Exception e) {
-                    transaction.rollback();
-                    throw e;
-                } finally {
-                    transaction.close();
-                }                
-
                 json.put("success", Boolean.TRUE);
             } while(false);
         } catch(Exception e) {
@@ -171,5 +152,94 @@ public class EditFeatureActionBean  implements ActionBean {
         }      
         
         return new StreamingResolution("application/json", new StringReader(json.toString(4)));            
+    }
+    
+    private String addNewFeature() throws Exception {
+        
+        SimpleFeature f = DataUtilities.template(store.getSchema());
+
+        Transaction transaction = new DefaultTransaction("create");
+        store.setTransaction(transaction);
+        
+        for(AttributeDescriptor ad: store.getSchema().getAttributeDescriptors()) {
+            if(ad.getType() instanceof GeometryType) {
+                String wkt = jsonFeature.optString(ad.getLocalName(), null);
+                Geometry g = null;
+                if(wkt != null) {
+                    g = new WKTReader().read(wkt);
+                }
+                f.setDefaultGeometry(g);
+            } else {
+                f.setAttribute(ad.getLocalName(), jsonFeature.opt(ad.getLocalName()));
+            }
+        }      
+
+        log.debug(String.format("Creating new feature in feature source source #%d: %s",
+                layer.getFeatureType().getId(),
+                f.toString()));
+
+        try {
+            List<FeatureId> ids = store.addFeatures(DataUtilities.collection(f));
+
+            transaction.commit();
+            return ids.get(0).getID();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            transaction.close();
+        }               
+    }
+    
+    private void editFeature(String fid) throws Exception {
+        Transaction transaction = new DefaultTransaction("edit");
+        store.setTransaction(transaction);
+
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        Filter filter = ff.id(new FeatureIdImpl(fid));
+
+        List<String> attributes = new ArrayList<String>();
+        List values = new ArrayList();
+        for(Iterator<String> it = jsonFeature.keys(); it.hasNext();) {
+            String attribute = it.next();
+            if(!FID.equals(attribute)) {
+                
+                AttributeDescriptor ad = store.getSchema().getDescriptor(attribute);
+                
+                if(ad != null) {
+                    attributes.add(attribute);
+                    
+                    if(ad.getType() instanceof GeometryType) {
+                        String wkt = jsonFeature.getString(ad.getLocalName());
+                        Geometry g = null;
+                        if(wkt != null) {
+                            g = new WKTReader().read(wkt);
+                        } 
+                        values.add(g);
+                    } else {
+                        values.add(jsonFeature.get(attribute));
+                    }
+                } else {
+                    log.warn(String.format("Attribute \"%s\" not in feature type; ignoring", attribute));
+                }
+            }
+        }
+
+        log.debug(String.format("Modifying feature source #%d fid=%s, attributes=%s, values=%s",
+                layer.getFeatureType().getId(),
+                fid,
+                attributes.toString(),
+                values.toString()));
+
+        try {
+            store.modifyFeatures(attributes.toArray(new String[] {}), values.toArray(), filter);
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+        } finally {
+            transaction.close();
+        }                
     }
 }
