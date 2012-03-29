dda18807a78f9f7d2ce28a19016eb9b1b04d5ab2
Matthijs Laan <matthijsln@gmail.com>
2012-03-29 15:40:06 +0000

    implemented csw search

--- src/java/nl/b3p/viewer/stripes/CatalogSearchActionBean.java
+++ src/java/nl/b3p/viewer/stripes/CatalogSearchActionBean.java
@@ -17,8 +17,23 @@
 package nl.b3p.viewer.stripes;
 
 import java.io.StringReader;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
 import net.sourceforge.stripes.action.*;
 import net.sourceforge.stripes.validation.Validate;
+import nl.b3p.csw.client.CswClient;
+import nl.b3p.csw.client.CswSmartRequestCreator;
+import nl.b3p.csw.client.InputBySearch;
+import nl.b3p.csw.client.OutputBySearch;
+import nl.b3p.csw.jaxb.csw.GetRecords;
+import nl.b3p.csw.server.CswServable;
+import nl.b3p.csw.server.GeoNetworkCswServer;
+import nl.b3p.csw.util.CswClientFactory;
+import nl.b3p.csw.util.OnlineResource;
+import nl.b3p.csw.util.Protocol;
+import org.jdom.Document;
+import org.jdom.output.XMLOutputter;
 import org.json.*;
 
 /**
@@ -63,18 +78,61 @@ public class CatalogSearchActionBean implements ActionBean {
     }
     //</editor-fold>        
     
+    @DefaultHandler
     public Resolution search() throws JSONException {    
         JSONObject json = new JSONObject();
         json.put("success", Boolean.FALSE);
         String error = null;
+    
+        try {
+            JSONArray results = new JSONArray();
         
-        JSONArray results = new JSONArray();
-        
-        
-        json.put("results", results);                
-        
-        json.put("success", Boolean.TRUE);
-        
+            CswServable server = new GeoNetworkCswServer(null,
+                    url,
+                    null, 
+                    null
+            );        
+            
+            CswClient client = new CswClient(server);
+            InputBySearch input = new InputBySearch(q);
+            OutputBySearch output = client.search(input);            
+
+            XMLOutputter outputter = new XMLOutputter();        
+            outputter.output(output.getXml(), System.out);
+
+            Map<URI, List<OnlineResource>> map = output.getResourcesMap();
+            for (List<OnlineResource> resourceList : map.values()) {
+                for (OnlineResource resource : resourceList) {
+
+                    
+                    String title = output.getTitle(resource.getMetadata());
+                    String rurl = resource.getUrl() != null ? resource.getUrl().toString() : null;
+                    String layer = resource.getName();
+                    String protocol = resource.getProtocol() != null ? resource.getProtocol().getName() : null;
+                    
+                    if(title != null && rurl != null && protocol != null) {
+                        if(protocol.toLowerCase().indexOf("wms") != -1) {
+                            JSONObject result = new JSONObject();
+                            result.put("label", title + (layer != null ? " (laag: " + layer + ")" : ""));
+                            result.put("url", rurl);
+                            result.put("protocol", "wms");
+                            results.put(result);
+                        }
+                    }
+                }
+            }
+
+            json.put("results", results);                
+
+            json.put("success", Boolean.TRUE);
+        } catch(Exception e) {
+
+            error = "Fout bij zoeken in CSW: " + e.toString();
+            if(e.getCause() != null) {
+                error += "; oorzaak: " + e.getCause().toString();
+            }
+        }
+                
         if(error != null) {
             json.put("error", error);
         }
