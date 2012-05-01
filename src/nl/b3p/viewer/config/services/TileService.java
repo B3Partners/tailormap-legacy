712ed15fbaba34c29ae2b4c30252bb05e05422dd
Roy Braam <braam.roy@gmail.com>
2012-05-01 14:16:28 +0000

    Tiling layer can be added in viewer-admin

--- src/nl/b3p/viewer/config/services/TileService.java
+++ src/nl/b3p/viewer/config/services/TileService.java
@@ -16,9 +16,13 @@
  */
 package nl.b3p.viewer.config.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.persistence.*;
 import nl.b3p.web.WaitPageStatus;
+import org.stripesstuff.stripersist.Stripersist;
 
 /**
  *
@@ -27,7 +31,15 @@ import nl.b3p.web.WaitPageStatus;
 @Entity
 @DiscriminatorValue(TileService.PROTOCOL)
 public class TileService extends GeoService {
-    public static final String PROTOCOL = "tiled";           
+    public static final String PROTOCOL = "tiled";
+    public static final String PARAM_RESOLUTIONS = "resolutions";
+    public static final String PARAM_TILESIZE = "tileSize";
+    public static final String PARAM_TILINGPROTOCOL = "tilingProtocol";
+    public static final String PARAM_SERVICENAME = "ServiceName";
+    public static final String PARAM_SERVICEBBOX= "serviceBbox";
+    public static final String PARAM_IMAGEEXTENSION= "imageExtension";
+    public static final String PARAM_CRS= "crs";
+    
     private String tilingProtocol;
 
     public String getTilingProtocol() {
@@ -40,7 +52,61 @@ public class TileService extends GeoService {
 
     @Override
     public GeoService loadFromUrl(String url, Map params, WaitPageStatus status) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        status.setCurrentAction("Bezig met aanmaken tile service");
+        try{
+            TileService s = new TileService();
+            s.setUrl(url);
+            
+            String serviceName = (String) params.get(PARAM_SERVICENAME);
+            s.setName(serviceName);
+
+            //make fake top layer for tiling.
+            Layer topLayer = new Layer();
+            topLayer.setName(serviceName);
+            topLayer.setVirtual(true);
+
+            s.setTopLayer(topLayer);
+            TileSet ts = new TileSet();
+            ts.setName(serviceName);
+            if (params.containsKey(PARAM_RESOLUTIONS)){
+                String resString = (String) params.get(PARAM_RESOLUTIONS);
+                List<Double> resolutions = new ArrayList<Double>();
+                String[] resTokens = resString.split(",");
+                for (int i = 0; i < resTokens.length; i++){
+                    resolutions.add(Double.parseDouble(resTokens[i].trim()));
+                }
+                ts.setResolutions(resolutions);
+            }
+            if (params.containsKey(PARAM_TILESIZE)){
+                Integer size = (Integer) params.get(PARAM_TILESIZE);
+                ts.setHeight(size);
+                ts.setWidth(size);            
+            }            
+            
+            if (params.containsKey(PARAM_SERVICEBBOX) && params.containsKey(PARAM_CRS)){
+                String[] bboxTokens = ((String)params.get(PARAM_SERVICEBBOX)).split(",");
+                BoundingBox bb = new BoundingBox();
+                bb.setMinx(Double.parseDouble(bboxTokens[0].trim()));
+                bb.setMiny(Double.parseDouble(bboxTokens[1].trim()));
+                bb.setMaxx(Double.parseDouble(bboxTokens[2].trim()));
+                bb.setMaxy(Double.parseDouble(bboxTokens[3].trim()));
+                bb.setCrs(new CoordinateReferenceSystem((String)params.get(PARAM_CRS)));
+                topLayer.getBoundingBoxes().put(bb.getCrs(), bb);
+            }
+            
+            if (params.containsKey(PARAM_IMAGEEXTENSION)){
+                topLayer.getDetails().put("image_extension",(String)params.get(PARAM_IMAGEEXTENSION));
+            }
+            
+            Stripersist.getEntityManager().persist(ts);           
+            topLayer.setTileset(ts);
+            
+            return s;
+        }finally {
+            status.setProgress(100);
+            status.setCurrentAction("Service ingeladen");
+            status.setFinished(true);
+        }
     }
 
 }
