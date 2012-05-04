a44493f2a485104f66735ae735835a54d14c8451
Matthijs Laan <matthijsln@gmail.com>
2012-05-04 10:28:48 +0000

    moved javax.mail to separate class so Tomcat does not fail to start up when mail.jar is not available

--- src/java/nl/b3p/viewer/admin/monitoring/GeoServiceMonitoringListener.java
+++ src/java/nl/b3p/viewer/admin/monitoring/GeoServiceMonitoringListener.java
@@ -16,18 +16,6 @@
  */
 package nl.b3p.viewer.admin.monitoring;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -49,37 +37,14 @@ public class GeoServiceMonitoringListener implements ServletContextListener {
         this.context = sce.getServletContext();
 
         try {
-            getMailSession();
+            Mailer.getMailSession();
         } catch(Exception e) {
             log.error("Error getting mail session, monitoring disabled! Please configure the JNDI JavaMail Session resource correctly.", e);
         }
     }
-
+    
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         log.info("stopped monitoring");
-    }
-    
-    private Session getMailSession() throws NamingException {
-        Context init = new InitialContext();
-        Context env = (Context) init.lookup("java:comp/env");
-        Session session = (Session)env.lookup("mail/session");
-        return session;
-    }
-    
-    private void sendMail(String email, String subject, String mailContent) throws UnsupportedEncodingException, MessagingException, NamingException {
-        
-        Address from = new InternetAddress(
-                context.getInitParameter("monitoring.from.email"), 
-                context.getInitParameter("monitoring.from.name"));
-
-        MimeMessage msg = new MimeMessage(getMailSession());
-        msg.setFrom(from);
-        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-        msg.setSubject(subject);
-        msg.setSentDate(new Date());
-        msg.setContent(mailContent, "text/plain");
-
-        Transport.send(msg);
     }    
 }
