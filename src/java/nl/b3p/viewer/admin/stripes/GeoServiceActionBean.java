bf256bd28bc1f0f090f571604fd94ea94eba2e92
Meine Toonen <toonen.meine@gmail.com>
2012-05-14 13:04:17 +0000

    Check if service has applicationlayers, if so, give notification en return to editting mode of service

--- src/java/nl/b3p/viewer/admin/stripes/GeoServiceActionBean.java
+++ src/java/nl/b3p/viewer/admin/stripes/GeoServiceActionBean.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import javax.annotation.security.RolesAllowed;
 import net.sourceforge.stripes.action.*;
 import net.sourceforge.stripes.validation.*;
+import nl.b3p.viewer.config.app.ApplicationLayer;
 import nl.b3p.viewer.config.security.Group;
 import nl.b3p.viewer.config.services.*;
 import nl.b3p.web.WaitPageStatus;
@@ -36,72 +37,55 @@ import org.stripesstuff.stripersist.Stripersist;
  */
 @StrictBinding
 @UrlBinding("/action/geoservice/{service}")
-@RolesAllowed({Group.ADMIN,Group.REGISTRY_ADMIN})
-public class GeoServiceActionBean implements ActionBean{
+@RolesAllowed({Group.ADMIN, Group.REGISTRY_ADMIN})
+public class GeoServiceActionBean implements ActionBean {
+
     private static final Log log = LogFactory.getLog(GeoServiceActionBean.class);
-    
     private static final String JSP = "/WEB-INF/jsp/services/geoservice.jsp";
-    
     private ActionBeanContext context;
-    
-    @Validate(on={"add"},required=true)
+    @Validate(on = {"add"}, required = true)
     private Category category;
-    
-    @Validate(on="editGeoService",required=true)
+    @Validate(on = "editGeoService", required = true)
     private GeoService service;
-
-    @Validate(on="add", required=true)
+    @Validate(on = "add", required = true)
     private String url;
-
-    @Validate(on="add", required=true)
+    @Validate(on = "add", required = true)
     private String protocol;
-
     /**
-     * Whether the service was succesfully deleted. Use in view JSP to update tree.
+     * Whether the service was succesfully deleted. Use in view JSP to update
+     * tree.
      */
     private boolean serviceDeleted;
-    
     @Validate
     private String name;
-    
     @Validate
     private String username;
     @Validate
     private String password;
-    
     @Validate
     private boolean overrideUrl;
-    
     @Validate
     private String serviceName;
-    
     @Validate
     private Integer tileSize;
-    
     @Validate
     private String tilingProtocol;
-    
     @Validate
     private String resolutions;
-    
     @Validate
     private String serviceBbox;
-    
     @Validate
     private String imageExtension;
-    
     @Validate
     private String crs;
-
     private WaitPageStatus status;
-    
     private JSONObject newService;
 
     //<editor-fold defaultstate="collapsed" desc="getters and setters">
     public ActionBeanContext getContext() {
         return context;
     }
-    
+
     public void setContext(ActionBeanContext context) {
         this.context = context;
     }
@@ -201,7 +185,7 @@ public class GeoServiceActionBean implements ActionBean{
     public void setServiceDeleted(boolean serviceDeleted) {
         this.serviceDeleted = serviceDeleted;
     }
-    
+
     public Integer getTileSize() {
         return tileSize;
     }
@@ -225,7 +209,7 @@ public class GeoServiceActionBean implements ActionBean{
     public void setResolutions(String resolutions) {
         this.resolutions = resolutions;
     }
-    
+
     public String getServiceBbox() {
         return serviceBbox;
     }
@@ -262,13 +246,13 @@ public class GeoServiceActionBean implements ActionBean{
             } else if (protocol.equals(TileService.PROTOCOL)) {
                 TileService ser = (TileService) service;
                 tilingProtocol = ser.getTilingProtocol();
-                
+
                 //tiling service has 1 layer with that has the settings.                
-                Layer layer=ser.getTilingLayer();
+                Layer layer = ser.getTilingLayer();
                 //set the resolutions
-                
-                TileSet tileSet = layer.getTileset();   
-                
+
+                TileSet tileSet = layer.getTileset();
+
                 if (tileSet != null) {
                     String res = "";
                     for (Double resolution : tileSet.getResolutions()) {
@@ -278,11 +262,11 @@ public class GeoServiceActionBean implements ActionBean{
                         res += resolution.toString();
                     }
                     resolutions = res;
-                    
+
                     //set the tilesize
                     tileSize = tileSet.getHeight();
                 }
-               
+
                 //set the service Bbox               
                 if (layer.getBoundingBoxes().size() == 1) {
                     BoundingBox bb = layer.getBoundingBoxes().values().iterator().next();
@@ -305,161 +289,169 @@ public class GeoServiceActionBean implements ActionBean{
         }
         return new ForwardResolution(JSP);
     }
-    
+
     public Resolution save() {
-        if(name != null){
+        if (name != null) {
             service.setName(name);
         }
-        if (service instanceof TileService){
-            TileService ser = (TileService)service;
-            if (tilingProtocol!=null){
-                ((TileService)service).setTilingProtocol(tilingProtocol);
+        if (service instanceof TileService) {
+            TileService ser = (TileService) service;
+            if (tilingProtocol != null) {
+                ((TileService) service).setTilingProtocol(tilingProtocol);
             }
             Layer l = ser.getTilingLayer();
-            if (tileSize!=null){
+            if (tileSize != null) {
                 l.getTileset().setWidth(tileSize);
                 l.getTileset().setHeight(tileSize);
             }
-            if (resolutions!=null){
+            if (resolutions != null) {
                 l.getTileset().setResolutions(resolutions);
             }
-            if (crs!=null && serviceBbox!=null){
+            if (crs != null && serviceBbox != null) {
                 BoundingBox bb = new BoundingBox();
                 bb.setBounds(serviceBbox);
                 bb.setCrs(new CoordinateReferenceSystem(crs));
                 l.getBoundingBoxes().clear();
                 l.getBoundingBoxes().put(bb.getCrs(), bb);
             }
-            if (imageExtension!=null){
-                l.getDetails().put("image_extension",imageExtension);
+            if (imageExtension != null) {
+                l.getDetails().put("image_extension", imageExtension);
             }
-                
+
         }
 
         service.setUsername(username);
         service.setPassword(password);
-        
+
         Stripersist.getEntityManager().persist(service);
         Stripersist.getEntityManager().getTransaction().commit();
-        
+
         getContext().getMessages().add(new SimpleMessage("De service is opgeslagen"));
-        
+
         return edit();
     }
-    
-    public Resolution delete(){
-        /* XXX Als een service layers heeft die toegevoegd zijn aan een applicatie 
-         * mag de service niet verwijderd worden 
+
+    public Resolution delete() {
+        /*
+         * XXX Als een service layers heeft die toegevoegd zijn aan een
+         * applicatie mag de service niet verwijderd worden
          */
-        
-        Category c = service.getCategory();
-        c.getServices().remove(service);
-        
-        List<FeatureSource> linkedSources = Stripersist.getEntityManager().createQuery(
-                "from FeatureSource where linkedService = :service")
-                .setParameter("service", service)
-                .getResultList();
-        for(FeatureSource fs: linkedSources) {
-            fs.setLinkedService(null);
-            getContext().getMessages().add(
-                    new SimpleMessage("De bij deze service automatisch aangemaakte attribuutbron \"{0}\" moet apart worden verwijderd", fs.getName()));
-                              
-        }
-        if (TileService.PROTOCOL.equals(service.getProtocol())){
-            if (service.getTopLayer()!=null && service.getTopLayer().getTileset()!=null){
-                TileSet ts=service.getTopLayer().getTileset();                
-                Stripersist.getEntityManager().remove(ts);     
+        List<ApplicationLayer> applicationLayers = Stripersist.getEntityManager().createQuery("from ApplicationLayer where service = :service").setParameter("service", service).getResultList();
+        if (applicationLayers.size() > 0) {
+            serviceDeleted = false;
+
+            getContext().getValidationErrors().addGlobalError(new SimpleError("Fout bij het verwijderen van de service. De service heeft kaartlagen geconfigureerd in {2} applicaties.", applicationLayers.size()));
+
+            return edit();
+        } else {
+            Category c = service.getCategory();
+            c.getServices().remove(service);
+
+            List<FeatureSource> linkedSources = Stripersist.getEntityManager().createQuery(
+                    "from FeatureSource where linkedService = :service").setParameter("service", service).getResultList();
+            for (FeatureSource fs : linkedSources) {
+                fs.setLinkedService(null);
+                getContext().getMessages().add(
+                        new SimpleMessage("De bij deze service automatisch aangemaakte attribuutbron \"{0}\" moet apart worden verwijderd", fs.getName()));
+
             }
+            if (TileService.PROTOCOL.equals(service.getProtocol())) {
+                if (service.getTopLayer() != null && service.getTopLayer().getTileset() != null) {
+                    TileSet ts = service.getTopLayer().getTileset();
+                    Stripersist.getEntityManager().remove(ts);
+                }
+            }
+
+            Stripersist.getEntityManager().remove(service);
+            Stripersist.getEntityManager().getTransaction().commit();
+
+            serviceDeleted = true;
+
+            getContext().getMessages().add(new SimpleMessage("De service is verwijderd"));
+            return new ForwardResolution(JSP);
         }
-        Stripersist.getEntityManager().remove(service);        
-        Stripersist.getEntityManager().getTransaction().commit();
-        
-        serviceDeleted = true;        
-        getContext().getMessages().add(new SimpleMessage("De service is verwijderd"));
-        
-        return new ForwardResolution(JSP);
     }
-    
-    @ValidationMethod(on="add")
+
+    @ValidationMethod(on = "add")
     public void validateParams(ValidationErrors errors) {
-        if(protocol.equals(ArcIMSService.PROTOCOL) || protocol.equals(TileService.PROTOCOL)) {
-            if(serviceName == null) {
+        if (protocol.equals(ArcIMSService.PROTOCOL) || protocol.equals(TileService.PROTOCOL)) {
+            if (serviceName == null) {
                 errors.add("serviceName", new LocalizableError("validation.required.valueNotPresent"));
             }
-            if (protocol.equals(TileService.PROTOCOL)){
-                if (resolutions==null){
+            if (protocol.equals(TileService.PROTOCOL)) {
+                if (resolutions == null) {
                     errors.add("resolutions", new LocalizableError("validation.required.valueNotPresent"));
                 }
-                if (serviceBbox==null){
+                if (serviceBbox == null) {
                     errors.add("serviceBbox", new LocalizableError("validation.required.valueNotPresent"));
                 }
-                if (crs==null){
+                if (crs == null) {
                     errors.add("crs", new LocalizableError("validation.required.valueNotPresent"));
                 }
-                if (tileSize==null){
+                if (tileSize == null) {
                     errors.add("tileSize", new LocalizableError("validation.required.valueNotPresent"));
                 }
             }
         }
-        
-            
+
+
     }
-    
+
     public Resolution addForm() {
         return new ForwardResolution(JSP);
     }
-    
-    @WaitPage(path="/WEB-INF/jsp/waitpage.jsp", delay=2000, refresh=1000, ajax="/WEB-INF/jsp/waitpageajax.jsp")
+
+    @WaitPage(path = "/WEB-INF/jsp/waitpage.jsp", delay = 2000, refresh = 1000, ajax = "/WEB-INF/jsp/waitpageajax.jsp")
     public Resolution add() throws JSONException {
 
         status = new WaitPageStatus();
-        
+
         Map params = new HashMap();
 
         try {
-            if(protocol.equals(WMSService.PROTOCOL)) {
+            if (protocol.equals(WMSService.PROTOCOL)) {
                 params.put(WMSService.PARAM_OVERRIDE_URL, overrideUrl);
                 params.put(WMSService.PARAM_USERNAME, username);
                 params.put(WMSService.PARAM_PASSWORD, password);
                 service = new WMSService().loadFromUrl(url, params, status);
-            } else if(protocol.equals(ArcGISService.PROTOCOL)) {
+            } else if (protocol.equals(ArcGISService.PROTOCOL)) {
                 params.put(ArcGISService.PARAM_USERNAME, username);
                 params.put(ArcGISService.PARAM_PASSWORD, password);
                 service = new ArcGISService().loadFromUrl(url, params, status);
-            } else if(protocol.equals(ArcIMSService.PROTOCOL)) {
+            } else if (protocol.equals(ArcIMSService.PROTOCOL)) {
                 params.put(ArcIMSService.PARAM_SERVICENAME, serviceName);
                 params.put(ArcIMSService.PARAM_USERNAME, username);
                 params.put(ArcIMSService.PARAM_PASSWORD, password);
                 service = new ArcIMSService().loadFromUrl(url, params, status);
-            } else if (protocol.equals(TileService.PROTOCOL)){
+            } else if (protocol.equals(TileService.PROTOCOL)) {
                 params.put(TileService.PARAM_SERVICENAME, serviceName);
                 params.put(TileService.PARAM_RESOLUTIONS, resolutions);
-                params.put(TileService.PARAM_SERVICEBBOX,serviceBbox);
-                params.put(TileService.PARAM_CRS,crs);
-                params.put(TileService.PARAM_IMAGEEXTENSION,imageExtension);
+                params.put(TileService.PARAM_SERVICEBBOX, serviceBbox);
+                params.put(TileService.PARAM_CRS, crs);
+                params.put(TileService.PARAM_IMAGEEXTENSION, imageExtension);
                 params.put(TileService.PARAM_TILESIZE, tileSize);
                 params.put(TileService.PARAM_TILINGPROTOCOL, tilingProtocol);
-                service = new TileService().loadFromUrl(url,params,status);
-            }else {
+                service = new TileService().loadFromUrl(url, params, status);
+            } else {
                 getContext().getValidationErrors().add("protocol", new SimpleError("Ongeldig"));
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             log.error("Exception loading " + protocol + " service from url " + url, e);
             String s = e.toString();
-            if(e.getCause() != null) {
+            if (e.getCause() != null) {
                 s += "; cause: " + e.getCause().toString();
             }
             getContext().getValidationErrors().addGlobalError(new SimpleError("Fout bij het laden van de service: {2}", s));
             return new ForwardResolution(JSP);
         }
 
-        if(name != null) {
+        if (name != null) {
             service.setName(name);
         }
-        if(username != null) {
+        if (username != null) {
             service.setUsername(username);
         }
-        if(password != null) {
+        if (password != null) {
             service.setPassword(password);
         }
         category = Stripersist.getEntityManager().find(Category.class, category.getId());
@@ -468,14 +460,14 @@ public class GeoServiceActionBean implements ActionBean{
 
         Stripersist.getEntityManager().persist(service);
         Stripersist.getEntityManager().getTransaction().commit();
-        
+
         newService = new JSONObject();
         newService.put("id", "s" + service.getId());
         newService.put("name", service.getName());
         newService.put("type", "service");
         newService.put("isLeaf", service.getTopLayer() == null);
         newService.put("status", "ok");//Math.random() > 0.5 ? "ok" : "error");
-        newService.put("parentid", "c"+category.getId());
+        newService.put("parentid", "c" + category.getId());
 
         getContext().getMessages().add(new SimpleMessage("Service is ingeladen"));
 
