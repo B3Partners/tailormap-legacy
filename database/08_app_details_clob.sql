aadfa3b6ac0505bb3b8332ed6de79813f84a01e8
Matthijs Laan <matthijsln@gmail.com>
2012-05-25 07:50:11 +0000

    changed sql script

--- database/08_app_details_clob.sql
+++ database/08_app_details_clob.sql
@@ -1,7 +1,4 @@
-alter table application_details add temp varchar2(255);
-update application_details set temp = details;
+ALTER TABLE APPLICATION_DETAILS add value CLOB;
+update application_details set value = details;
 alter table application_details drop column details;
-ALTER TABLE APPLICATION_DETAILS add DETAILS CLOB;
-update application_details set details = temp;
-alter table application_details drop column temp;
 
