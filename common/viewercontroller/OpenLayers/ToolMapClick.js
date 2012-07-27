0de337893b71248f368d812bdc1a3e0ba3418dc8
Meine Toonen <toonen.meine@gmail.com>
2012-07-27 08:02:41 +0000

    Added license headers

--- common/viewercontroller/OpenLayers/ToolMapClick.js
+++ common/viewercontroller/OpenLayers/ToolMapClick.js
@@ -14,7 +14,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+/**
+ * @class 
+ * @constructor
+ * @description A drawable vector layer
+ */
 
 Ext.define ("viewer.viewercontroller.openlayers.ToolMapClick",{
     extend: "viewer.viewercontroller.controller.ToolMapClick",
