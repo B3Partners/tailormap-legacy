aa30a6e66a0917c1380a839c22b5e5b0eaf3bd38
Meine Toonen <toonen.meine@gmail.com>
2012-07-27 07:56:57 +0000

    Added license headers

--- common/viewercontroller/OpenLayers/OpenLayersTool.js
+++ common/viewercontroller/OpenLayers/OpenLayersTool.js
@@ -1,8 +1,26 @@
-/**
- * @class 
- * @constructor
- * @description
+/* 
+ * Copyright (C) 2012 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+ /**
+  *Openlayers implementation of Tool.
+  *@see viewer.viewercontroller.controller.Tool
+  *@author <a href="mailto:roybraam@b3partners.nl">Roy Braam</a>
+  *@author <a href="mailto:meinetoonen@b3partners.nl">Meine Toonen</a>
+  **/
 Ext.define("viewer.viewercontroller.openlayers.OpenLayersTool",{
     extend: "viewer.viewercontroller.controller.Tool",
     onActiveHandler:null,
@@ -34,7 +52,7 @@ Ext.define("viewer.viewercontroller.openlayers.OpenLayersTool",{
 
     addControl : function(control){
         if (!(this.type == Tool.GET_FEATURE_INFO)){
-            Ext.Error.raise({msg: "The given Control object is not of type get feature info. But: "+this.type});
+            this.viewerController.logger.info({msg: "The given Control object is not of type get feature info. But: "+this.type});
         }
         this.controls.push(control);
     },
