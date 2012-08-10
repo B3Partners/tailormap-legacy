86d072c8942026af74c5f2a2e38510792554837a
Matthijs Laan <matthijsln@gmail.com>
2012-08-10 11:49:44 +0000

    fix typo so ON_SELECTEDCONTENT_CHANGE event works

--- components/Legend.js
+++ components/Legend.js
@@ -75,7 +75,7 @@ Ext.define("viewer.components.Legend", {
         this.legendContainer = document.getElementById(this.name + 'legendContainer');
         
         this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_LAYERS_INITIALIZED, this.onLayersInitialized,this);
-        this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_SELECTEDCONTENT_CHANGE,this.onSelectedContentChanged,this);
+        this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_SELECTEDCONTENT_CHANGE,this.onSelectedContentChange,this);
         this.viewerController.mapComponent.getMap().registerEvent(viewer.viewercontroller.controller.Event.ON_LAYER_VISIBILITY_CHANGED,this.onLayerVisibilityChanged,this);
         
         return this;
