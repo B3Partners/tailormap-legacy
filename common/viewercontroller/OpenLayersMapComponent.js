039e0634abea4677f8ccd4a9457091b347c86f3a
Roy Braam <braam.roy@gmail.com>
2012-08-14 09:32:10 +0000

    load proj4js when openlayers viewer. add projection, for now hard coded, but needs to be configurable

--- common/viewercontroller/OpenLayersMapComponent.js
+++ common/viewercontroller/OpenLayersMapComponent.js
@@ -20,6 +20,8 @@ Ext.define("viewer.viewercontroller.OpenLayersMapComponent",{
         if(config && config.resolutions){
             resolutions = (config.resolutions).split(",");
         }
+        //TODO: remove the hardcoded projection....
+        Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs";
         this.mapOptions =  {
             projection:new OpenLayers.Projection("EPSG:28992"),
             maxExtent: maxBounds,
