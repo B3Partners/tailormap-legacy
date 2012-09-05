e22b279a87b1dd19dceb928ec3d02ccf9cd15348
Roy Braam <braam.roy@gmail.com>
2012-09-05 14:15:59 +0000

    make sure the resolutions are numbers.

--- common/viewercontroller/openlayers/OpenLayersMapComponent.js
+++ common/viewercontroller/openlayers/OpenLayersMapComponent.js
@@ -21,9 +21,18 @@ Ext.define("viewer.viewercontroller.OpenLayersMapComponent",{
         this.pointButton = null;
         this.lineButton = null;
         this.polygonButton = null;
-        var resolutions = [512,256,128,64,32,16,8,4,2,1,0.5,0.25,0.125];
+        var resolutions;
         if(config && config.resolutions){
-            resolutions = (config.resolutions).split(",");
+            var rString = (config.resolutions).split(",");
+            resolutions=[];
+            for (var i = 0; i < rString.length; i++){
+                var res=Number(rString[i]);
+                if (!isNaN(res)){
+                    resolutions.push(res);
+                }
+            }
+        }else{
+            resolutions = [512,256,128,64,32,16,8,4,2,1,0.5,0.25,0.125];
         }
         //TODO: remove the hardcoded projection....
         Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs";
