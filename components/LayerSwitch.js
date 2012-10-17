bbb14b9b0c82ac158348bf38dedc07bdbb5761f5
Matthijs Laan <matthijsln@gmail.com>
2012-10-17 12:19:46 +0000

    refactored LayerSwitch to work on selected content and listen to events when selected content changes and layers are made visible and invisible; now also makes sure only one background layer is visible, fixes #492

--- components/LayerSwitch.js
+++ components/LayerSwitch.js
@@ -14,39 +14,51 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
  * LayerSwitch component
- * Creates a LayerSwitch component
+ * 
+ * Creates a Ext.button.Cycle to allow the user to quickly change the background
+ * layer without the complexity of a TOC component.
+ * 
  * @author <a href="mailto:meinetoonen@b3partners.nl">Meine Toonen</a>
  */
 Ext.define ("viewer.components.LayerSwitch",{
     extend: "viewer.components.Component",
     container: null,
-    states:null,
-    layers:null,
-    levels:null,
-    prev:null,
-    appLayers:null,
-    button:null,
+    
+    items: null,
+    selectedBackgroundLevels: null,
+    control: null,
+    
     config: {
         top:null,
         left:null
     },
+    
     constructor: function (conf){        
         viewer.components.LayerSwitch.superclass.constructor.call(this, conf);
         this.initConfig(conf);
-        this.prev = new Array();
         this.loadComponent();
+        
+        this.viewerController.on(viewer.viewercontroller.controller.Event.ON_SELECTEDCONTENT_CHANGE, function() {
+            this.loadComponent();
+        }, this);
+        
+        this.viewerController.mapComponent.getMap().on(viewer.viewercontroller.controller.Event.ON_LAYER_VISIBILITY_CHANGED, 
+            this.layerVisibilityChanged, this);
+        
         return this;
     },
+    
     loadComponent : function (){
-        var p = Ext.get(this.div).parent();
-        var pid = p.id;
-        this.loadLayers();
-        
+        this.loadItems();
+        if(this.button) {
+            this.button.destroy();
+        }
         this.button = Ext.create('Ext.button.Cycle', {
             showText: true,
-            renderTo:  pid,
+            renderTo:  Ext.get(this.div).parent(),
            // width: "100%",
             top:  '30px',
             style: {
@@ -55,11 +67,11 @@ Ext.define ("viewer.components.LayerSwitch",{
             floating: true,
             menu: {
                 id: 'view-type-menu',
-                items: this.states
+                items: this.items
             },
             listeners:{
                 change:{
-                    fn: this.layerChanged,
+                    fn: this.controlItemChanged,
                     scope: this
                 }
             }
@@ -67,69 +79,119 @@ Ext.define ("viewer.components.LayerSwitch",{
         this.button.zIndexManager.bringToFront(this.button);
         this.button.setPosition(Number(this.left), Number(this.top));
     },
-    loadLayers : function(){
-        this.layers = new Array();
-        this.levels = this.viewerController.app.levels;
-        this.appLayers = this.viewerController.app.appLayers;
-        var backgroundRoot;
-        // Find root background
-        for ( var i in this.levels){
-            var level = this.levels[i];
-            if(level.background && !level.layers){
-                backgroundRoot = level;
-                break;
-            }
-        }
-        this.states = new Array();
-        this.addLevel(backgroundRoot);
-        
+    
+    levelItemId: function(level) {
+        return this.name + "_l" + level.id;
     },
-    addLevel : function (level){
-        if(!level.background || level.layers){
-            this.states.push({
+    
+    levelFromItemId: function(id) {
+        var levelId = Number(id.substring(this.name.length+"_i".length));
+        var l = null;
+        Ext.each(this.selectedBackgroundLevels, function(level) {
+            if(level.id == levelId) {
+                l = level;
+                return false;
+            }
+            return true;
+        });
+        return l;
+    },
+    
+    loadItems: function() {
+        var me = this;
+        
+        // Find the (singleton) level which contains all background levels
+        // as children to put in the following array:
+        var allBackgroundLevelIds = [];        
+        Ext.iterate(this.viewerController.app.levels, function(id, level) {
+            if(level.background) {
+                /* String array to Number array */
+                allBackgroundLevelIds = Ext.Array.map(level.children, function(s) { return Number(s); });
+                return false;
+            }
+            return true;
+        });
+        
+        // Find out which background levels are in the selected content
+        me.selectedBackgroundLevels = [];
+        this.viewerController.traverseSelectedContent(function(level) {
+            if(Ext.Array.contains(allBackgroundLevelIds, level.id)) {
+                me.selectedBackgroundLevels.push(level);
+            }
+        }, Ext.emptyFn);
+
+        // Create control items
+        me.items = [];
+        var checkedBackgroundLevel = this.getCheckedBackgroundLevel();
+        Ext.each(me.selectedBackgroundLevels, function(level) {
+            me.items.push({
+                id: me.levelItemId(level),
                 text: level.name,
-                iconCls: 'view-html'
+                iconCls: 'view-text',
+                checked: level == checkedBackgroundLevel
+            });
+        });
+    },
+
+    /**
+     * Assume only one background level is 'checked' by checking if any of the
+     * layers in the level is checked. 
+     */
+    getCheckedBackgroundLevel: function() {
+        var me = this;
+        var foundLevel = null;
+        Ext.each(this.selectedBackgroundLevels, function(level) {
+            Ext.each(level.layers, function(appLayerId) {
+                if(me.viewerController.getAppLayerById(appLayerId).checked) {
+                    foundLevel = level;
+                    return false;
+                }
+                return true;
+            });
+            return foundLevel == null;
+        });
+        return foundLevel;
+    },
+    
+    controlItemChanged: function(control, item) {
+        var me = this;
+        // XXX either change background when layers initialized or only enable
+        // control when layers initialized
+        if(this.viewerController.layersInitialized) {
+            var selectedLevel = me.levelFromItemId(item.id);
+            Ext.each(this.selectedBackgroundLevels, function(level) {
+                var checked = level == selectedLevel;
+                Ext.each(level.layers, function(appLayerId) {
+                    var appLayer = me.viewerController.getAppLayerById(appLayerId);
+                    me.viewerController.setLayerVisible(appLayer, checked);                    
+                });
             });
         }
-        if(level.layers){
-            if(this.layers[level.name] == undefined ){
-                this.layers[level.name] = new Array();
+    },
+    
+    layerVisibilityChanged: function(map, event) {
+
+        var eventLevel = this.viewerController.getAppLayerParent(event.layer.id);
+        var backgroundLevel = null;
+        Ext.each(this.selectedBackgroundLevels, function(level) {
+            if(level == eventLevel) {
+                backgroundLevel = level;
+                return false;
             }
-            this.addLayers(level.layers,level)
+            return true;
+        });
+        
+        if(backgroundLevel == null) {
+            return;
         }
-        if(level.children){
-            for(var i = 0 ; i < level.children.length ;i++){
-                var l = this.levels[level.children[i]];
-                this.addLevel(l);
-            }
+
+        if(event.visible) {
+            var itemId = this.levelItemId(backgroundLevel);
+            var activeItem = this.button.menu.items.getByKey(itemId);
+            this.button.setActiveItem(activeItem, true);
         }
     },
-    addLayers : function (layers, level){
-        for( var i = 0 ; i < layers.length ;i++){
-            var appLayerObj = this.appLayers[layers[i]];
-            if(appLayerObj.checked){
-                this.prev.push( appLayerObj );
-            }
-            this.layers[level.name].push(appLayerObj);
-        }
-    },
-    layerChanged : function (button, active){
-        if (this.viewerController.layersInitialized){
-            var levelName = active.text;
-            var layers = this.layers[levelName];
-            this.setLayersActive(layers, true);
-            if(this.prev != null){
-                this.setLayersActive(this.prev,false);
-            }
-            this.prev = layers;
-        }
-    },
-    setLayersActive : function (layerArray, visible){
-         for( var i = 0 ; i < layerArray.length ;i++){
-            var appLayerObj = layerArray[i];
-            this.viewerController.setLayerVisible(appLayerObj, visible);
-        }
-    },
+    
     getExtComponents: function() {
         return [this.button];
     }
