240972efba549bb8bfa5b9c57186d03689101595
Roy Braam <braam.roy@gmail.com>
2013-08-01 15:50:34 +0200

    use next on sequence to fill the id of the metadata update script for oracle

--- viewer-config-persistence/database/schema-updates/20ora_add_metadata.sql
+++ viewer-config-persistence/database/schema-updates/20ora_add_metadata.sql
@@ -4,6 +4,7 @@ create table metadata (
     config_value varchar2(255 char),
     primary key (id)
 );
+create sequence metadata_id_seq;
 
-insert into metadata(config_key,config_value)
-values('database_version','0');
+insert into metadata(id,config_key,config_value)
+values(metadata_id_seq.nextval,'database_version','0');
