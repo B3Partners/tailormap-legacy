b87ff7d518afb5612e2f4d33a0073195cd8b756d
Roy Braam <braam.roy@gmail.com>
2013-08-21 11:27:26 +0200

    Made the searchClient test a offline test.

--- viewer/src/test/java/nl/b3p/viewer/search/SearchClientTest.java
+++ viewer/src/test/java/nl/b3p/viewer/search/SearchClientTest.java
@@ -5,7 +5,13 @@ package nl.b3p.viewer.search;
  * and open the template in the editor.
  */
 
-import nl.b3p.viewer.search.OpenLSSearchClient;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import nl.b3p.viewer.HttpTestSupport;
 import org.json.JSONArray;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -18,15 +24,25 @@ import static org.junit.Assert.*;
  *
  * @author Roy Braam
  */
-public class SearchClientTest {
+public class SearchClientTest extends HttpTestSupport{
     private OpenLSSearchClient ols;
-    
     public SearchClientTest() {
-        
+        //super();
+        HttpHandler handler = new HttpHandler(){
+            @Override
+            public void handle(HttpExchange ex) throws IOException{                
+                byte[] response = "<xls:GeocodeResponse xmlns:xls=\"http://www.opengis.net/xls\" xmlns:gml=\"http://www.opengis.net/gml\"><xls:GeocodeResponseList numberOfGeocodedAddresses=\"1\"><xls:GeocodedAddress><gml:Point srsName=\"EPSG:28992\"><gml:pos dimension=\"2\">233818.478 582036.58</gml:pos></gml:Point><xls:Address countryCode=\"NL\"><xls:StreetAddress><xls:Street>Grote Markt</xls:Street></xls:StreetAddress><xls:Place type=\"MunicipalitySubdivision\">Groningen</xls:Place><xls:Place type=\"Municipality\">Groningen</xls:Place><xls:Place type=\"CountrySubdivision\">Groningen</xls:Place></xls:Address></xls:GeocodedAddress></xls:GeocodeResponseList></xls:GeocodeResponse>".getBytes();
+                ex.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length);
+                ex.getResponseBody().write(response);
+                ex.close();
+            }
+        };
+        httpServer.createContext("/geocoder/Geocoder",handler);
     }
     
     @BeforeClass
     public static void setUpClass() {
+        
     }
     
     @AfterClass
@@ -35,7 +51,7 @@ public class SearchClientTest {
     
     @Before
     public void setUp() {
-        ols = new OpenLSSearchClient("http://geodata.nationaalgeoregister.nl/geocoder/Geocoder?zoekterm=","GET");        
+        ols = new OpenLSSearchClient("http://localhost:8888/geocoder/Geocoder?zoekterm=","GET");        
     }
     
     @After
@@ -43,7 +59,7 @@ public class SearchClientTest {
     }
     
     @Test
-    public void search(){        
+    public void searchOpenLs(){        
         JSONArray result = ols.search("grote+markt+groningen");
         assertTrue(result.length()==1);
     }    
