906d313479cca4d8d3cabf217cc61f70d5bec9af
Meine Toonen <toonen.meine@gmail.com>
2013-12-30 10:02:01 +0100

    Missing tables added

--- viewer-config-persistence/src/main/resources/scripts/postgresql-add_solr_config.sql
+++ viewer-config-persistence/src/main/resources/scripts/postgresql-add_solr_config.sql
@@ -1,4 +1,12 @@
 
+    create table solr_configuration (
+        id  bigserial not null,
+        last_updated timestamp,
+        name varchar(255),
+        simple_feature_type int8,
+        primary key (id)
+    );
+
     create table solr_configuration_index_attributes (
         solr_configuration int8 not null,
         attribute_ int8 not null
@@ -13,6 +21,11 @@
         add constraint FK83A3760A893D7FEC 
         foreign key (solr_configuration) 
         references solr_configuration;
+
+    alter table solr_configuration 
+        add constraint FKB8F008594AC66EED 
+        foreign key (simple_feature_type) 
+        references feature_type;
 
     alter table solr_configuration_index_attributes 
         add constraint FK83A3760A91241D0D 
