6eb7dd0255494b48d4a294504f8e45a50acaad28
Meine Toonen <toonen.meine@gmail.com>
2014-01-15 12:07:44 +0100

    Functionality for zooming to the start extent instead of the max exent in the google navigation component. Fixes #145

--- viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/OpenLayersMapComponent.js
+++ viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/OpenLayersMapComponent.js
@@ -365,8 +365,39 @@ Ext.define("viewer.viewercontroller.OpenLayersMapComponent",{
             }if (config.left){
                 x = Number(config.left);
             }
-            comp = Ext.create("viewer.viewercontroller.openlayers.OpenLayersComponent",config,
-                new OpenLayers.Control.PanZoomBar({position: new OpenLayers.Pixel(x,y), zoomWorldIcon: true}));
+            var panZoom =  new OpenLayers.Control.PanZoomBar({position: new OpenLayers.Pixel(x,y), zoomWorldIcon:true});
+            if(config.zoomToFullIsStart){
+                var me = this;
+                function onButtonClick (evt) {
+                    var btn = evt.buttonElement;
+                    switch (btn.action) {
+                        case "panup": 
+                            this.map.pan(0, -this.getSlideFactor("h"));
+                            break;
+                        case "pandown": 
+                            this.map.pan(0, this.getSlideFactor("h"));
+                            break;
+                        case "panleft": 
+                            this.map.pan(-this.getSlideFactor("w"), 0);
+                            break;
+                        case "panright": 
+                            this.map.pan(this.getSlideFactor("w"), 0);
+                            break;
+                        case "zoomin": 
+                            this.map.zoomIn(); 
+                            break;
+                        case "zoomout": 
+                            this.map.zoomOut(); 
+                            break;
+                        case "zoomworld": 
+                            me.viewerController.mapComponent.getMap().zoomToExtent(me.viewerController.mapComponent.mapOptions.options.startExtent); 
+                            break;
+                    }
+                }
+                panZoom.onButtonClick = onButtonClick;
+            }
+            
+            comp = Ext.create("viewer.viewercontroller.openlayers.OpenLayersComponent",config,panZoom);
         }else if (type == viewer.viewercontroller.controller.Component.BORDER_NAVIGATION){
             comp = Ext.create("viewer.viewercontroller.openlayers.components.OpenLayersBorderNavigation",config);                
         }else if(type == viewer.viewercontroller.controller.Component.COORDINATES){            
