c1ad12d48d2cd63a3296a97d84aa5427c158ab17
Meine Toonen <toonen.meine@gmail.com>
2014-05-09 15:19:57 +0200

    Graph component retrieves data

--- viewer/src/main/webapp/viewer-html/components/Graph.js
+++ viewer/src/main/webapp/viewer-html/components/Graph.js
@@ -22,13 +22,15 @@
 Ext.define("viewer.components.Graph", {
     extend: "viewer.components.Component",
     panel: null,
-    initialized:false,
+    initialized: false,
+    toolMapClick:null,
+    deActivatedTools:null,
+    layers:null,
     config: {
         title: null,
         iconUrl: null,
         tooltip: null,
-        label: null,
-        graphs:null
+        graphs: null
     },
     constructor: function(conf) {
         viewer.components.Graph.superclass.constructor.call(this, conf);
@@ -36,7 +38,7 @@ Ext.define("viewer.components.Graph", {
         var me = this;
         this.renderButton({
             handler: function() {
-                // me.buttonClick();
+                 me.buttonClick();
             },
             text: me.title,
             icon: me.iconUrl,
@@ -45,34 +47,303 @@ Ext.define("viewer.components.Graph", {
         });
         // Make hook for Returned feature infos
         // Stub for development
-        this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_LAYERS_INITIALIZED, this.initialize,this);
-        //this.
-        // Make hook for onaddlayers
-        this.getViewerController().mapComponent.getMap().addListener(viewer.viewercontroller.controller.Event.ON_LAYER_ADDED,this.addLayer,this);
+        this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_LAYERS_INITIALIZED, this.initialize, this);
         return this;
     },
-    initialize : function(){
+    initialize: function() {
         this.initialized = true;
-        var layer = this.viewerController.mapComponent.getMap().getLayer(3);// gemeente
+            
+        this.toolMapClick =  this.viewerController.mapComponent.createTool({
+            type: viewer.viewercontroller.controller.Tool.MAP_CLICK,
+            id: this.name + "toolMapClick",
+            handler:{
+                fn: this.mapClicked,
+                scope:this
+            },
+            viewerController: this.viewerController
+        });
         
-        
-        this.addLayer(null,{layer:layer});
+        this.layers = new Array();
+        for (var i = 0 ; i < this.graphs.length ;i ++){
+            var graph = this.graphs[i];
+            this.layers.push(graph.layer);
+        }
+        this.loadWindow();
     },
-    addLayer : function(layer,options){
-        if (this.initialized) {
-            var mapLayer = options.layer;
-            mapLayer.addListener(viewer.viewercontroller.controller.Event.ON_GET_FEATURE_INFO_DATA, this.featureInfoReturned, this);
+     loadWindow : function (){
+        var me =this;
+        this.maincontainer = Ext.create('Ext.container.Container', {
+            id: this.name + 'Container',
+            width: '100%',
+            height: '100%',
+            autoScroll: true,
+            layout: {
+                type: 'vbox'
+            },
+            style: {
+                backgroundColor: 'White'
+            },
+            renderTo: this.getContentDiv(),
+            items: [
+                {
+                    id: this.name + 'LayerSelectorPanel',
+                    xtype: "container",
+                    padding: "4px",
+                    width: '100%',
+                    height: 36
+                },
+                {
+                    xtype: 'button',
+                    text: 'Selecteer punt voor grafiek',
+                    listeners: {
+                        click: {
+                            scope: me,
+                            fn: me.activateMapClick
+                        }
+                    }
+
+                }
+            ]
+        });
+        this.createLayerSelector();
+    },
+    createLayerSelector: function(){
+        var config = {
+            viewerController : this.viewerController,
+            restriction : "attribute",
+            id : this.name + "layerSelector",
+            layers: this.layers,
+            div: this.name + 'LayerSelectorPanel'
+        };
+        this.layerSelector = Ext.create("viewer.components.LayerSelector",config);
+        this.layerSelector.addListener(viewer.viewercontroller.controller.Event.ON_LAYERSELECTOR_CHANGE,this.layerChanged,this);  
+        if(this.layers.length === 1){
+          //  this.layerSelector.setValue(this.layers[0]);
         }
     },
-    featureInfoReturned : function (layer,options){
+    layerChanged : function (layer){
+      var a = 0;  
+    },
+            
+    mapClicked : function(tool, comp){
+       this.deactivateMapClick();
+        //Ext.get(this.getContentDiv()).mask("Haalt features op...")
+        var coords = comp.coord;
+        var x = coords.x;
+        var y = coords.y;
+        
+        var appLayer = this.layerSelector.getValue();
+        var graphConfig = this.getConfigByAppLayer(appLayer.id)
+        var attributes = new Array();
+        if(!graphConfig){
+            return;
+        }
+        attributes.push(graphConfig.serieAttribute);
+        attributes.push(graphConfig.categoryAttribute);
+        
+        var extraParams = {
+            attributesToInclude : attributes,
+            graph:true
+        };
+        this.viewerController.mapComponent.getMap().setMarker("edit",x,y);
+        var featureInfo = Ext.create("viewer.FeatureInfo", {
+            viewerController: this.viewerController
+        });
+        var me =this;
+        featureInfo.editFeatureInfo(x,y,this.viewerController.mapComponent.getMap().getResolution() * 4,appLayer, function (features){
+            me.featuresReceived(features, attributes);
+        },function(msg){me.failed(msg);},extraParams);
+    },
+    featuresReceived : function (features,attributes){
+        var json = new Object();
+        for (var i = 0 ; i < features.length ;i++){
+            var feature = features[i];
+            json = feature;
+            if(json.related_featuretypes){
+                for (var j = 0 ; j < json.related_featuretypes.length ;j++){
+                    var linked = this.getLinkedData(json.related_featuretypes[i],attributes);
+                    json.linkedData = linked;
+                    break;
+                }
+            }
+        }
+        var a =0;
+    },
+    getLinkedData : function (related_feature,attributes){
+        var appLayer = this.layerSelector.getValue();
+        var options = {};
+         
+        var filter = "&filter="+encodeURIComponent(related_feature.filter);
+        
+        var featureType="&featureType="+related_feature.id;
+                
+        options.application = this.appId;
+        options.appLayer = appLayer.id;
+        options.limit = 1000;
+        options.filter = filter;
+        options.graph = true;
+        options.arrays = false;
+        options.attributesToInclude = attributes;
+        Ext.Ajax.request({
+            url: appLayer.featureService.getStoreUrl() + featureType+filter,
+            params: options,
+            scope: this,
+            success: function(result) {
+                var response = Ext.JSON.decode(result.responseText);
+                var a =0;
+            },
+            failure: function(result) {
+               var b =0;
+            }
+        });
+    },
+    buttonClick: function(){
+        this.popup.show();
+        
+    },
+    featureInfoReturned: function(layer, options) {
         this.loadGraph(layer);
     },
-    loadGraph : function (appLayer){
+    loadGraph: function(appLayer) {
         this.popup.show();
         this.popup.setWindowTitle(appLayer.alias);
-        // Create store
-        // Create graph
+        var featureService = appLayer.featureService;
+        if (featureService) {
+
+            // Create store
+            // Create graph
+            //  loadFeatures: function(appLayer, successFunction, failureFunction,options,scope) {
+            var filter;
+            var me = this;
+            if (appLayer.attributes === undefined) {
+                featureService.loadAttributes(me.appLayer, function(attributes) {
+                    me.loadData(appLayer);
+                });
+            } else {
+                this.loadData(appLayer);
+            }
+        } else {
+            this.viewerController.logger.error("No featureservice available for layer " + appLayer.alias);
+        }
+
+    },
+    loadData: function(appLayer) {
+        var featureService = appLayer.featureService;
+        /*
+        var modelName = 'Masdfodel';
+        var visCols = {
+            "AANTAL_HH":true
+        };
+        var attributes = this.viewerController.getAttributesFromAppLayer(appLayer);
+        var attributeList = new Array();
+        var columns = new Array();
+        var index = 0;
+        for(var i= 0 ; i < attributes.length ;i++){
+            var attribute = attributes[i];
+            var colName = attribute.alias != undefined ? attribute.alias : attribute.name;
+            if(visCols.hasOwnProperty(colName)){
+                
+                var attIndex = index++;
+                
+                attributeList.push({
+                    name: "c" + attIndex,
+                    type : 'string'
+                });
+                columns.push({
+                    id: "c"+name+ +attIndex,
+                    header:colName,
+                    dataIndex: "c" + attIndex,
+                    flex: 1,
+                    filter: {
+                        xtype: 'textfield'
+                    }
+                });
+            }
+        }
+        var name = appLayer.alias;
+        var modelName= name + 'Model';
+        Ext.define(modelName, {
+            extend: 'Ext.data.Model',
+            fields: attributeList
+        });
         
+        var filter = "";
+        
+        if (appLayer.filter){
+            filter=appLayer.filter.getCQL();
+        }
+        var featureType = "";
+       
+
+        var store = Ext.create('Ext.data.Store', {
+            storeId: name + "Store",
+            pageSize: 10,
+            model: modelName,
+            remoteSort: true,
+            remoteFilter: true,
+            proxy: {
+                type: 'ajax',
+                timeout: 40000,
+                url: appLayer.featureService.getStoreUrl() + "&arrays=1" + featureType + filter,
+                reader: {
+                    type: 'json',
+                    root: 'features',
+                    totalProperty: 'total'
+                },
+                simpleSortMode: true,
+                listeners: {
+                    exception: function(store, response, op) {
+
+                        msg = response.responseText;
+                        if (response.status == 200) {
+                            try {
+                                var j = Ext.JSON.decode(response.responseText);
+                                if (j.message) {
+                                    msg = j.message;
+                                }
+                            } catch (e) {
+                            }
+                        }
+
+                        if (msg == null) {
+                            if (response.timedout) {
+                                msg = "Request timed out";
+                            } else if (response.statusText != null && response.statusText != "") {
+                                msg = response.statusText;
+                            } else {
+                                msg = "Unknown error";
+                            }
+                        }
+
+                        Ext.getCmp(me.name + "mainGrid").getStore().removeAll();
+
+                        Ext.MessageBox.alert("Foutmelding", msg);
+
+                    }
+                }
+            },
+            autoLoad: true
+        });*/
+    },
+    
+    activateMapClick: function(){
+        this.deActivatedTools = this.viewerController.mapComponent.deactivateTools();
+        this.toolMapClick.activateTool();
+    },
+    deactivateMapClick: function(){
+        for (var i=0; i < this.deActivatedTools.length; i++){
+            this.deActivatedTools[i].activate();
+        }
+        this.deActivatedTools = [];
+        this.toolMapClick.deactivateTool();
+    },
+    getConfigByAppLayer : function (appLayerId){
+        for (var i = 0 ; i < this.graphs.length ;i++){
+            if(this.graphs[i].layer === appLayerId){
+                return this.graphs[i];
+            }
+        }
+        return null;
     },
     getExtComponents: function() {
         return [];
