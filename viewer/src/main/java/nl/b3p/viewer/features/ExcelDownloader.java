0563623df27e165740b723e13f117d8f67d05ced
Meine Toonen <toonen.meine@gmail.com>
2014-06-18 14:48:15 +0200

    Removed unused styles

--- viewer/src/main/java/nl/b3p/viewer/features/ExcelDownloader.java
+++ viewer/src/main/java/nl/b3p/viewer/features/ExcelDownloader.java
@@ -137,91 +137,9 @@ public class ExcelDownloader implements FeatureDownloader{
         styles.put("header", style);
 
         style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_CENTER);
-        style.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
-        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-        style.setFont(headerFont);
-        style.setDataFormat(df.getFormat("d-mmm"));
-        styles.put("header_date", style);
-
-        Font font1 = wb.createFont();
-        font1.setBoldweight(Font.BOLDWEIGHT_BOLD);
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_LEFT);
-        style.setFont(font1);
-        styles.put("cell_b", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_CENTER);
-        style.setFont(font1);
-        styles.put("cell_b_centered", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_RIGHT);
-        style.setFont(font1);
-        style.setDataFormat(df.getFormat("d-mmm"));
-        styles.put("cell_b_date", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_RIGHT);
-        style.setFont(font1);
-        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-        style.setDataFormat(df.getFormat("d-mmm"));
-        styles.put("cell_g", style);
-
-        Font font2 = wb.createFont();
-        font2.setColor(IndexedColors.BLUE.getIndex());
-        font2.setBoldweight(Font.BOLDWEIGHT_BOLD);
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_LEFT);
-        style.setFont(font2);
-        styles.put("cell_bb", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_RIGHT);
-        style.setFont(font1);
-        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-        style.setDataFormat(df.getFormat("d-mmm"));
-        styles.put("cell_bg", style);
-
-        Font font3 = wb.createFont();
-        font3.setFontHeightInPoints((short)14);
-        font3.setColor(IndexedColors.DARK_BLUE.getIndex());
-        font3.setBoldweight(Font.BOLDWEIGHT_BOLD);
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_LEFT);
-        style.setFont(font3);
-        style.setWrapText(true);
-        styles.put("cell_h", style);
-
-        style = createBorderedStyle(wb);
         style.setAlignment(CellStyle.ALIGN_LEFT);
         style.setWrapText(true);
         styles.put("cell_normal", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_CENTER);
-        style.setWrapText(true);
-        styles.put("cell_normal_centered", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_RIGHT);
-        style.setWrapText(true);
-        style.setDataFormat(df.getFormat("d-mmm"));
-        styles.put("cell_normal_date", style);
-
-        style = createBorderedStyle(wb);
-        style.setAlignment(CellStyle.ALIGN_LEFT);
-        style.setIndention((short)1);
-        style.setWrapText(true);
-        styles.put("cell_indented", style);
-
-        style = createBorderedStyle(wb);
-        style.setFillForegroundColor(IndexedColors.BLUE.getIndex());
-        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-        styles.put("cell_blue", style);
 
         return styles;
     }
