b4d9d486771a69c80bbe511832da844234106602
Meine Toonen <toonen.meine@gmail.com>
2014-06-30 13:21:59 +0200

    First setup of SpatialFilter component

---
+++ viewer/src/main/webapp/viewer-html/components/SpatialFilter.js
@@ -0,0 +1,209 @@
+/* 
+ * Copyright (C) 2012-2013 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Edit component
+ * @author <a href="mailto:meinetoonen@b3partners.nl">Meine Toonen</a>
+ */
+Ext.define ("viewer.components.SpatialFilter",{
+    extend: "viewer.components.Component",
+    layerSelector:null,
+    drawingButtonIds:null,
+    iconPath:null,
+    config:{
+        title: "",
+        iconUrl: "",
+        tooltip: "",
+        layers:null,
+        label: ""
+    },
+    constructor: function (conf){        
+        viewer.components.SpatialFilter.superclass.constructor.call(this, conf);
+        this.initConfig(conf);     
+        var me = this;
+        this.renderButton({
+            handler: function(){
+                me.showWindow();
+            },
+            text: me.title,
+            icon: me.iconUrl,
+            tooltip: me.tooltip,
+            label: me.label
+        });
+        // Needed to untoggle the buttons when drawing is finished
+        this.drawingButtonIds = {
+            'polygon': Ext.id(),
+            'circle': Ext.id()
+        };
+        this.iconPath=contextPath+"/viewer-html/components/resources/images/drawing/";
+     
+   
+        this.loadWindow(); 
+        this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_SELECTEDCONTENT_CHANGE,this.selectedContentChanged,this );
+        return this;
+    },
+    selectedContentChanged : function (){
+       /* if(this.vectorLayer == null){
+            this.createVectorLayer();
+        }else{
+            this.viewerController.mapComponent.getMap().addLayer(this.vectorLayer);
+        }*/
+    },
+    createVectorLayer : function (){
+        /* this.vectorLayer=this.viewerController.mapComponent.createVectorLayer({
+            name: this.name + 'VectorLayer',
+            geometrytypes:["Circle","Polygon","MultiPolygon","Point", "LineString"],
+            showmeasures:false,
+            viewerController : this.viewerController,
+            style: {
+                fillcolor: "FF0000",
+                fillopacity: 50,
+                strokecolor: "FF0000",
+                strokeopacity: 50
+            }
+        });
+        this.viewerController.mapComponent.getMap().addLayer(this.vectorLayer);*/
+    },
+    showWindow : function(){
+       /* if(this.vectorLayer == null){
+            this.createVectorLayer();
+        }
+        this.layerSelector.initLayers();
+        this.popup.popupWin.setTitle(this.title);*/
+        this.popup.show();
+    },
+    loadWindow : function (){
+        var me =this;
+        var drawingItems = [
+        
+        {
+            xtype: 'button',
+            id: this.drawingButtonIds.polygon,
+            icon: this.iconPath+"shape_square_red.png",
+            componentCls: 'mobileLarge',
+            tooltip: "Teken een polygoon",
+            enableToggle: true,
+            toggleGroup: 'drawingTools',
+            listeners: {
+                click:{
+                    scope: me,
+                    fn: me.drawPolygon
+                }
+            }
+        }];
+        if(!MobileManager.isMobile()) {
+            drawingItems.push({
+                xtype: 'button',
+                id: this.drawingButtonIds.circle,
+                icon: this.iconPath+"shape_circle_red.png",
+                componentCls: 'mobileLarge',
+                tooltip: "Teken een cirkel",
+                enableToggle: true,
+                toggleGroup: 'drawingTools',
+                listeners: {
+                    click:{
+                        scope: me,
+                        fn: me.drawCircle
+                    }
+                }
+            });
+        }
+        
+        this.maincontainer = Ext.create('Ext.container.Container', {
+            id: this.name + 'Container',
+            width: '100%',
+            height: '100%',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            style: {
+                backgroundColor: 'White'
+            },
+            renderTo: this.getContentDiv(),
+            items: [{
+                id: this.name + 'LayerSelectorPanel',
+                xtype: "container",
+                padding: "4px",
+                width: '100%',
+                height: 36
+            },{
+                id: this.name + 'filterButtons',
+                xtype: "container",
+                autoScroll: true,
+                width: '100%',
+                flex: 1,
+                items: drawingItems
+            },{
+                id: this.name + 'ClosingPanel',
+                xtype: "container",
+                width: '100%',
+                height: MobileManager.isMobile() ? 45 : 25,
+                style: {
+                    marginTop: '10px'
+                },
+                layout: {
+                    type:'hbox',
+                    pack:'end'
+                },
+                items: [
+                    {xtype: 'button', text: 'Sluiten', componentCls: 'mobileLarge', handler: function() {
+                        me.popup.hide();
+                    }}
+                ]
+            }]
+        });
+        this.createLayerSelector();
+    },
+    createLayerSelector: function(){
+        var config = {
+            viewerController : this.viewerController,
+            restriction : "filterable",
+            id : this.name + "layerSelector",
+            layers: this.layers,
+            div: this.name + 'LayerSelectorPanel'
+        };
+        this.layerSelector = Ext.create("viewer.components.LayerSelector",config);
+        this.layerSelector.addListener(viewer.viewercontroller.controller.Event.ON_LAYERSELECTOR_CHANGE,this.layerChanged,this);  
+    },
+            
+    layerChanged : function (appLayer,afterLoadAttributes,scope){
+       /* if(appLayer != null){
+            this.vectorLayer.removeAllFeatures();
+            this.mode=null;
+            this.viewerController.mapComponent.getMap().removeMarker("edit");
+            if(appLayer.details && appLayer.details["editfunction.title"]){
+                this.popup.popupWin.setTitle(appLayer.details["editfunction.title"]);
+            }
+          
+            this.loadAttributes(appLayer,afterLoadAttributes,scope);
+        }else{
+            this.cancel();
+        }*/
+    },
+    resetForm : function (){
+        Ext.getCmp(this.name +"editButton").setDisabled(true);
+        Ext.getCmp(this.name +"newButton").setDisabled(true);
+        this.mode=null;
+        this.layerSelector.combobox.select(null);
+        Ext.getCmp( this.name +"geomLabel").setText("");
+        this.viewerController.mapComponent.getMap().removeMarker("edit");
+        this.vectorLayer.removeAllFeatures();
+    },
+    getExtComponents: function() {
+        return [ this.maincontainer.getId() ];
+    }
+});
