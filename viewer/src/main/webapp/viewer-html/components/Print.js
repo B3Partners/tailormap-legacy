eeec8158e93546818019b9e99b4b90fd4c33994b
Meine Toonen <toonen.meine@gmail.com>
2014-07-03 15:33:40 +0200

    Print.js can be compiled/minified again

--- viewer/src/main/webapp/viewer-html/components/Print.js
+++ viewer/src/main/webapp/viewer-html/components/Print.js
@@ -692,7 +692,7 @@ Ext.define ("viewer.components.Print",{
         for (var i = 0 ; i < this.extraInfoCallbacks.length ; i++){
             var entry = this.extraInfoCallbacks[i];
             var extraInfo = {
-                class: entry.component.$className,
+                class: Ext.getClass(entry.component).getName(),
                 componentName: entry.component.name,
                 info: entry.callback() // Produces an JSONObject
             };
