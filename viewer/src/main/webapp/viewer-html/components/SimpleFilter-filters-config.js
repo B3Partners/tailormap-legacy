8a86bbb92c89fa749a360de9331efea59c16db4e
Meine Toonen <toonen.meine@gmail.com>
2014-09-30 12:20:24 +0200

    Refactoring configs: separate global funcitonality to superclass

--- viewer/src/main/webapp/viewer-html/components/SimpleFilter-filters-config.js
+++ viewer/src/main/webapp/viewer-html/components/SimpleFilter-filters-config.js
@@ -15,13 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-Ext.define("viewer.components.sf.SliderConfig", {
+Ext.define("viewer.components.sf.Config", {
     configObject: null,
     id: null,
     form: null,
-    constructor: function(config) {
+    constructor: function (config) {
         this.configObject = config.configObject;
         this.id = this.configObject.id ? this.configObject.id : Ext.id();
+        var items = this.getFormItems();
         this.form = Ext.create("Ext.form.Panel", {
             title: 'Slider',
             width: 325,
@@ -31,88 +32,106 @@ Ext.define("viewer.components.sf.SliderConfig", {
                 anchor: '100%'
             },
             defaultType: 'textfield',
-            items: [{
-                fieldLabel: 'Label',
-                name: 'label',
-                value: this.configObject.label ? this.configObject.label : ""
-            },{
-                fieldLabel: "Minimale waarde",
-                name: "min",
-                qtip: "Indien geen waarde ingevuld wordt kleinste attribuutwaarde uit de attribuutlijst gebruikt",
-                value: this.configObject.min ? this.configObject.min : "",
-                listeners: {
-                    render: function(c) {
-                        Ext.QuickTips.register({
-                            target: c.getEl(),
-                            text: c.qtip
-                        });
-                    }
-                }
-            },{
-                fieldLabel: "Maximale waarde",
-                name: "max",
-                value: this.configObject.max ? this.configObject.max : "",
-                qtip: "Indien geen waarde ingevuld wordt grootste attribuutwaarde uit de attribuutlijst gebruikt",
-                listeners: {
-                    render: function(c) {
-                        Ext.QuickTips.register({
-                            target: c.getEl(),
-                            text: c.qtip
-                        });
-                    }
-                }
-            },{
-                fieldLabel: "Beginwaarde(s)",
-                name: "start",
-                value: this.configObject.start ? this.configObject.start : "min,max",
-                qtip: "Vul een vaste waarde in of 'min' of 'max'. Bij een slider voor een bereik geef twee waardes op gescheiden door een komma",
-                listeners: {
-                    render: function(c) {
-                        Ext.QuickTips.register({
-                            target: c.getEl(),
-                            text: c.qtip
-                        });
-                    }
-                }
-            },{
-                fieldLabel: "Stap",
-                name: "step",
-                value: this.configObject.step ? this.configObject.step : "1"
-            },{
-                xtype: 'combo',
-                fieldLabel: "Soort slider",
-                name: "sliderType",
-                store: Ext.create("Ext.data.Store", {
-                    fields: ["type", "label"],
-                    data: [
-                        {type: "eq", label: "Attribuut gelijk aan waarde slider"},
-                        {type: "gt", label: "Attribuut groter dan waarde slider"},
-                        {type: "lt", label: "Attribuut kleiner dan waarde slider"},
-                        {type: "range", label: "Attribuut binnen bereik (twee schuifjes)"}
-                    ]
-                }),
-                queryModes: "local",
-                displayField: "label",
-                editable: false,
-                valueField: "type",
-                value: this.configObject.sliderType ? this.configObject.sliderType : "eq"
-            },{
-                fieldLabel: "Waarde format string",
-                name: "valueFormatString",
-                value: this.configObject.valueFormatString ? this.configObject.valueFormatString : "",
-                qtip: "Laat leeg om geen waarde van de schuifjes te tonen. Voorbeelden format strings: '0' (alleen hele getallen), '0 m²' (met eenheid), '0.00' (twee decimalen), '0,000' (met duizendtalscheidingsteken), '€ 0,000.00' (bedrag)",
-                listeners: {
-                    render: function(c) {
-                        Ext.QuickTips.register({
-                            target: c.getEl(),
-                            text: c.qtip
-                        });
-                    }
-                }
-            }]
+            items: items
         });
 
         Ext.getCmp(config.renderTo).items.add(this.form);
+    },
+    getFormItems: function () {
+        var items = [{
+            fieldLabel: 'Label',
+            name: 'label',
+            value: this.configObject.label ? this.configObject.label : ""
+        }, {
+            fieldLabel: "Beginwaarde(s)",
+            name: "start",
+            value: this.configObject.start ? this.configObject.start : "min,max",
+            qtip: "Vul een vaste waarde in of 'min' of 'max'. Bij een slider voor een bereik geef twee waardes op gescheiden door een komma",
+            listeners: {
+                render: function (c) {
+                    Ext.QuickTips.register({
+                        target: c.getEl(),
+                        text: c.qtip
+                    });
+                }
+            }
+        }];
+
+        return items;
+    }
+
+});
+
+Ext.define("viewer.components.sf.SliderConfig", {
+    extend: "viewer.components.sf.Config",
+    constructor: function(config) {
+        viewer.components.sf.SliderConfig.superclass.constructor.call(this, config);
+    },
+    getFormItems : function(){
+        var items = this.callParent();
+        items = items.concat([{
+            fieldLabel: "Minimale waarde",
+            name: "min",
+            qtip: "Indien geen waarde ingevuld wordt kleinste attribuutwaarde uit de attribuutlijst gebruikt",
+            value: this.configObject.min ? this.configObject.min : "",
+            listeners: {
+                render: function (c) {
+                    Ext.QuickTips.register({
+                        target: c.getEl(),
+                        text: c.qtip
+                    });
+                }
+            }
+        }, {
+            fieldLabel: "Maximale waarde",
+            name: "max",
+            value: this.configObject.max ? this.configObject.max : "",
+            qtip: "Indien geen waarde ingevuld wordt grootste attribuutwaarde uit de attribuutlijst gebruikt",
+            listeners: {
+                render: function (c) {
+                    Ext.QuickTips.register({
+                        target: c.getEl(),
+                        text: c.qtip
+                    });
+                }
+            }
+        }, {
+            fieldLabel: "Stap",
+            name: "step",
+            value: this.configObject.step ? this.configObject.step : "1"
+        }, {
+            xtype: 'combo',
+            fieldLabel: "Soort slider",
+            name: "sliderType",
+            store: Ext.create("Ext.data.Store", {
+                fields: ["type", "label"],
+                data: [
+                    {type: "eq", label: "Attribuut gelijk aan waarde slider"},
+                    {type: "gt", label: "Attribuut groter dan waarde slider"},
+                    {type: "lt", label: "Attribuut kleiner dan waarde slider"},
+                    {type: "range", label: "Attribuut binnen bereik (twee schuifjes)"}
+                ]
+            }),
+            queryModes: "local",
+            displayField: "label",
+            editable: false,
+            valueField: "type",
+            value: this.configObject.sliderType ? this.configObject.sliderType : "eq"
+        }, {
+            fieldLabel: "Waarde format string",
+            name: "valueFormatString",
+            value: this.configObject.valueFormatString ? this.configObject.valueFormatString : "",
+            qtip: "Laat leeg om geen waarde van de schuifjes te tonen. Voorbeelden format strings: '0' (alleen hele getallen), '0 m²' (met eenheid), '0.00' (twee decimalen), '0,000' (met duizendtalscheidingsteken), '€ 0,000.00' (bedrag)",
+            listeners: {
+                render: function (c) {
+                    Ext.QuickTips.register({
+                        target: c.getEl(),
+                        text: c.qtip
+                    });
+                }
+            }
+        }]);
+        return items;
     },
     getConfig: function() {
         var config = this.form.getValues();
