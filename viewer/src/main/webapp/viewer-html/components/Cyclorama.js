3f5b37ee0d89ec630f8a4eb1fd2768ed9c1c8d9d
Meine Toonen <toonen.meine@gmail.com>
2014-10-10 15:42:17 +0200

    Skeleton component created and registered event

--- viewer/src/main/webapp/viewer-html/components/Cyclorama.js
+++ viewer/src/main/webapp/viewer-html/components/Cyclorama.js
@@ -25,6 +25,33 @@ Ext.define ("viewer.components.Cyclorama",{
     constructor: function (conf){
         viewer.components.Cyclorama.superclass.constructor.call(this, conf);
         this.initConfig(conf);
+        // Registreer voor layerinitialized
+        this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_LAYERS_INITIALIZED, this.initComp, this);
         return this;
+    },
+    initComp : function(){
+        console.log("layers initialized");
+        var appLayer = this.viewerController.getAppLayerById(this.config.layers);
+        var mapLayer = this.viewerController.getLayer(appLayer);
+        mapLayer.addListener(viewer.viewercontroller.controller.Event.ON_GET_FEATURE_INFO_DATA, this.onFeatureInfo,this);
+    },
+    onFeatureInfo: function (appLayer,event){
+        if(appLayer.id === this.layers && event.features){
+            if(event.features.length >1 ){
+                this.showOptions(event.features);
+            }else if(event.features.length === 1){
+                this.openGlobespotter(event.features[0]);
+            }
+        }
+    },
+    showOptions : function(features){
+        // laat meerdere opties zien
+        // klik is open globespotter
+    },
+    openGlobespotter : function(feature){
+        // Get link from backend
+    },
+    linkReceived: function(link){
+        // Open popup
     }
 });
