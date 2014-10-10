be1d8655073d989a3f98508b38f07d9c974441e8
Meine Toonen <toonen.meine@gmail.com>
2014-10-10 10:58:15 +0200

    Removed copied code

--- viewer/src/main/webapp/viewer-html/components/Cyclorama-config.js
+++ viewer/src/main/webapp/viewer-html/components/Cyclorama-config.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2012-2013 B3Partners B.V.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -24,70 +24,6 @@ Ext.define("viewer.components.CustomConfiguration",{
         }
 
         var me=this;
-        this.form = new Ext.form.FormPanel({
-            url: 'Home/SubmitForm',
-            frame: false,
-            title: 'Configureer dit component',
-            bodyPadding: me.formPadding,
-            defaults: {
-                anchor: '100%'
-            },
-            width: me.formWidth,
-            items: [{ 
-                xtype: 'textfield',
-                fieldLabel: 'Tooltip',
-                name: 'tooltip',
-                value: config.tooltip,
-                labelWidth:me.labelWidth
-            
-            },{
-                xtype: 'checkbox',
-                fieldLabel: "Zet een marker na klikken",
-                inputValue: false,
-                name: 'useMarker',
-                checked: config.useMarker !== undefined ? config.useMarker : false,
-                value: false,
-                labelWidth: me.labelWidth
-            },{
-                xtype: 'checkbox',
-                fieldLabel:  "Ga na gebruik naar de eerste tool",
-                inputValue: false,
-                name: 'nonSticky',
-                checked: config.nonSticky !== undefined ? config.nonSticky : false,
-                value: false,
-                labelWidth: me.labelWidth
-            },{
-                xtype: 'checkbox',
-                fieldLabel: "Gebruik pop-up voor streetview",
-                inputValue: false,
-                name: 'usePopup',
-                checked: config.usePopup !== undefined ? config.usePopup : false,
-                value: false,
-                labelWidth: me.labelWidth,
-                listeners: {
-                    change: function(obj, val) {
-                        Ext.getCmp("popupHeight").setVisible(val);
-                        Ext.getCmp("popupWidth").setVisible(val);
-                    }
-                }
-            }, {
-                id: "popupHeight",
-                xtype: 'textfield',
-                fieldLabel: "Hoogte popup",
-                name: 'height',
-                hidden:  config.usePopup!==undefined ? !config.usePopup : true,
-                value: config.height!==undefined? config.height:"400",
-                labelWidth:me.labelWidth
-            },{
-                id: "popupWidth",
-                xtype: 'textfield',
-                fieldLabel: "Breedte popup",
-                name: 'width',
-                hidden:  config.usePopup!==undefined ? !config.usePopup : true,
-                value: config.width!==undefined? config.width:"400",
-                labelWidth:me.labelWidth
-            }],
-            renderTo: parentid
-        });      
+       
     }
 });
