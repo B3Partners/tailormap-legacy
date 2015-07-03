93f0804e37da96e63f0297e06a52c3a3dcd34aef
Matthijs Laan <matthijsln@gmail.com>
2015-07-03 12:25:52 +0200

    fix bookmark update script for oracle

--- viewer-config-persistence/src/main/resources/scripts/oracle-add_application_to_bookmark.sql
+++ viewer-config-persistence/src/main/resources/scripts/oracle-add_application_to_bookmark.sql
@@ -1,14 +1,16 @@
-alter table bookmark add column application int8;
-alter table bookmark add column id number(19,0);
-
+alter table bookmark add application number(19,0);
+alter table bookmark add id number(19,0);
 
     alter table bookmark 
         add constraint FK7787A5362A91FB84 
         foreign key (application) 
         references application;
-        
 
-ALTER TABLE bookmark
-  DROP CONSTRAINT bookmark_pkey;
-ALTER TABLE bookmark
-  ADD CONSTRAINT bookmark_pkey PRIMARY KEY (id);
+ALTER TABLE bookmark DROP primary key;
+
+create sequence bookmark_id_seq;
+
+update bookmark set id = bookmark_id_seq.nextval;
+
+ALTER TABLE bookmark ADD CONSTRAINT bookmark_pkey PRIMARY KEY (id);
+
