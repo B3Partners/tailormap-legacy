bafd3ff03c44a5c0d0513a495bcba4b52d042a01
Mark Prins <mark@b3partners.nl>
2015-07-10 12:52:23 +0200

    Add a getExtent function to Feature
    so the bbox may be retrieved using the Extent

--- viewer/src/main/webapp/viewer-html/common/viewercontroller/controller/Feature.js
+++ viewer/src/main/webapp/viewer-html/common/viewercontroller/controller/Feature.js
@@ -1,10 +1,9 @@
 /**
  * @class 
- * @constructor
  * @param id The id of the Feature
  * @param wkt The wkt describing the Feature
- * @description
- * The generic class for defining a feature. A feature consists of a id and a wkt. Convenience methods for converting from and to viewerspecific features.
+ * @description The generic class for defining a feature. A feature consists of a id and a wkt.
+ *           Convenience methods for converting from and to viewerspecific features.
  */
 Ext.define("viewer.viewercontroller.controller.Feature",{
     config:{
@@ -13,6 +12,10 @@ Ext.define("viewer.viewercontroller.controller.Feature",{
         color:null,
         label:null
     },
+    /**
+     * @param {Object} config
+     * @constructor
+     */
     constructor: function (config){
         this.initConfig(config);
         if(this.label == null){
@@ -20,11 +23,10 @@ Ext.define("viewer.viewercontroller.controller.Feature",{
         }
        // this.wktParser = new OpenLayers.Format.WKT();
     },
-
-    
     
     /**
      * Function to get the JSON representation for this feature object.
+     * @return {Object} this feature as an object
      */
     toJsonObject : function (){
         var json = {};
@@ -33,6 +35,33 @@ Ext.define("viewer.viewercontroller.controller.Feature",{
         json.color = this.color;
         json.label = this.label;
         return json;
+    },
+    /**
+     * get the extent of this feature, only works for 2D space.
+     * @return {viewer.viewercontroller.controller.Extent} The extent of this feature (or null when there is valid geometry)
+     */
+    getExtent: function () {
+        if (this._wktgeom === null) {
+            return null;
+        }
+        var regex = /(\d+[\.]?\d*)/g;
+        var vertices = (this._wktgeom).match(regex);
+        if (vertices === null) {
+            return null;
+        }
+        var xcoords = [];
+        var ycoords = [];
+        for (var i = 0; i < vertices.length; i++) {
+            if (i % 2 === 0) {
+                xcoords.push(parseFloat(vertices[i]));
+            } else {
+                ycoords.push(parseFloat(vertices[i]));
+            }
+        }
+        return Ext.create('viewer.viewercontroller.controller.Extent',
+                Ext.Array.min(xcoords),
+                Ext.Array.min(ycoords),
+                Ext.Array.max(xcoords),
+                Ext.Array.max(ycoords));
     }
-
 });
