c9c674ab9ef44d1ecc2704c37c07c936dc23420a
Meine Toonen <toonen.meine@gmail.com>
2015-09-03 11:23:04 +0200

    Use dutch tileset resolutions by default. Fixes #408

--- viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/OpenLayersMapComponent.js
+++ viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/OpenLayersMapComponent.js
@@ -39,7 +39,7 @@ Ext.define("viewer.viewercontroller.OpenLayersMapComponent",{
                 }
             }
         }else{
-            resolutions = [512,256,128,64,32,16,8,4,2,1,0.5,0.25,0.125];
+            resolutions = [3440.64,1720.32,860.16,430.08,215.04,107.52,53.76,26.88,13.44,6.72,3.36,1.68,0.84,0.42,0.21,0.105];
         }
         //TODO: remove the hardcoded projection....
         Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs";
