b41a507653f8b205880cd0314f0e72f2c5131645
Mark Prins <mark@b3partners.nl>
2015-10-13 14:45:29 +0200

    change getter methods for accessing properties directly
    this seems to fix the aspect ration problem, but the overview is a bit off in the panel.
    
    close #430

--- viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/components/OpenLayersOverview.js
+++ viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/components/OpenLayersOverview.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2012-2013 B3Partners B.V.
+ * Copyright (C) 2012-2015 B3Partners B.V.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,51 +16,54 @@
  */
 
 /**
- * OpenLayers Overview component
- * Creates a Overview component for OpenLayers
+ * OpenLayers Overview component.
+ * Creates an Overview component for an OpenLayers map.
  * @author <a href="mailto:meinetoonen@b3partners.nl">Meine Toonen</a>
  */
-Ext.define ("viewer.viewercontroller.openlayers.components.OpenLayersOverview",{
-    extend: "viewer.viewercontroller.openlayers.OpenLayersComponent",    
-    config:{
-        top:null,
-        left:null,
-        url:null,
-        layers:null,
-        position:null,
+Ext.define("viewer.viewercontroller.openlayers.components.OpenLayersOverview", {
+    extend: "viewer.viewercontroller.openlayers.OpenLayersComponent",
+    config: {
+        top: null,
+        left: null,
+        url: null,
+        layers: null,
+        position: null,
         height: null,
         width: null,
         lox: null,
         loy: null,
         rbx: null,
         rby: null,
-        followZoom:null
+        followZoom: null
     },
-    
-    constructor: function (conf){        
+    constructor: function (conf) {
         this.height = 300;
-        this.width= 300;
+        this.width = 300;
         viewer.viewercontroller.openlayers.components.OpenLayersOverview.superclass.constructor.call(this, conf);
-        
-        if (Ext.isEmpty(this.config.url)){
+
+        if (Ext.isEmpty(this.config.url)) {
             throw new Error("No URL set for Overview component, unable to load component");
         }
-        var maxBounds =this.config.viewerController.mapComponent.getMap().frameworkMap.maxExtent;
+        var maxBounds = this.config.viewerController.mapComponent.getMap().frameworkMap.maxExtent;
         var bounds;
-        if (this.getLox()!=null && this.getLoy()!=null && this.getRbx()!=null && this.getRby()!=null
-            && this.getLox()!=this.getRbx() && this.getLoy() != this.getRby()){            
-            bounds = new OpenLayers.Bounds(this.getLox(),this.getLoy(),this.getRbx(),this.getRby());
-        }else{
-            bounds= maxBounds;
+        if (this.config.lox !== null && this.config.loy !== null && this.config.rbx !== null && this.config.rby !== null
+                && this.config.lox !== this.config.rbx && this.config.loy !== this.config.rby) {
+            bounds = new OpenLayers.Bounds(
+                    parseFloat(this.config.lox),
+                    parseFloat(this.config.loy),
+                    parseFloat(this.config.rbx),
+                    parseFloat(this.config.rby));
+        } else {
+            bounds = maxBounds;
         }
-        var size=new OpenLayers.Size(""+this.width,""+this.height);
-        var layer =  new OpenLayers.Layer.Image(
-            "OverviewLaag", 
-            this.config.url,
-            bounds,
-            size
-        );
-        
+       
+        var size = new OpenLayers.Size(parseInt(this.config.width, 10), parseInt(this.config.height, 10));
+        var layer = new OpenLayers.Layer.Image(
+                "OverviewLaag",
+                this.config.url,
+                bounds,
+                size);
+
         this.frameworkObject = new OpenLayers.Control.OverviewMap({
             maximized: true,
             mapOptions: {
@@ -71,63 +74,61 @@ Ext.define ("viewer.viewercontroller.openlayers.components.OpenLayersOverview",{
             size: size,
             layers: [layer]
         });
-        if(this.config.followZoom !== undefined && this.config.followZoom !== null && this.config.followZoom ===false){
-            this.frameworkObject.maxRatio= 999999;
+        if (this.config.followZoom !== undefined && this.config.followZoom !== null && this.config.followZoom === false) {
+            this.frameworkObject.maxRatio = 999999;
         }
 
         return this;
     },
-    
-    getExtComponents: function() {
+    getExtComponents: function () {
         return [];
     },
-    
     //setters for bounds, make sure it are numbers
-    setLox: function (value){
-        if (isNaN(value)){
-            this.lox=null;
+    setLox: function (value) {
+        if (isNaN(value)) {
+            this.lox = null;
             return;
         }
         this.lox = Number(value);
     },
-    setLoy: function (value){
-        if (isNaN(value)){
-            this.loy=null;
+    setLoy: function (value) {
+        if (isNaN(value)) {
+            this.loy = null;
             return;
         }
         this.loy = Number(value);
     },
-    setRbx: function (value){
-        if (isNaN(value)){
-            this.rbx=null;
+    setRbx: function (value) {
+        if (isNaN(value)) {
+            this.rbx = null;
             return;
         }
         this.rbx = Number(value);
     },
-    setRby: function (value){
-        if (isNaN(value)){
-            this.rby=null;
+    setRby: function (value) {
+        if (isNaN(value)) {
+            this.rby = null;
             return;
         }
         this.rby = Number(value);
     },
     //make sure the heigth and width are numbers
-    setHeight: function (value){
-        if (isNaN(value)){
-            this.height=null;
+    setHeight: function (value) {
+        if (isNaN(value)) {
+            this.height = null;
             return;
-        }else if (!Ext.isEmpty(value) && value > 0){
+        } else if (!Ext.isEmpty(value) && value > 0) {
             this.height = Number(value);
         }
     },
-    setWidth: function (value){
-        if (isNaN(value)){
-            this.width=null;
+    setWidth: function (value) {
+        if (isNaN(value)) {
+            this.width = null;
             return;
-        }else if (!Ext.isEmpty(value) && value > 0){
+        } else if (!Ext.isEmpty(value) && value > 0) {
             this.width = Number(value);
         }
-        
+
     }
 });
 
