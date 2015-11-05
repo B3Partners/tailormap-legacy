eee03cb38844dc738633339784118bdc14d9681e
Meine Toonen <toonen.meine@gmail.com>
2015-11-05 14:13:18 +0100

    First stab at managing bookmarks

---
+++ viewer-admin/src/main/webapp/resources/js/services/bookmark.js
@@ -0,0 +1,100 @@
+
+Ext.Loader.setConfig({enabled: true});
+Ext.Loader.setPath('Ext.ux', uxpath);
+Ext.require([
+    'Ext.grid.*',
+    'Ext.data.*',
+    'Ext.util.*',
+    'Ext.ux.grid.GridHeaderFilters',
+    'Ext.toolbar.Paging'
+]);
+
+Ext.onReady(function(){
+
+    Ext.define('TableRow', {
+        extend: 'Ext.data.Model',
+        fields: [
+            {name: 'id', type: 'int' },
+            {name: 'application.name', type: 'string'},
+            {name: 'createdAt', type: 'string'}
+        ]
+    });
+
+    var store = Ext.create('Ext.data.Store', {
+        pageSize: 10,
+        model: 'TableRow',
+        remoteSort: true,
+        remoteFilter: true,
+        sorters: 'id',
+        autoLoad: true,
+        proxy: {
+            type: 'ajax',
+            url: gridurl,
+            reader: {
+                type: 'json',
+                root: 'gridrows',
+                totalProperty: 'totalCount'
+            },
+            simpleSortMode: true
+        },
+        listeners: {
+            load: function() {
+                // Fix to apply filters
+                Ext.getCmp('editGrid').doLayout();
+            }
+        }
+    });
+
+    var grid = Ext.create('Ext.grid.Panel', Ext.merge(defaultGridConfig, {
+        id: 'editGrid',
+        store: store,
+        columns: [
+            {
+                id: 'applicationname',
+                text: "Applicatie",
+                dataIndex: 'application.name',
+                flex: 1,
+                filter: {
+                    xtype: 'textfield'
+                }
+            },{
+                id: 'createdAt',
+                text: "Datum",
+                dataIndex: 'createdAt',
+                flex: 1,
+                filter: {
+                    xtype: 'textfield'
+                }
+            },{
+                id: 'delete',
+                header: '',
+                dataIndex: 'id',
+                flex: 1,
+                sortable: false,
+                hideable: false,
+                menuDisabled: true,
+                renderer: function(value, style, row) {
+                    return Ext.String.format('<a href="#" onclick="return deleteBookmark({0});">Verwijderen</a>', value);
+                }
+            }
+        ],
+        bbar: Ext.create('Ext.PagingToolbar', {
+            store: store,
+            displayInfo: true,
+            displayMsg: 'Applicaties {0} - {1} of {2}',
+            emptyMsg: "Geen applicaties weer te geven"
+        }),
+        plugins: [
+            Ext.create('Ext.ux.grid.GridHeaderFilters', {
+                enableTooltip: false
+            })
+        ],
+        renderTo: 'grid-container'
+    }));
+
+});
+
+
+function reloadGrid(){
+Ext.getCmp('editGrid').getStore().load();
+}
