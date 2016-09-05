5df407a9a2affe4b81512308939e93f897295a6c
Meine Toonen <toonen.meine@gmail.com>
2016-09-05 15:46:20 +0200

    unit test for deleting applayers which are used in mashups

--- viewer-admin/src/test/java/nl/b3p/viewer/admin/stripes/ApplicationTreeLevelActionBeanTest.java
+++ viewer-admin/src/test/java/nl/b3p/viewer/admin/stripes/ApplicationTreeLevelActionBeanTest.java
@@ -6,6 +6,7 @@
 package nl.b3p.viewer.admin.stripes;
 
 import net.sourceforge.stripes.action.ActionBeanContext;
+import nl.b3p.viewer.config.app.Application;
 import nl.b3p.viewer.config.app.ApplicationLayer;
 import nl.b3p.viewer.config.app.StartLayer;
 import nl.b3p.viewer.util.TestUtil;
@@ -18,37 +19,37 @@ import org.junit.Test;
 
 /**
  *
- * @author meine
+ * @author Meine Toonen
  */
 public class ApplicationTreeLevelActionBeanTest extends TestUtil {
+
     private static final Log log = LogFactory.getLog(ApplicationTreeLevelActionBeanTest.class);
 
-    
-    private  ApplicationTreeLevelActionBean instance;
-    
+    private ApplicationTreeLevelActionBean instance;
+
     @Before
-    public void setup(){
-        instance = new ApplicationTreeLevelActionBean();
-    }
-    
-    @Test
-    public void testDeleteAppLayer(){
-        
+    public void setup() {
         initData(true);
+        instance = new ApplicationTreeLevelActionBean();
         instance.setContext(new ActionBeanContext());
         instance.setApplication(app);
+    }
+
+    @Test
+    public void testDeleteAppLayer() {
+
         assertNotNull(testAppLayer);
         assertNotNull(testStartLayer);
-        
+
         String selectedLayer = "";
-        
+
         instance.updateApplayersInLevel(selectedLayer, testLevel, entityManager);
-                 
-        try{
+
+        try {
             entityManager.getTransaction().commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Fout bij verwijderen", e);
-            assert(false);
+            assert (false);
         }
         entityManager.getTransaction().begin();
 
@@ -56,5 +57,21 @@ public class ApplicationTreeLevelActionBeanTest extends TestUtil {
         StartLayer startLayerNull = entityManager.find(StartLayer.class, testStartLayer.getId());
         assertNull(appLayerNull);
         assertNull(startLayerNull);
+    }
+
+    @Test
+    public void testRemoveAppLayerUsedInMashup() throws Exception {
+        Application mashup = app.createMashup("mashup", entityManager, false);
+        entityManager.persist(mashup);
+        objectsToRemove.add(mashup);
+        String selectedLayer = "";
+
+        instance.updateApplayersInLevel(selectedLayer, testLevel, entityManager);
+           try {
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            log.error("Fout bij verwijderen", e);
+            assert (false);
+        }
     }
 }
