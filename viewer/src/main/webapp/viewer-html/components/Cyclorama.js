5ffe8bb0f67fc2643c718340287918277e394eb6
Meine Toonen <toonen.meine@gmail.com>
2016-09-09 16:17:32 +0200

    Fixes #690: the attributename is now retrieved and not the attributeId.

--- viewer/src/main/webapp/viewer-html/components/Cyclorama.js
+++ viewer/src/main/webapp/viewer-html/components/Cyclorama.js
@@ -24,6 +24,7 @@ Ext.define ("viewer.components.Cyclorama",{
     deActivatedTools:null,
     window:null,
     optionWindow:null,
+    imageIdName:null,
     config:{
     },
     constructor: function (conf){
@@ -31,6 +32,7 @@ Ext.define ("viewer.components.Cyclorama",{
 		viewer.components.Cyclorama.superclass.constructor.call(this, this.config);
         // Registreer voor layerinitialized
         this.viewerController.addListener(viewer.viewercontroller.controller.Event.ON_LAYERS_INITIALIZED, this.initComp, this);
+       
         return this;
     },
     initComp : function(){
@@ -43,7 +45,26 @@ Ext.define ("viewer.components.Cyclorama",{
             },
             viewerController: this.config.viewerController
         });
-        this.toolMapClick.activateTool();
+        var appLayer = this.viewerController.getAppLayerById(this.config.layers);
+
+        var attributes = appLayer.attributes;
+        var me = this;
+        function processAttributes(attributes) {
+            for (var i = 0; i < attributes.length; i++) {
+                if (attributes[i].id === me.config.imageIdAttribute) {
+                    me.imageIdName = attributes[i].name;
+                    break;
+                }
+            }
+            me.toolMapClick.activateTool();
+        }
+        if(!attributes){
+            this.viewerController.app.appLayers[this.layers].featureService.loadAttributes(appLayer, processAttributes);
+        }else{
+            processAttributes(appLayer.attributes);
+        }
+/*
+       */
     },
     processResponse: function (response){
         if(response.features.length >1 ){
@@ -68,7 +89,7 @@ Ext.define ("viewer.components.Cyclorama",{
         var grid = Ext.create('Ext.grid.Panel', {
             store: store,
             columns: [
-                { header: 'Image id',  dataIndex: this.config.imageIdAttribute,flex:1 }
+                { header: 'Image id',  dataIndex: this.imageIdName,flex:1 }
             ],
             listeners:{
                 itemdblclick:{
@@ -93,7 +114,7 @@ Ext.define ("viewer.components.Cyclorama",{
     },
     openGlobespotter : function(feature){
         var params = {
-            imageId: feature[this.config.imageIdAttribute] ,
+            imageId: feature[this.imageIdName],
             appId: appId,
             accountId: this.config.keyCombo
         };
