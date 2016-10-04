37be6fdc0e39b975ae83ad6a5a3d107197d256b1
Meine Toonen <toonen.meine@gmail.com>
2016-10-04 13:56:59 +0200

    Probably ExtJS has changed it's inner workings for dropdowns/picklists. Retrieve the highlighedItem on the correct parent. Now the picklist is correctly hidden again after pressing enter

--- viewer/src/main/webapp/viewer-html/components/Search.js
+++ viewer/src/main/webapp/viewer-html/components/Search.js
@@ -257,8 +257,8 @@ Ext.define ("viewer.components.Search",{
                         specialkey: function(field, e){
                             if (e.getKey() === e.ENTER) {
                                 var item = null;
-                                if(field.picker && field.picker.pickerField && field.picker.pickerField.listKeyNav && field.picker.pickerField.listKeyNav.boundList.highlightedItem) {
-                                    item = field.picker.pickerField.listKeyNav.boundList.highlightedItem;
+                                if(field.picker && field.picker.highlightedItem){
+                                    item = field.picker.highlightedItem;
                                 }
                                 if(!item){
                                     me.search();
