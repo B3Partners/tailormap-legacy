69ba63b8a0a031d0d2e49accdcc3e37c70f64f0f
Meine Toonen <toonen.meine@gmail.com>
2017-02-03 09:18:00 +0100

    Updated updatescripts

--- viewer-config-persistence/src/main/resources/scripts/oracle-add-wmts-support.sql
+++ viewer-config-persistence/src/main/resources/scripts/oracle-add-wmts-support.sql
@@ -1,7 +1,6 @@
-
-    create table layer_matrix_sets (
+create table layer_matrix_sets (
         layer number(19,0) not null,
-        matrix_set number(19,0) not null unique,
+        matrix_set number(19,0) not null,
         list_index number(10,0) not null,
         primary key (layer, list_index)
     );
@@ -14,23 +13,28 @@
         matrix_width number(10,0) not null,
         scale_denominator varchar2(255 char),
         tile_height number(10,0) not null,
-        tile_witdh number(10,0) not null,
+        tile_width number(10,0) not null,
         title varchar2(255 char),
-        top_left_point varchar2(255 char),
+        top_left_corner varchar2(255 char),
         matrix_set number(19,0),
         primary key (id)
     );
 
+
     create table tile_matrix_set (
         id number(19,0) not null,
+        max_crs varchar2(255 char),
+        max_maxx double precision,
+        max_maxy double precision,
+        max_minx double precision,
+        max_miny double precision,
         crs varchar2(255 char),
         identifier varchar2(255 char),
         tile_service number(19,0),
         primary key (id)
     );
 
-
-    alter table layer_matrix_sets 
+alter table layer_matrix_sets 
         add constraint FK118A64219EBB0496 
         foreign key (matrix_set) 
         references tile_matrix_set;
@@ -39,7 +43,6 @@
         add constraint FK118A64212AB24981 
         foreign key (layer) 
         references layer;
-
 
     alter table tile_matrix 
         add constraint FK937382729EBB0496 
@@ -55,3 +58,4 @@
     create sequence tile_matrix_id_seq;
 
     create sequence tile_matrix_set_id_seq;
+
--- viewer-config-persistence/src/main/resources/scripts/postgresql-add-wmts-support.sql
+++ viewer-config-persistence/src/main/resources/scripts/postgresql-add-wmts-support.sql
@@ -1,11 +1,10 @@
 
     create table layer_matrix_sets (
         layer int8 not null,
-        matrix_set int8 not null unique,
+        matrix_set int8 not null,
         list_index int4 not null,
         primary key (layer, list_index)
     );
-
 
     create table tile_matrix (
         id  bigserial not null,
@@ -15,22 +14,25 @@
         matrix_width int4 not null,
         scale_denominator varchar(255),
         tile_height int4 not null,
-        tile_witdh int4 not null,
+        tile_width int4 not null,
         title varchar(255),
-        top_left_point varchar(255),
+        top_left_corner varchar(255),
         matrix_set int8,
         primary key (id)
     );
 
     create table tile_matrix_set (
         id  bigserial not null,
+        max_crs varchar(255),
+        max_maxx float8,
+        max_maxy float8,
+        max_minx float8,
+        max_miny float8,
         crs varchar(255),
         identifier varchar(255),
         tile_service int8,
         primary key (id)
     );
-
-
 
     alter table layer_matrix_sets 
         add constraint FK118A64219EBB0496 
@@ -41,8 +43,6 @@
         add constraint FK118A64212AB24981 
         foreign key (layer) 
         references layer;
-
-
     alter table tile_matrix 
         add constraint FK937382729EBB0496 
         foreign key (matrix_set) 
