fe4d96f122e4fde1400e244dccffa5519b1c2402
Geert Plaisier <geert.plaisier@gnamic.nl>
2017-04-13 17:57:11 +0200

    Fixes ExtJS error messages in console
    TypeError: undefined is not an object (evaluating 'e.touches.length')

--- viewer/src/main/webapp/viewer-html/common/AppLoader.js
+++ viewer/src/main/webapp/viewer-html/common/AppLoader.js
@@ -36,6 +36,7 @@ Ext.define("viewer.AppLoader", {
      * @param {Object} config
      */
     constructor: function(config) {
+        this.initOverrides();
         this.initConfig(config);
         this.exposeGlobalVariables();
         this.loadApplication();
@@ -240,6 +241,25 @@ Ext.define("viewer.AppLoader", {
             steunkleur2: appConfig.details.steunkleur2,
             font: appConfig.details.font,
             globalLayout: globalLayout
+        });
+    },
+
+    initOverrides: function() {
+        Ext.define('viewer.overrides.dom.TouchAction', {
+            override: 'Ext.dom.TouchAction',
+            fixEvent: function(e) {
+                if(!e.touches) e.touches = [];
+                return e;
+            },
+            onTouchStart: function(e) {
+                this.callParent([this.fixEvent(e)]);
+            },
+            onTouchMove: function(e) {
+                this.callParent([this.fixEvent(e)]);
+            },
+            onTouchEnd: function(e) {
+                this.callParent([this.fixEvent(e)]);
+            }
         });
     }
 
