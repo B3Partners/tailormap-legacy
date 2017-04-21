927dbad522dd087313a7de04968a983511e1bde3
Meine Toonen <toonen.meine@gmail.com>
2017-04-21 11:11:16 +0200

    Reduce warnings

--- viewer-admin/src/main/webapp/resources/js/services/geoserviceregistry.js
+++ viewer-admin/src/main/webapp/resources/js/services/geoserviceregistry.js
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* global Ext */
+
 Ext.onReady(function() {
 
     // Definition of the TreeNode model. Get function is overridden, so custom
