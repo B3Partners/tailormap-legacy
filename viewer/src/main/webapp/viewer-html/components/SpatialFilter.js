3f0a55cba101993bc77afd8e2ae28c95921e1aea
Meine Toonen <toonen.meine@gmail.com>
2017-08-10 16:01:26 +0200

    added js hints

--- viewer/src/main/webapp/viewer-html/components/SpatialFilter.js
+++ viewer/src/main/webapp/viewer-html/components/SpatialFilter.js
@@ -14,6 +14,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/* global Ext, contextPath, actionBeans, MobileManager */
+
 /**
  * Spatial Filter component
  * This component adds the functionality of creating a spatial filter: a filter based on a drawn geometry (polygon, rectangle, circle or freeform). All features must
