c64d245013d8019f66d4bb8515cb14eb6be19e73
Meine Toonen <toonen.meine@gmail.com>
2017-08-14 16:24:11 +0200

    Added FlamingoCQL. Delegates ECQL filters to ECQL class when there is no APPLAYER type of cql filter

---
+++ viewer/src/main/java/nl/b3p/viewer/util/FlamingoCQL.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.viewer.util;
+
+import org.geotools.filter.text.cql2.CQLException;
+import org.geotools.filter.text.ecql.ECQL;
+import org.opengis.filter.Filter;
+
+/**
+ *
+ * @author Meine Toonen
+ */
+public class FlamingoCQL {
+    public Filter toFilter(String filter) throws CQLException{
+        if(filter.contains("APPLAYER")){
+            
+        }
+        
+        
+        return ECQL.toFilter(filter);
+    }
+}
