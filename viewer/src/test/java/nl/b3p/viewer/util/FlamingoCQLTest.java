f5644f847ad26db9e88568324d219da6dc9d1f62
Meine Toonen <toonen.meine@gmail.com>
2017-08-16 10:56:05 +0200

    Fixed nested APPLAYER filters.

--- viewer/src/test/java/nl/b3p/viewer/util/FlamingoCQLTest.java
+++ viewer/src/test/java/nl/b3p/viewer/util/FlamingoCQLTest.java
@@ -35,7 +35,7 @@ public class FlamingoCQLTest extends TestUtil{
         FeatureSource fs;
     }
 
-   // @Test
+    //@Test
     public void testToFilterSingleGeom() throws CQLException {
         String input = "INTERSECTS(the_geom, POLYGON(( 1 1, 2 1, 2 2, 1 2, 1 1)))";
         Filter output = cql.toFilter(input, entityManager);
@@ -43,7 +43,7 @@ public class FlamingoCQLTest extends TestUtil{
         assertEquals("[ the_geom intersects POLYGON ((1 1, 2 1, 2 2, 1 2, 1 1)) ]", output.toString());
     }
 
-   // @Test
+    //@Test
     public void testToFilterSingleAttribute() throws CQLException {
         String input = "pietje = 2";
         Filter output = cql.toFilter(input, entityManager);
@@ -52,7 +52,7 @@ public class FlamingoCQLTest extends TestUtil{
     }
     
 
-   // @Test
+    //@Test
     public void testToFilterMultipleAttributes() throws CQLException {
         String input = "pietje = 2 and aap = 'noot'";
         Filter output = cql.toFilter(input, entityManager);
@@ -61,47 +61,60 @@ public class FlamingoCQLTest extends TestUtil{
     }
     
     
-    @Test
+    //@Test
     public void testToFilterSingleApplayerWithoutFilter() throws CQLException {
         initData(true);
-        String input = "APPLAYER(the_geom, " + testAppLayer.getId() + ",'')";
+        String input = "APPLAYER(the_geom, " + testAppLayer.getId() + ",)";
         Filter output = cql.toFilter(input, entityManager);
         
-        assertEquals("[ the_geom intersects MULTIPOLYGON (((150218.522352941 432398.543058824, 142036.882823529 432504.798117647, 142355.648 437923.806117647, 150218.522352941 432398.543058824)), ((156487.570823529 452161.984, 156912.591058823 442917.793882353, 191870.505411764 441111.457882353, 191870.505411764 454712.105411765, 191870.505411764 454712.105411765, 156487.570823529 452161.984)), ((137467.915294117 453543.299764706, 137680.425411764 443130.304, 144693.259294117 443874.089411765, 145437.044705882 451418.198588236, 145437.044705882 451418.198588236, 137467.915294117 453543.299764706))) ]", output.toString());
+        assertEquals("[ the_geom intersects MULTIPOLYGON (((156487.5708 452161.984, 191870.5054 454712.1054, 191870.5054 441111.4579, 156912.5911 442917.7939, 156487.5708 452161.984)), ((137467.9153 453543.2998, 145437.0447 451418.1986, 144693.2593 443874.0894, 137680.4254 443130.304, 137467.9153 453543.2998)), ((150218.5224 432398.5431, 142036.8828 432504.7981, 142355.648 437923.8061, 150218.5224 432398.5431))) ]", output.toString());
     }
     
     
     //@Test
     public void testToFilterSingleApplayerWithFilter() throws CQLException {
-        String input = "APPLAYER(1, the_geom, gid = 1)";
+        initData(true);
+        String input = "APPLAYER(the_geom, " + testAppLayer.getId() + ", gid = 1)";
         Filter output = cql.toFilter(input, entityManager);
         
-        assertEquals("[ the_geom intersects POLYGON((156487.570823529 452161.984,156912.591058823 442917.793882353,191870.505411764 441111.457882353,191870.505411764 454712.105411765,191870.505411764 454712.105411765,156487.570823529 452161.984)) ]", output.toString());
+        assertEquals("[ the_geom intersects POLYGON ((156487.5708 452161.984, 156912.5911 442917.7939, 191870.5054 441111.4579, 191870.5054 454712.1054, 191870.5054 454712.1054, 156487.5708 452161.984)) ]", output.toString());
     }
     
           
-    //@Test
+   // @Test
     public void testToFilterNestedApplayer() throws CQLException {
-        String input = "APPLAYER(1, the_geom,  APPLAYER (2, geometrie, ''))";
+        initData(true);
+        String input = "APPLAYER(geom, " + testAppLayer.getId() + ",  APPLAYER(geom, " + testAppLayer.getId() + ", gid = 1))";
         Filter output = cql.toFilter(input, entityManager);
         
-        assertEquals("[ the_geom intersects POLYGON ((1 1, 2 1, 2 2, 1 2, 1 1)) ]", output.toString());
+        assertEquals("[ geom intersects POLYGON ((156487.5708 452161.984, 156912.5911 442917.7939, 191870.5054 441111.4579, 191870.5054 454712.1054, 191870.5054 454712.1054, 156487.5708 452161.984)) ]", output.toString());
     }
     
     //@Test
-    public void testToFilterApplayerAndAttribute() throws CQLException {
-        String input = "pietje = 2 AND PPLAYER(1, the_geom,  gm_naam = 'Gouda')";
+    public void testToFilterAttributeAndApplayer() throws CQLException {
+        initData(true);
+        String input = "pietje = 2 AND APPLAYER(the_geom, " + testAppLayer.getId() + ",  gid = 1)";
         Filter output = cql.toFilter(input, entityManager);
-        
-        assertEquals("[ the_geom intersects POLYGON ((1 1, 2 1, 2 2, 1 2, 1 1)) ]", output.toString());
+        String result = output.toString();
+        assertEquals("[[ pietje = 2 ] AND [ the_geom intersects POLYGON ((156487.5708 452161.984, 156912.5911 442917.7939, 191870.5054 441111.4579, 191870.5054 454712.1054, 191870.5054 454712.1054, 156487.5708 452161.984)) ]]", result);
+    }
+    
+    @Test
+    public void testToFilterApplayerAndAttribute() throws CQLException {
+        initData(true);
+        String input = "APPLAYER(the_geom, " + testAppLayer.getId() + ",  gid = 1) AND pietje = 2 ";
+        Filter output = cql.toFilter(input, entityManager);
+        String result = output.toString();
+        assertEquals("[[ the_geom intersects POLYGON ((156487.5708 452161.984, 156912.5911 442917.7939, 191870.5054 441111.4579, 191870.5054 454712.1054, 191870.5054 454712.1054, 156487.5708 452161.984)) ] AND [ pietje = 2 ]]", result);
     }
     
     //@Test
     public void testToFilterApplayerAndGeom() throws CQLException {
-        String input = "INTERSECTS(the_geom, POLYGON(( 1 1, 2 1, 2 2, 1 2, 1 1))) AND APPLAYER(1, the_geom,  gm_naam = 'Gouda')";
+        initData(true);
+        String input = "INTERSECTS(the_geom, POLYGON(( 1 1, 2 1, 2 2, 1 2, 1 1))) AND APPLAYER(the_geom, " + testAppLayer.getId() + ",  gid = 1)";
         Filter output = cql.toFilter(input, entityManager);
         
-        assertEquals("[ the_geom intersects POLYGON ((1 1, 2 1, 2 2, 1 2, 1 1)) ]", output.toString());
+        assertEquals("[[ the_geom intersects POLYGON ((1 1, 2 1, 2 2, 1 2, 1 1)) ] AND [ the_geom intersects POLYGON ((156487.5708 452161.984, 156912.5911 442917.7939, 191870.5054 441111.4579, 191870.5054 454712.1054, 191870.5054 454712.1054, 156487.5708 452161.984)) ]]", output.toString());
     }
     
 }
