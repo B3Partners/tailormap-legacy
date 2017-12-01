0c11401f77c6409f3191ee1c2157eed7e9819a5f
Mark Prins <mark@b3partners.nl>
2017-12-01 16:42:39 +0100

    fix typo in sql script name

--- viewer-config-persistence/src/main/java/nl/b3p/viewer/util/databaseupdate/DatabaseSynchronizer.java
+++ viewer-config-persistence/src/main/java/nl/b3p/viewer/util/databaseupdate/DatabaseSynchronizer.java
@@ -153,7 +153,7 @@ public class DatabaseSynchronizer implements Servlet {
         
         updates.put("28", new UpdateElement(Collections.singletonList("add_geoservice_rights.sql"), String.class));
 
-        updates.put("29", new UpdateElement(Collections.singletonList("add_wms_discover-wfs"), String.class));
+        updates.put("29", new UpdateElement(Collections.singletonList("add_wms_discover-wfs.sql"), String.class));
         // NB when adding an update also update the metadata version in the testdata.sql file around line 343
     }
 
