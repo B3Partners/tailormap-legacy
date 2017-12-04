5c0db8be5e2ef597ae0e7fd82fa19db38baf6dd2
Geert Plaisier <geert.plaisier@gnamic.nl>
2017-12-04 11:56:53 +0100

    Added default strategy for Merge

--- viewer/src/main/webapp/viewer-html/components/Merge.js
+++ viewer/src/main/webapp/viewer-html/components/Merge.js
@@ -31,7 +31,7 @@ Ext.define("viewer.components.Merge", {
     config: {
         name: "Samenvoegen",
         tooltip: "Features samenvoegen",
-        strategy: null,
+        strategy: "replace",
         actionbeanUrl: "/viewer/action/feature/merge",
         layers: null,
         mergeGapDist: null,
