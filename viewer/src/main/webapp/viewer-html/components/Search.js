3572f8f25a02a0ab1d4eee6a1be9b24b6eff8f99
Meine Toonen <toonen.meine@gmail.com>
2018-03-29 15:23:34 +0200

    removed dead/commented out code

--- viewer/src/main/webapp/viewer-html/components/Search.js
+++ viewer/src/main/webapp/viewer-html/components/Search.js
@@ -711,8 +711,6 @@ Ext.define ("viewer.components.Search",{
         if(this.getCurrentSearchType() === "solr"){
             var appLayers = this.config.viewerController.getVisibleLayers();
             params["visibleLayers"] = appLayers.join(", ");
-        }else{
-            // Nothing to do here
         }
     }, 
     simpleListSearch:function(term){
