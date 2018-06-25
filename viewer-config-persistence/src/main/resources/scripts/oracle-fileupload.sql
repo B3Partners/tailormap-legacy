f9e851fef1fe5cdc0a744f923dec51fd1aab863d
Meine Toonen <toonen.meine@gmail.com>
2018-06-25 09:38:59 +0200

    Fix for wrong columns in fileupload

--- viewer-config-persistence/src/main/resources/scripts/oracle-fileupload.sql
+++ viewer-config-persistence/src/main/resources/scripts/oracle-fileupload.sql
@@ -1,13 +1,18 @@
 
+
     create table file_upload (
         id number(19,0) not null,
         created_at timestamp,
         created_by varchar2(255 char),
         fid varchar2(255 char),
-        file varchar2(255 char),
+        filename varchar2(255 char),
+        location varchar2(255 char),
+        mimetype varchar2(255 char),
+        type_ varchar2(255 char),
         sft number(19,0),
         primary key (id)
     );
+
 
     alter table file_upload 
         add constraint FK7D6832246F7FEB1E 
