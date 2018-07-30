ca6075af5f88dc707f14e8830dae34aa8dd682b8
Mark Prins <mark@b3partners.nl>
2018-07-30 16:26:39 +0200

    reinstate integration testing with oracle

--- .jenkins/insert-fla-version.sh
+++ .jenkins/insert-fla-version.sh
@@ -3,4 +3,4 @@
 #  to get the current database version to be inserted
 #
 fladbversion=$(grep 'INSERT INTO metadata (id' ./viewer-config-persistence/src/test/resources/nl/b3p/viewer/util/testdata.sql)"
-sqlplus jenkins_flamingo/jenkins_flamingo@192.168.1.41:1521/DB01 <<< ${fladbversion}"
+sqlplus C##JENKINS_FLAMINGO/jenkins_flamingo@192.168.1.11:1521/orcl <<< ${fladbversion}"
