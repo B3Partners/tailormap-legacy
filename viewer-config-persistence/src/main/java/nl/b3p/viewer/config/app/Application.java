3294cd88fd6cd94bf506170846af4760d1c80935
mvdstruijk <m_struijk1990@hotmail.nl>
2018-11-05 10:06:17 +0100

    set projectionString on default projection

--- viewer-config-persistence/src/main/java/nl/b3p/viewer/config/app/Application.java
+++ viewer-config-persistence/src/main/java/nl/b3p/viewer/config/app/Application.java
@@ -442,7 +442,7 @@ public class Application implements Comparable<Application>{
         }
         o.put("version", version);
         o.put("title", title);
-        o.put("projectionCode", projectionCode != null ? projectionCode : "EPSG:28992");
+        o.put("projectionCode", projectionCode != null ? projectionCode : "EPSG:28992[+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs]");
 
         if (!onlyServicesAndLayers) {
             JSONObject d = new JSONObject();
