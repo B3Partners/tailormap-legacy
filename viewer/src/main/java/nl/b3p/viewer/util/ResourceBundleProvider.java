222dcb40ec1c8ebd4334afc2a07fbb10dcaf4114
Matthijs Laan <matthijsln@gmail.com>
2018-11-15 18:51:24 +0100

    fix getting base locale bundle, fixes #1262

--- viewer/src/main/java/nl/b3p/viewer/util/ResourceBundleProvider.java
+++ viewer/src/main/java/nl/b3p/viewer/util/ResourceBundleProvider.java
@@ -10,6 +10,8 @@ public class ResourceBundleProvider {
 
     private static final String VIEWER_RESOURCES_FILE = "ViewerResources";
 
+    private static final String BASE_LANGUAGE = "nl";
+
     public static ResourceBundle getResourceBundle() {
         return ResourceBundle.getBundle(VIEWER_RESOURCES_FILE);
     }
@@ -22,7 +24,16 @@ public class ResourceBundleProvider {
         if (application != null) {
             locale = LocaleUtils.toLocale(application.getLang());
         }
-        return ResourceBundle.getBundle(VIEWER_RESOURCES_FILE, locale);
-    }
 
+        // The language of the base properties file is "nl", but getBundle() 
+        // considers the property file for the system default locale before
+        // returning the base name properties file. We want the base
+        // properties when "nl" language is requested. Specifying Locale.ROOT
+        // makes getBundle() only consider the base name.
+        if(BASE_LANGUAGE.equals(locale.getLanguage())) {
+            return ResourceBundle.getBundle(VIEWER_RESOURCES_FILE, Locale.ROOT);
+        } else {
+            return ResourceBundle.getBundle(VIEWER_RESOURCES_FILE, locale);
+        }
+    }
 }
