26ee74000717d38897a8675f958c42ea3ff8601c
Meine Toonen <toonen.meine@gmail.com>
2018-11-15 11:34:42 +0100

    Fixes warning

--- viewer/src/main/webapp/viewer-html/components/Drawing.js
+++ viewer/src/main/webapp/viewer-html/components/Drawing.js
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/* global Ext, contextPath, MobileManager, actionBeans */
+/* global Ext, contextPath, MobileManager, actionBeans, i18next, viewer */
 
 /**
  * Drawing component
