f1c8f212e470c6d83e3254e59995552d66797327
mvdstruijk <m_struijk1990@hotmail.nl>
2019-01-10 14:41:43 +0100

    Adjusted the way the measure tool is created

--- viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/OpenLayersMapComponent.js
+++ viewer/src/main/webapp/viewer-html/common/viewercontroller/openlayers/OpenLayersMapComponent.js
@@ -535,9 +535,18 @@ Ext.define("viewer.viewercontroller.OpenLayersMapComponent",{
             return new viewer.viewercontroller.openlayers.OpenLayersTool(conf,new OpenLayers.Control.DragPan(frameworkOptions));
         }else if (type == viewer.viewercontroller.controller.Tool.GET_FEATURE_INFO) {
             return new viewer.viewercontroller.openlayers.tools.OpenLayersIdentifyTool(conf);
-        }else if(type === viewer.viewercontroller.controller.Tool.MEASURELINE ||type === viewer.viewercontroller.controller.Tool.MEASUREAREA ){
-            conf.frameworkOptions = frameworkOptions;
-            var measureTool = new viewer.viewercontroller.openlayers.tools.OpenLayersMeasureTool(conf);
+        }else if(type === viewer.viewercontroller.controller.Tool.MEASURELINE  ||type === viewer.viewercontroller.controller.Tool.MEASUREAREA ){
+            var t = new viewer.viewercontroller.openlayers.OpenLayersMeasure(conf);
+            frameworkOptions = t.initialConfig.frameworkOptions;
+            if (conf.tooltip){
+                frameworkOptions.title=conf.tooltip;
+            }
+            var handler = conf.type === viewer.viewercontroller.controller.Tool.MEASURELINE ? OpenLayers.Handler.Path : OpenLayers.Handler.Polygon;
+            var measureTool= new viewer.viewercontroller.openlayers.OpenLayersTool(conf, new OpenLayers.Control.Measure( handler, frameworkOptions));
+            if(conf.type === viewer.viewercontroller.controller.Tool.MEASUREAREA){
+                measureTool.getFrameworkTool().displayClass = 'olControlMeasureArea';
+            }
+            t.initEvents(measureTool.getFrameworkTool());
             return measureTool;
         }else if (type==viewer.viewercontroller.controller.Tool.ZOOM_BAR){//13,
             return new OpenLayersTool(conf,new OpenLayers.Control.PanZoomBar(frameworkOptions));
