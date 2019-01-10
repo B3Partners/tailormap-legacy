0ffdd04e88b82cef0e4a2eeea99bd4626dc3e9bf
mvdstruijk <m_struijk1990@hotmail.nl>
2019-01-10 14:39:47 +0100

    Deleted OpenLayers2 specific things

--- viewer/src/main/webapp/viewer-html/components/tools/Measure.js
+++ viewer/src/main/webapp/viewer-html/components/tools/Measure.js
@@ -24,74 +24,8 @@
  * @author <a href="mailto:meinetoonen@b3partners.nl">Meine Toonen</a>
  */
 
-Ext.define ("viewer.components.tools.Measure",{
-    extend: "viewer.components.tools.Tool",
-    config: {},
-    constructor: function (conf) {
-        this.initConfig(conf);
-        viewer.components.tools.Measure.superclass.constructor.call(this, this.config);
-        if( viewer.viewercontroller.openlayers &&
-            viewer.viewercontroller.openlayers.tools &&
-            viewer.viewercontroller.openlayers.tools.OpenLayersMeasureHandler) {
-            conf.frameworkOptions = {
-                persist: true,
-                callbacks: {
-                    modify: function (vertex, feature){
-                        viewer.viewercontroller.openlayers.tools.OpenLayersMeasureHandler.modifyHandler(this, conf, vertex);
-                        if(this.handler.layer){
-                            this.handler.layer.events.triggerEvent("sketchmodified", {vertex: vertex, feature: feature});
-                        }
-                    }
-                }
-            };
-        }
-    },
-    initEvents: function() {
-        var config = this.config;
-        var frameworkTool = this.tool.getFrameworkTool();
-
-        function removeClone() {
-            var clonedValueDiv = document.getElementById("olControlMeasureValueClone");
-            if(clonedValueDiv && clonedValueDiv.parentNode) {
-                clonedValueDiv.parentNode.removeChild(clonedValueDiv);
-            }
-        }
-
-        function createMeasureClone() {
-            var measureValueDiv = document.getElementById("olControlMeasureValue");
-            if(!measureValueDiv) {
-                return;
-            }
-            removeClone();
-            var clonedValueDiv = measureValueDiv.cloneNode(true);
-            clonedValueDiv.id = 'olControlMeasureValueClone';
-            clonedValueDiv.className += ' olControlMeasureValueClone';
-            clonedValueDiv.querySelector('#olControlMeasureValueText').id = 'olControlMeasureValueTextClone';
-            return clonedValueDiv;
-        }
-
-        frameworkTool.events.register('measure', frameworkTool, function() {
-           this.map.div.appendChild(createMeasureClone());
-            if(config.nonSticky){
-                config.viewerController.mapComponent.activateTool(null, true);
-            }
-        });
-        frameworkTool.events.register('measurepartial', frameworkTool, function(){
-            removeClone();
-        });
-        frameworkTool.events.register('deactivate', frameworkTool, function(){
-            if( viewer.viewercontroller.openlayers &&
-                viewer.viewercontroller.openlayers.tools &&
-                viewer.viewercontroller.openlayers.tools.OpenLayersMeasureHandler) {
-                viewer.viewercontroller.openlayers.tools.OpenLayersMeasureHandler.removeMeasure();
-            }
-            removeClone();
-        });
-    }
-});
-
 Ext.define ("viewer.components.tools.Measure.Line",{
-    extend: "viewer.components.tools.Measure",
+    extend: "viewer.components.tools.Tool",
     config:{
         name: "measureline",
         tooltip: i18next.t('viewer_components_tools_measure_0'),
@@ -102,12 +36,12 @@ Ext.define ("viewer.components.tools.Measure.Line",{
         viewer.components.tools.Measure.Line.superclass.constructor.call(this, this.config);
         this.config.type = viewer.viewercontroller.controller.Tool.MEASURELINE;
         this.initTool(this.config);
-        this.initEvents();
+        //this.initEvents();
         return this;
     }
 });
 Ext.define ("viewer.components.tools.Measure.Area",{
-    extend: "viewer.components.tools.Measure",
+    extend: "viewer.components.tools.Tool",
     config:{
         name: "measurearea",
         tooltip: i18next.t('viewer_components_tools_measure_1'),
@@ -118,7 +52,7 @@ Ext.define ("viewer.components.tools.Measure.Area",{
         viewer.components.tools.Measure.Area.superclass.constructor.call(this, this.config);
         this.config.type = viewer.viewercontroller.controller.Tool.MEASUREAREA;
         this.initTool(this.config);
-        this.initEvents();
+        //this.initEvents();
         return this;
     }
 });
