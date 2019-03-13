921423450ab2ad0ab89e881f54ca4355a5f88683
Meine Toonen <toonen.meine@gmail.com>
2019-03-13 13:58:09 +0100

    Use streetsmart api 19.3

--- viewer/src/main/webapp/resources/js/b3p-cyclomedia-wrapper.js
+++ viewer/src/main/webapp/resources/js/b3p-cyclomedia-wrapper.js
@@ -1,7 +1,9 @@
-(function() {
+/* global StreetSmartApi */
+
+(function () {
 
     var loader = {
-        hide: function() {
+        hide: function () {
             document.querySelector(".loading").style.display = "none";
         }
     };
@@ -21,11 +23,13 @@
 
     function initApi(username, password) {
         return StreetSmartApi.init({
+            targetElement: document.getElementById('streetsmartAPI1'),
             username: username,
             password: password,
             apiKey: "K3MRqDUdej4JGvohGfM5e78xaTUxmbYBqL0tSHsNWnwdWPoxizYBmjIBGHAhS3U1",
             srs: "EPSG:28992",
             locale: "nl",
+            configurationUrl: 'https://atlas.cyclomedia.com/configuration',
             addressSettings: {
                 locale: "nl",
                 database: "CMDatabase"
@@ -34,12 +38,13 @@
     }
 
     function getQueryStringParameters() {
-        return (function(a) {
-            if (a == "") return {};
+        return (function (a) {
+            if (a == "")
+                return {};
             var b = {};
             for (var i = 0; i < a.length; ++i)
             {
-                var p=a[i].split('=', 2);
+                var p = a[i].split('=', 2);
                 if (p.length == 1)
                     b[p[0]] = "";
                 else
@@ -50,38 +55,46 @@
     }
 
     function init(qs) {
-        if(qs["auth"]) {
+        if (qs["auth"]) {
             var auth = window.atob(qs["auth"]);
             var up = auth.split(":");
             qs["username"] = up[0];
             qs["password"] = up[1];
         }
-        if(!qs["username"] || !qs["password"]) {
+        if (!qs["username"] || !qs["password"]) {
             alert("Username & Password are required");
         }
         initApi(qs["username"], qs["password"]).then(
-            function success() {
-                var cyclomediaViewer = StreetSmartApi.addPanoramaViewer(
-                    document.getElementById("streetsmartAPI1"),
-                    {
-                        recordingsVisible: true,
-                        timeTravelVisible: true
-                    }
+            function () {
+                console.log('Api: init: success!');
+                var query = qs.x + ", " + qs.y;
+                StreetSmartApi.open(query, {
+                    viewerType: [StreetSmartApi.ViewerType.PANORAMA, StreetSmartApi.ViewerType.OBLIQUE],
+                    srs: 'EPSG:28992'
+                }).then(
+                        function (result) {
+                            console.log('Created component through API:', result);
+                            if (result) {
+                                for (let i = 0; i < result.length; i++) {
+                                    if (result[i].getType() === StreetSmartApi.ViewerType.PANORAMA){
+                                        window.panoramaViewer = result[i];
+                                    }
+                                }
+                            }
+                            loader.hide();
+                        }.bind(this)
+                        ).catch(
+                        function (reason) {
+                            loader.hide();
+                            console.log('Failed to create component(s) through API: ' + reason);
+                        }
                 );
-                if(qs["imageId"]) {
-                    openByImageId(qs["imageId"], cyclomediaViewer);
-                } else if(qs["x"] && qs["y"]) {
-                    openByCoordinate(parseFloat(qs["x"]), parseFloat(qs["y"]), cyclomediaViewer);
-                } else {
-                    loader.hide();
-                }
             },
-            function error(err) {
-                console.log("Api: init: failed. Error: ", err);
-                alert("Api Init Failed!");
-                loader.hide();
+            function (err) {
+                console.log('Api: init: failed. Error: ', err);
+                alert('Api Init Failed!');
             }
-        )
+        );
     }
 
     init(getQueryStringParameters());
