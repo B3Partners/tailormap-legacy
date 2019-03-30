67a3c72e88a7e20534ae0760b7b684005e601972
Mark Prins <mprins@users.sf.net>
2019-03-30 17:08:26 +0100

    remove system printline and use @Ignore instead of a comment to exclude the webservices.gbo-provincies.nl testcase
    and some other cleanup for this PR only

--- viewer-config-persistence/src/test/java/nl/b3p/viewer/config/services/TileServiceTest.java
+++ viewer-config-persistence/src/test/java/nl/b3p/viewer/config/services/TileServiceTest.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2017 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package nl.b3p.viewer.config.services;
 
@@ -26,6 +37,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -50,7 +62,6 @@ public class TileServiceTest extends TestUtil{
      */
     @Test
     public void testLoadFromUrl() {
-        System.out.println("loadFromUrl");
         String url = "http://www.openbasiskaart.nl/mapcache/tms/1.0.0/osm-nb@rd";
         Map params = new HashMap();
         params.put(TileService.PARAM_SERVICENAME, "osm");
@@ -206,7 +217,8 @@ public class TileServiceTest extends TestUtil{
 
     }
 
-   // @Test
+    @Test
+    @Ignore("Meine knows why..")
     public void testLoadLufoWMTSFromURL() throws MalformedURLException {
         URL u = new URL("http://webservices.gbo-provincies.nl/lufo/services/wmts?request=GetCapabilities");
         String url = u.toString();
