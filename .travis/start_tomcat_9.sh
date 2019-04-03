8a260265f11084aa983718f2e55babda51f4d427
Mark Prins <mprins@users.sf.net>
2019-04-03 18:02:21 +0200

    unzip depedencies before running docker tomcat

--- .travis/start_tomcat_9.sh
+++ .travis/start_tomcat_9.sh
@@ -2,6 +2,8 @@
 VERSION=$(grep "<version>.*<.version>" -m1 pom.xml | sed -e "s/^.*<postgresql.version/<postgresql.version/" | cut -f2 -d">"| cut -f1 -d"<")
 PGVERSION=$(grep "<postgresql.version>.*<.postgresql.version>" -m1 pom.xml | sed -e "s/^.*<postgresql.version/<postgresql.version/" | cut -f2 -d">"| cut -f1 -d"<")
 
+unzip tomcat-lib/target/tomcat-lib-$VERSION.zip -d tomcat-lib/target/
+
 docker run -d --name tomcat9 -it --rm --network=host -p 9090:8080 \
   -v $PWD/viewer/target/viewer-$VERSION.war:/usr/local/tomcat/webapps/viewer.war \
   -v $PWD/viewer-admin/target/viewer-admin-$VERSION.war:/usr/local/tomcat/webapps/viewer-admin.war \
