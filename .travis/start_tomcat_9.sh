628c015bcc6cf23acad968baf74770226ef42aad
Mark Prins <mprins@users.sf.net>
2019-04-03 13:33:06 +0200

    docker network config

--- .travis/start_tomcat_9.sh
+++ .travis/start_tomcat_9.sh
@@ -2,7 +2,7 @@
 VERSION=$(grep "<version>.*<.version>" -m1 pom.xml | sed -e "s/^.*<postgresql.version/<postgresql.version/" | cut -f2 -d">"| cut -f1 -d"<")
 PGVERSION=$(grep "<postgresql.version>.*<.postgresql.version>" -m1 pom.xml | sed -e "s/^.*<postgresql.version/<postgresql.version/" | cut -f2 -d">"| cut -f1 -d"<")
 
-docker run -d --name tomcat9 -it --rm -p 9090:8080 \
+docker run -d --name tomcat9 -it --rm --network=host -p 9090:8080 \
   -v $PWD/viewer/target/viewer-$VERSION.war:/usr/local/tomcat/webapps/viewer.war \
   -v $PWD/viewer/target/viewer-admin-$VERSION.war:/usr/local/tomcat/webapps/viewer-admin.war \
   -v $PWD/tomcat-lib/target/lib/postgresql-$PGVERSION.jar:/usr/local/tomcat/lib/postgresql.jar \
