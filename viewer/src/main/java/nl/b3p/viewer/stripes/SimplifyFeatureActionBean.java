e7514b4b22c2c9992a6769e135c80722a21f62c5
Meine Toonen <toonen.meine@gmail.com>
2019-05-07 09:40:19 +0200

    added auditmessageobject. typo

--- viewer/src/main/java/nl/b3p/viewer/stripes/SimplifyFeatureActionBean.java
+++ viewer/src/main/java/nl/b3p/viewer/stripes/SimplifyFeatureActionBean.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2019 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package nl.b3p.viewer.stripes;
 
@@ -128,7 +139,20 @@ public class SimplifyFeatureActionBean extends LocalizableApplicationActionBean 
 
     @Override
     public AuditMessageObject getAuditMessageObject() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        AuditMessageObject aum = new AuditMessageObject();
+        
+        if(solrconfig != null){
+            aum.addMessage("SolrConfig: " + solrconfig.getId());
+        }
+        if(appLayer != null){
+            aum.addMessage("appLayer: " + appLayer.getId());
+        }
+        aum.addMessage("application: " + application.getId());
+        aum.addMessage("featureId: " + featureId);
+        if(sft != null){
+            aum.addMessage("sft: " + sft.getId());
+        }
+        return aum;
     }
 
     @DefaultHandler
