d0e24f52620fc3ada245c621f924ae51a654abe0
Geert Plaisier <geert.plaisier@gnamic.nl>
2019-05-16 15:42:41 +0200

    Fixes JS error in for Disclaimer component
    See mantis#13635

--- viewer/src/main/webapp/viewer-html/components/Disclaimer.js
+++ viewer/src/main/webapp/viewer-html/components/Disclaimer.js
@@ -84,7 +84,7 @@ Ext.define ("viewer.components.Disclaimer",{
         var hash = 0, i, chr;
         if (str.length === 0) return hash;
         for (i = 0; i < str.length; i++) {
-            chr   = String.charCodeAt(i);
+            chr   = str.charCodeAt(i);
             hash  = ((hash << 5) - hash) + chr;
             hash |= 0;
         }
