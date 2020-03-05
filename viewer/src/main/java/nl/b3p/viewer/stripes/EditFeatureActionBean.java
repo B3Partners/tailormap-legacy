af6d1f19b61ff0a2be49680a59649476c1009043
Meine Toonen <toonen.meine@gmail.com>
2020-03-05 11:12:09 +0100

    Remove audittrail, fixes bulkedit when using gemeentes as featuresource

--- viewer/src/main/java/nl/b3p/viewer/stripes/EditFeatureActionBean.java
+++ viewer/src/main/java/nl/b3p/viewer/stripes/EditFeatureActionBean.java
@@ -196,7 +196,6 @@ public class EditFeatureActionBean extends LocalizableApplicationActionBean impl
                     break;
                 }
                 store = (SimpleFeatureStore)fs;
-                addAuditTrailLog();
                 jsonFeature = getJsonFeature(feature);
                 if (!this.isFeatureWriteAuthorized(appLayer,jsonFeature,context.getRequest())){
                      error = getBundle().getString("viewer.editfeatureactionbean.6");
@@ -606,39 +605,5 @@ public class EditFeatureActionBean extends LocalizableApplicationActionBean impl
             }
         }
         return true;
-    }
-
-    /**
-     * Method to query the datastore with a dummy query, containing the username. This is used for an audittrail.
-     * A query is composed using the
-     * first attribute from the type, and constructing a Query with it:
-     * {@code <firstattribute> = 'username is <username>'}.
-     */
-    private void addAuditTrailLog() {
-        try{
-            List<AttributeDescriptor> attributeDescriptors = store.getSchema().getAttributeDescriptors();
-            String typeName = null;
-            for (AttributeDescriptor ad : attributeDescriptors) {
-                // Get an attribute of type string. This because the username is almost always a string, and passing it to a Integer/Double will result in a invalid
-                // query which will not log the passed values (possibly because the use of geotools).
-                if (ad.getType().getBinding() == String.class) {
-                    typeName = ad.getLocalName();
-                    break;
-                }
-            }
-
-            if (typeName == null) {
-                typeName = store.getSchema().getAttributeDescriptors().get(0).getLocalName();
-                log.warn("Audittrail: cannot find attribute of type double/integer or string. Take the first attribute.");
-            }
-            String username = context.getRequest().getRemoteUser();
-            String[] dummyValues = new String[]{"a", "b"}; // use these values for creating a statement which will always fail: attribute1 = a AND attribute1 = b.
-            String valueToInsert = "username = " + username;
-            store.modifyFeatures(typeName, valueToInsert, CQL.toFilter(typeName + " = '" + dummyValues[0] + "' and " + typeName + " = '" + dummyValues[1] + "'"));
-
-        } catch (Exception ex) {
-            // Swallow all exceptions, because this inherently fails. It's only use is to log the application username, so it can be matched (via the database process id
-            // to the following insert/update/delete statement.
-        }
     }
 }
