96e665cec3bc00905efed05c8ad1cd4ce971a78a
Mark Prins <mark@b3partners.nl>
2020-06-02 13:53:03 +0200

    Bump hibernate.version 5.4.12.Final ➜ 5.4.28.Final

--- viewer-config-persistence/src/main/java/nl/b3p/viewer/config/services/FeatureTypeRelationKey.java
+++ viewer-config-persistence/src/main/java/nl/b3p/viewer/config/services/FeatureTypeRelationKey.java
@@ -16,11 +16,13 @@
  */
 package nl.b3p.viewer.config.services;
 
-import org.json.JSONObject;
-
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import org.json.JSONObject;
 
 /**
  *
