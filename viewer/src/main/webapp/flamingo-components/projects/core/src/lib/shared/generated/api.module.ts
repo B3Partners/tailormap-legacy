061a8d1ea992d1ddfb1aa849353956b22850bbbc
Meine Toonen <toonen.meine@gmail.com>
2020-08-24 14:56:13 +0200

    8-> 9

--- viewer/src/main/webapp/flamingo-components/projects/core/src/lib/shared/generated/api.module.ts
+++ viewer/src/main/webapp/flamingo-components/projects/core/src/lib/shared/generated/api.module.ts
@@ -19,7 +19,7 @@ import { WegvakonderdeelplanningControllerService } from './api/wegvakonderdeelp
     WegvakonderdeelplanningControllerService ]
 })
 export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
         return {
             ngModule: ApiModule,
             providers: [ { provide: Configuration, useFactory: configurationFactory } ]
