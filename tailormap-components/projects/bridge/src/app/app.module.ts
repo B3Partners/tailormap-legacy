08713e8d302e56f0d79e23afe7d4a67cbc77ec18
Meine Toonen <toonen.meine@gmail.com>
2020-09-03 11:26:56 +0200

    Feat/gbiw 140 rename tags and folders (#1972)
    * Added angular components directory
    
    * removed old dir
    
    * changed filenames and pom for running build from different folder
    
    * added gitignore for dist folder
    
    * renamed viewer-components -> tailormap-components
    
    * rename tags
    
    * http-> https
    
    * Removed test stuff
    
    * removed openapi dep

---
+++ tailormap-components/projects/bridge/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from 'projects/core/src';
+import { FormComponent } from 'projects/core/src/lib/feature-form/form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormPopupComponent } from 'projects/core/src/lib/feature-form/form-popup/form-popup.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+  ],
+  entryComponents: [
+    FormPopupComponent,
+  ],
+  bootstrap: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+})
+export class AppModule {
+  constructor(injector: Injector) {
+    customElements.define('tailormap-wegvak-popup', createCustomElement(FormPopupComponent, {injector}));
+  }
+  public ngDoBootstrap() {}
+}
---
+++ tailormap-components/projects/core/src/lib/feature-form/form-tree/form-tree.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+
+
+import { FlatNode, FeatureNode } from './form-tree-models';
+import { FormConfiguration, FormConfigurations} from "../form/form-models";
+import {Feature} from "../../shared/generated";
+import {FormTreeHelpers} from "./form-tree-helpers";
+import {FormconfigRepositoryService} from "../../shared/formconfig-repository/formconfig-repository.service";
+
+@Component({
+  selector: 'tailormap-form-tree',
+  templateUrl: './form-tree.component.html',
+  styleUrls: ['./form-tree.component.css'],
+})
+export class FormTreeComponent implements OnInit,  OnChanges {
+
+  constructor(
+    private formConfigRepo: FormconfigRepositoryService,) {
+  }
+
+  @Output()
+  public nodeClicked = new EventEmitter<Feature>();
+
+  @Input()
+  public features: Feature[];
+
+  @Input()
+  public feature: Feature;
+
+  public treeControl = new FlatTreeControl<FlatNode>(node => node.level, node => node.expandable);
+
+  public treeFlattener = new MatTreeFlattener(
+    FormTreeHelpers.transformer, node => node.level, node => node.expandable, node => node.children);
+
+  public dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  public ngOnInit() {
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    this.dataSource.data = this.convertFeatureToNode(this.features);
+    this.treeControl.expandAll();
+  }
+
+  public setNodeSelected(node: FlatNode) {
+    this.nodeClicked.emit(node.feature);
+  }
+
+  private convertFeatureToNode(features: Feature[]): FeatureNode[] {
+      const nodes: FeatureNode[] = [];
+      features.forEach(feature => {
+        const children: FeatureNode[] = [];
+        if (feature.children ) {
+          const fts = {};
+          feature.children.forEach((child: Feature) => {
+            const featureType = child.clazz;
+            if(this.formConfigRepo.getFormConfig(featureType)) {
+              if (!fts.hasOwnProperty(featureType)) {
+                fts[featureType] = {
+                  name: featureType,
+                  children: [],
+                  id: featureType,
+                  isFeatureType: true,
+                };
+              }
+              fts[featureType].children.push(this.convertFeatureToNode([child])[0]);
+            }
+          });
+          for (const key in fts) {
+            if (fts.hasOwnProperty(key)) {
+              const child = fts[key];
+              children.push(child);
+            }
+          }
+        }
+        nodes.push({
+            name: this.getNodeLabel(feature),
+            children,
+            object_guid: feature.object_guid,
+            feature,
+            selected: feature.object_guid === this.feature.object_guid,
+            isFeatureType: false,
+          });
+      });
+      return nodes;
+  }
+
+  private getNodeLabel (feature:Feature) :string{
+    const config : FormConfiguration = this.formConfigRepo.getFormConfig(feature.clazz);
+    let label = this.getFeatureValue(feature, config.treeNodeColumn);
+    if(config.idInTreeNodeColumn){
+      let id = feature.object_guid;
+
+      label = (label ? label : config.name) + ' (id: ' + id + ')';
+    }
+    return label;
+  }
+
+  private getFeatureValue(feature: Feature, key: string): any {
+    let val = feature[key];
+    return val;
+  }
+
+  public getNodeClassName(node: FlatNode) {
+    const treeNodeBaseClass = 'tree-node-wrapper';
+
+    const cls = [
+      treeNodeBaseClass,
+      node.expandable ? `${treeNodeBaseClass}--folder` : `${treeNodeBaseClass}--leaf`,
+      `${treeNodeBaseClass}--level-${node.level}`,
+    ];
+
+    if (node.selected) {
+      cls.push(`${treeNodeBaseClass}--selected`);
+    }
+
+    return cls.join(' ');
+  }
+  public hasChild = (_: number, node: FlatNode) => node.expandable;
+}
