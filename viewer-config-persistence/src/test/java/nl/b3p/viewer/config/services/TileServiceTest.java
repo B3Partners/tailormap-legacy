b0f27a29290213b703fc0cd3b35d683ebbeded87
Meine Toonen <toonen.meine@gmail.com>
2020-09-03 08:34:07 +0200

    http-> https

--- viewer-config-persistence/src/test/java/nl/b3p/viewer/config/services/TileServiceTest.java
+++ viewer-config-persistence/src/test/java/nl/b3p/viewer/config/services/TileServiceTest.java
@@ -142,7 +142,7 @@ public class TileServiceTest extends TestUtil{
     
     @Test
     public void testLoadArcGisWMTSFromURL() throws MalformedURLException {
-        URL u = new URL("http://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_1950/MapServer/WMTS?request=getcapabilities");
+        URL u = new URL("https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_1950/MapServer/WMTS?request=getcapabilities");
         String url = u.toString();
         Map params = new HashMap();
         params.put(TileService.PARAM_TILINGPROTOCOL, "WMTS");
