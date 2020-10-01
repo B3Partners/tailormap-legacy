d8eb09406a40ee7dfdb1aed2459cfbc302f00724
Meine Toonen <toonen.meine@gmail.com>
2020-10-01 12:00:58 +0200

    Lint fixes

--- tailormap-components/projects/core/src/lib/feature-form/form-creator/form-creator.component.ts
+++ tailormap-components/projects/core/src/lib/feature-form/form-creator/form-creator.component.ts
@@ -42,7 +42,6 @@ export class FormCreatorComponent implements OnChanges, OnDestroy, AfterViewInit
     private actions: FormActionsService,
     private registry: LinkedAttributeRegistryService,
     private _snackBar: MatSnackBar) {
-    const a = 0;
   }
 
   @Input()
