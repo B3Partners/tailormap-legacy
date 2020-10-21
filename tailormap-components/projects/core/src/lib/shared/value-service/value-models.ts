b3204ac54849bdcd88cbfe96495f0ab5a8a53773
RinkeHeida <git4HEI>
2020-10-21 10:07:35 +0200

    value-service toegevoegd voor ophalen unique waarden (RH)

---
+++ tailormap-components/projects/core/src/lib/shared/value-service/value-models.ts
@@ -0,0 +1,20 @@
+// Parameters as defined in viewer/src/main/java/nl/b3p/viewer/stripes/UniqueValuesActionBean.java
+
+export interface ValueParameters {
+  applicationLayer: number;  //appLayer in export-models?
+  featureType?: number;
+  attributes: string[];
+  attribute?: string;
+  operator?: string;
+  maxFeatures?: number;
+  filter?: string;
+}
+
+export interface UniqueValuesResponse {
+  uniqueValues: AttributeUniqueValues[];
+  success: boolean;
+}
+
+export interface AttributeUniqueValues {
+  [key: string]: string[];
+}
---
+++ tailormap-components/projects/core/src/lib/shared/value-service/value.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams,
+} from '@angular/common/http';
+import {
+  UniqueValuesResponse,
+  ValueParameters,
+} from './value-models';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ValueService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public uniqueValues (params: ValueParameters): any {
+    let httpParams: HttpParams = new HttpParams();
+    Object.entries(params).forEach(([key, value ]) => {
+      httpParams = httpParams.set(key, String(value));
+    });
+    return this.http.get<UniqueValuesResponse>('/viewer/action/uniquevalues', {params: httpParams, observe: 'response'});
+  }
+
+}
