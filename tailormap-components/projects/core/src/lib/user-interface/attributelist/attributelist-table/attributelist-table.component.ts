90519f6b71c00b36339a6c95bcf9ae6c8ca6af1d
RinkeHeida <git4HEI>
2020-11-24 12:00:31 +0100

    Paging gerepareerd bij filteren.

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-table/attributelist-table.component.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-table/attributelist-table.component.ts
@@ -314,6 +314,7 @@ export class AttributelistTableComponent implements AttributelistTable, OnInit, 
               this.filter.layerFilterValues.columns[colIndex].status = false;
             }
             this.dataSource.params.valueFilter = this.filter.createFilter();
+            this.paginator.pageIndex = 0;
             this.updateTable();
             this.setFilterInAppLayer();
           }
