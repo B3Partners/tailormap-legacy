3461effedc81163a647640e418dac9d52116251c
RinkeHeida <git4HEI>
2020-11-25 12:55:28 +0100

    test opgeschoond

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-statistic.spec.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-statistic.spec.ts
@@ -1,8 +1,5 @@
 import { AttributeDataSource } from '../attributelist-common/attributelist-datasource';
 import { AttributelistStatistic } from './attributelist-statistic';
-import { AttributeService } from '../../../shared/attribute-service/attribute.service';
-import { FormconfigRepositoryService } from '../../../shared/formconfig-repository/formconfig-repository.service';
-import { LayerService } from '../layer.service';
 import { StatisticService } from '../../../shared/statistic-service/statistic.service';
 import {
   ComponentFixture,
@@ -11,18 +8,12 @@ import {
 
 describe('AttributelistStatistic', () => {
   let statisticsService: StatisticService;
-  let attributeService: AttributeService;
-  let formconfigRepoService: FormconfigRepositoryService;
-  let layerService: LayerService;
   let dataSource: AttributeDataSource;
   let fixture: ComponentFixture<AttributeDataSource>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     statisticsService = TestBed.inject(StatisticService);
-    attributeService = TestBed.inject(AttributeService);
-    formconfigRepoService = TestBed.inject(FormconfigRepositoryService);
-    layerService = TestBed.inject(LayerService);
     fixture = TestBed.createComponent(AttributeDataSource);
     dataSource  = fixture.componentInstance;
   });
