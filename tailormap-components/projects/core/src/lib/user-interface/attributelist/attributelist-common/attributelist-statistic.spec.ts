1008d3804a4c02fc4ac3c4587d712bd12066a1e9
RinkeHeida <git4HEI>
2020-11-25 12:24:18 +0100

    test uitgebreid

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-statistic.spec.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-statistic.spec.ts
@@ -1,19 +1,36 @@
+import { AttributeDataSource } from '../attributelist-common/attributelist-datasource';
 import { AttributelistStatistic } from './attributelist-statistic';
+import { AttributeService } from '../../../shared/attribute-service/attribute.service';
+import { FormconfigRepositoryService } from '../../../shared/formconfig-repository/formconfig-repository.service';
+import { LayerService } from '../layer.service';
 import { StatisticService } from '../../../shared/statistic-service/statistic.service';
-import { TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
 
 describe('AttributelistStatistic', () => {
   let statisticsService: StatisticService;
+  let attributeService: AttributeService;
+  let formconfigRepoService: FormconfigRepositoryService;
+  let layerService: LayerService;
+  let dataSource: AttributeDataSource;
+  let fixture: ComponentFixture<AttributeDataSource>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     statisticsService = TestBed.inject(StatisticService);
+    attributeService = TestBed.inject(AttributeService);
+    formconfigRepoService = TestBed.inject(FormconfigRepositoryService);
+    layerService = TestBed.inject(LayerService);
+    fixture = TestBed.createComponent(AttributeDataSource);
+    dataSource  = fixture.componentInstance;
   });
 
   it('should create an instance', () => {
     expect(new AttributelistStatistic(
-      this.statisticsService,
-      this.dataSource,
+      statisticsService,
+      dataSource,
     )).toBeTruthy();
   });
 });
