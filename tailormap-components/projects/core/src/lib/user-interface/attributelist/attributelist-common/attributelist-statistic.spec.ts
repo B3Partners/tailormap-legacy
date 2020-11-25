6d79a69712ebceb0792ba694da6028414094c949
RinkeHeida <git4HEI>
2020-11-25 11:18:27 +0100

    test gerepareerd

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-statistic.spec.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-statistic.spec.ts
@@ -1,7 +1,19 @@
 import { AttributelistStatistic } from './attributelist-statistic';
+import { StatisticService } from '../../../shared/statistic-service/statistic.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('AttributelistStatistic', () => {
+  let statisticsService: StatisticService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    statisticsService = TestBed.inject(StatisticService);
+  });
+
   it('should create an instance', () => {
-    expect(new AttributelistStatistic()).toBeTruthy();
+    expect(new AttributelistStatistic(
+      this.statisticsService,
+      this.dataSource,
+    )).toBeTruthy();
   });
 });
