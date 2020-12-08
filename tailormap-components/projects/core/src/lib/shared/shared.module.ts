9b001469ea4e1bf09cdb7939e06362a5c1356a15
Meine Toonen <toonen.meine@gmail.com>
2020-12-08 10:40:51 +0100

    Edit bar created

--- tailormap-components/projects/core/src/lib/shared/shared.module.ts
+++ tailormap-components/projects/core/src/lib/shared/shared.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core';
+import {
+  NgModule,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import {
@@ -31,6 +33,7 @@ import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.componen
 import { ConfirmDialogService } from './confirm-dialog/confirm-dialog.service';
 import { ApiModule } from './generated';
 import { DomSanitizer } from '@angular/platform-browser';
+import { TailorMapService } from '../../../../bridge/src/tailor-map.service';
 
 @NgModule({
   declarations: [
@@ -97,9 +100,17 @@ export class SharedModule {
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
   ) {
-    this.matIconRegistry.addSvgIcon(
-      'pietje',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:3200/assets/imgs/pietje.svg'),
-    );
+
+    const url = '/viewer/tailormap-components/bridge/assets/imgs/';
+    const icons = ['draw_polygon', 'draw_line', 'draw_point', 'split', 'new_object', 'merge'];
+    icons.forEach(value => {
+      this.matIconRegistry.addSvgIcon(
+        value,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          url
+          + value + '.svg'),
+      );
+
+    });
   }
 }
--- tailormap-components/projects/core/src/lib/workflow/workflow-controller/workflow-controller.component.ts
+++ tailormap-components/projects/core/src/lib/workflow/workflow-controller/workflow-controller.component.ts
@@ -8,6 +8,9 @@ import { WorkflowFactoryService } from '../workflow-factory/workflow-factory.ser
 import { VectorLayer } from '../../../../../bridge/typings';
 import { TailorMapService } from '../../../../../bridge/src/tailor-map.service';
 import { MapClickedEvent } from '../../shared/models/event-models';
+import { AddFeatureMenuComponent } from '../../user-interface/edit-bar/add-feature-menu/add-feature-menu.component';
+import { MatDialog } from '@angular/material/dialog';
+import { EditBarComponent } from '../../user-interface/edit-bar/edit-bar/edit-bar.component';
 
 @Component({
   selector: 'tailormap-workflow-controller',
@@ -21,7 +24,20 @@ export class WorkflowControllerComponent implements OnInit {
     private controller: WorkflowControllerService,
     private factory: WorkflowFactoryService,
     private tailorMap: TailorMapService,
+    public dialog: MatDialog,
   ) {
+    const dialogRef = this.dialog.open(EditBarComponent, {
+      width: '150px',
+      position: {
+        top: '100px',
+        left: '580px',
+      },
+      height: '60px',
+      disableClose: true,
+      hasBackdrop: false,
+      panelClass: 'panelClass',
+
+    });
   }
 
   @Input()
