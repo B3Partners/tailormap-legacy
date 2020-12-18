a7d050a7f2142cbad4859b8fa513e28174ec7ec4
RinkeHeida <git4HEI>
2020-12-18 15:49:57 +0100

    GBIW-249 meer filtertypes uitproberen gebruik helper

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-filter-values-form/attributelist-filter-values-form.component.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-filter-values-form/attributelist-filter-values-form.component.ts
@@ -7,7 +7,12 @@ import {
   MAT_DIALOG_DATA,
   MatDialogRef,
 } from '@angular/material/dialog';
+import { AttributeTypeEnum } from '../../../analysis/models/attribute-type.enum';
+import { CriteriaHelper } from '../../../analysis/criteria/helpers/criteria.helper'
+import { CriteriaModel } from '../../../analysis/models/criteria.model';
 import { FilterValueSettings } from '../attributelist-common/attributelist-filter-models';
+import { CriteriaConditionModel } from '../../../analysis/models/criteria-condition.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'tailormap-attributelist-filter-values-form',
@@ -17,18 +22,29 @@ import { FilterValueSettings } from '../attributelist-common/attributelist-filte
 export class AttributelistFilterValuesFormComponent implements OnInit {
 
   public values: FilterValueSettings[];
+  public criteriaValue = new FormControl();
   public colName: string;
 
   public allOn: boolean
 
-  public filterTypeSelected: string;
+  public criteria: CriteriaConditionModel;
+
+  public filterTypeSelected: string = '';
   public filterTypes: string[] = ['Contains', 'NotContains', 'UniqueValue'];
 
   constructor(private dialogRef: MatDialogRef<AttributelistFilterValuesFormComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
     this.colName = data.colName;
     this.values = data.values;
-    this.allOn = true
+    this.allOn = true;
+    this.criteria = new class implements CriteriaConditionModel {
+      attribute: string;
+      attributeType: AttributeTypeEnum;
+      condition: string;
+      id: string;
+      source: number;
+      value: string;
+    };
     this.updateSelected();
   }
 
@@ -65,6 +81,15 @@ export class AttributelistFilterValuesFormComponent implements OnInit {
     } else {
       // no values are selected, so set the filter on NULL values
       filterSetting = 'NONE'
+    }
+    if (this.filterTypeSelected !== '' && this.filterTypeSelected !== 'UniqueValues') {
+      this.criteria.id  = this.colName;
+      this.criteria.attribute = this.colName;
+      this.criteria.attributeType = AttributeTypeEnum.STRING; //CriteriaHelper.getAttributeType
+      this.criteria.condition = this.filterTypeSelected;
+      this.criteria.value = this.criteriaValue.value;
+      // ter test even kijken of dit kan gaan werken:
+      console.log("condition: " + CriteriaHelper.convertConditionToQuery(this.criteria));
     }
 
     this.dialogRef.close(filterSetting);
