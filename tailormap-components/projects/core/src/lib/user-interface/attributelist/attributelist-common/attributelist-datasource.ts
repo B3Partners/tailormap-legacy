c1f58af0a3e8368c9b6082196fc04bd54f054769
Meine Toonen <toonen.meine@gmail.com>
2020-12-24 11:54:55 +0100

    GBIW 314 edit objects from userlayer (#2190)
    * edit objects from userlayer
    
    * also enable to edit features from attributelist. For userlayers, enable the geometry column

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-datasource.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-common/attributelist-datasource.ts
@@ -43,6 +43,7 @@ import {
   FormFieldType,
 } from '../../../feature-form/form/form-models';
 import { FormFieldHelpers } from '../../../feature-form/form-field/form-field-helpers';
+import { TailorMapService } from '../../../../../../bridge/src/tailor-map.service';
 
 export class AttributeDataSource extends DataSource<any> {
 
@@ -68,6 +69,7 @@ export class AttributeDataSource extends DataSource<any> {
 
   constructor(private layerService: LayerService,
               private attributeService: AttributeService,
+              private tailorMapService: TailorMapService,
               private formconfigRepoService: FormconfigRepositoryService) {
     super();
   }
@@ -147,7 +149,8 @@ export class AttributeDataSource extends DataSource<any> {
         if (row.geometrie) {
           rest.geometrie =  wellknown.parse(row.geometrie);
         }
-        const className = LayerUtils.sanitizeLayername(this.getLayerName().toLowerCase());
+        const appLayer = this.tailorMapService.getApplayerById(this.params.layerId);
+        const className = LayerUtils.sanitizeLayername(appLayer);
         feature.children = [];
         feature.clazz = className;
         feature.objectGuid = row.object_guid;
