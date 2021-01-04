cb73832a17fe3dde2c1ac9ad289b55160a140011
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-01-04 14:16:58 +0100

    Allow to select related source for criteria in frontend

--- tailormap-components/projects/core/src/lib/analysis/models/criteria-condition.model.ts
+++ tailormap-components/projects/core/src/lib/analysis/models/criteria-condition.model.ts
@@ -3,6 +3,7 @@ import { AttributeTypeEnum } from '../../application/models/attribute-type.enum'
 export interface CriteriaConditionModel {
   id: string;
   source?: number;
+  relatedTo?: number;
   attribute?: string;
   attributeType?: AttributeTypeEnum;
   condition?: string;
