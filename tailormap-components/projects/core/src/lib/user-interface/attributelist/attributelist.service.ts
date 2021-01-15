e253247b11826640c42dfef6fc0100e067e076b9
Martijn <m_struijk1990@hotmail.nl>
2021-01-15 12:06:55 +0100

    added subject and observable for calling event for loading table data

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist.service.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist.service.ts
@@ -17,13 +17,21 @@ export class AttributelistService {
 
   public config: AttributelistConfig;
 
+  private loadTableDataSubject$ = new Subject();
+
   private updateTreeDataSubject$ = new Subject<AttributelistNode[]>();
 
   private selectedTreeDataSubject$ = new Subject<SelectedTreeData>(); // source voor Observable
 
+  public loadTableData$ = this.loadTableDataSubject$.asObservable();
+
   public updateTreeData$ = this.updateTreeDataSubject$.asObservable();
 
   public selectedTreeData$ = this.selectedTreeDataSubject$.asObservable(); // ingang naar Observable
+
+  public loadTableData(): void {
+    this.loadTableDataSubject$.next();
+  }
 
   public updateTreeData(treeData: AttributelistNode[]): void {
     this.updateTreeDataSubject$.next(treeData);
