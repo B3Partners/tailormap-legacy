9dc0d89c3a0484f5c45f457419d016d15b02dc4f
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-01-19 23:52:38 +0100

    Fixed test

--- tailormap-components/projects/core/src/lib/analysis/analysis-button/analysis-button.component.spec.ts
+++ tailormap-components/projects/core/src/lib/analysis/analysis-button/analysis-button.component.spec.ts
@@ -1,14 +1,14 @@
 import { AnalysisButtonComponent } from './analysis-button.component';
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
-import { analysisStateKey, initialAnalysisState } from '../state/analysis.state';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { SharedModule } from '../../shared/shared.module';
 import { getUserLayerServiceMockProvider } from '../services/mocks/user-layer.service.mock';
+import { defaultRootState } from '../../state/root-state.model';
 
 describe('AnalysisButtonComponent', () => {
 
   let spectator: Spectator<AnalysisButtonComponent>;
-  const initialState = { [analysisStateKey]: initialAnalysisState };
+  const initialState = { ...defaultRootState };
   let store: MockStore;
 
   const createComponent = createComponentFactory({
