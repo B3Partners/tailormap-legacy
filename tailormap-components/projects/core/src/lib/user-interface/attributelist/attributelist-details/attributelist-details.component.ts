ad45916e3e0ca65c339d5fc6f401c9658ad613f3
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-02-13 23:26:59 +0100

    Removed some unneeded code

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-details/attributelist-details.component.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attributelist-details/attributelist-details.component.ts
@@ -1,13 +1,8 @@
-import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Sort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-
-import { AttributelistTable } from '../attributelist-common/attributelist-models';
 import { AttributeService } from '../../../shared/attribute-service/attribute.service';
-import { AttributeListParameters, RelatedFeatureType } from '../../../shared/attribute-service/attribute-models';
+import { AttributeListFeature, AttributeListParameters, RelatedFeatureType } from '../../../shared/attribute-service/attribute-models';
 import { Observable, Subject } from 'rxjs';
-import { AttributeListRowModel } from '../models/attribute-list-row.model';
 import { AttributeListState } from '../state/attribute-list.state';
 import { Store } from '@ngrx/store';
 import { selectActiveColumnsForFeature } from '../state/attribute-list.selectors';
@@ -20,24 +15,16 @@ import { AttributeListColumnModel } from '../models/attribute-list-column-models
   templateUrl: './attributelist-details.component.html',
   styleUrls: ['./attributelist-details.component.css'],
 })
-// export class AttributelistDetailsComponent implements OnInit, AttributelistTable {
-export class AttributelistDetailsComponent implements OnInit,
-  OnDestroy,
-  AttributelistTable,
-  AfterViewInit {
 
-  // Table reference for 'manually' rendering.
-  @ViewChild('detailtable') public table: MatTable<any>;
+export class AttributelistDetailsComponent implements OnInit, OnDestroy {
 
-  // The parent layer id for this detail table.
   @Input()
   public parentLayerId: string;
 
-  // The parent row featuretype for this detail table.
   @Input()
   public featureType: RelatedFeatureType;
 
-  public rows$: Observable<AttributeListRowModel[]>;
+  public rows$: Observable<AttributeListFeature[]>;
 
   public columnsNames: string[];
   public columns: AttributeListColumnModel[];
@@ -60,31 +47,14 @@ export class AttributelistDetailsComponent implements OnInit,
       this.columns = columns;
       this.columnsNames = columns.map(column => column.name);
     })
-    // Update the table.
     this.updateTable();
   }
 
-  public ngAfterViewInit(): void {
-  }
-
-  /**
-   * Renders the table rows after the data is loaded.
-   */
-  public onAfterLoadData(): void {
-  }
-
-  /**
-   * Fired when a column header is clicked.
-   */
   public onSortClick(sort: Sort): void {
     this.updateTable(sort);
   }
 
-  /**
-   * (Re)loads the data, which fires the "onAfterLoadData" method.
-   */
   private updateTable(sort?: Sort): void {
-
     const attrParams: AttributeListParameters = {
       application: this.applicationService.getId(),
       appLayer: +(this.parentLayerId),
@@ -96,23 +66,7 @@ export class AttributelistDetailsComponent implements OnInit,
     this.rows$ = this.attributeService.features$(attrParams)
       .pipe(
         takeUntil(this.destroyed),
-        map(response => {
-          if (response.success) {
-            return response.features.map(feature => {
-              const a: AttributeListRowModel = {
-                ...feature,
-                rowId: '' + feature.__fid,
-                _checked: false,
-                _expanded: false,
-                _selected: false,
-
-              };
-              return a;
-            });
-          } else {
-            return null;
-          }
-        }),
+        map(response => response.success ? response.features : []),
       );
   }
 }
