5f166d48bdaeb034fde7627e31aa1292b0f2f15f
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-02-16 00:11:20 +0100

    Updated icon + styling for details

--- tailormap-components/projects/core/src/lib/shared/icons/icon.service.ts
+++ tailormap-components/projects/core/src/lib/shared/icons/icon.service.ts
@@ -12,6 +12,7 @@ export class IconService {
     'draw_polygon', 'draw_line', 'draw_point', 'split', 'new_object', 'merge',
     'contextual_drag', 'contextual_resize', 'contextual_chevron_bottom', 'contextual_chevron_left', 'contextual_chevron_right', 'contextual_chevron_top',
     'contextual_close', 'contextual_minimize', 'contextual_drop_down', 'contextual_drop_top',
+    'contextual_expand_close', 'contextual_expand_open',
     'interface_trash_filled',
     { folder: 'markers', icons: [ 'arrow', 'circle', 'cross', 'square', 'star', 'triangle', 'x' ] },
     { folder: 'components', icons: [ 'attributelist' ] },
