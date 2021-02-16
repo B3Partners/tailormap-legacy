a244049cb924b47dc6674add6e1e96f592df3653
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-02-16 11:56:17 +0100

    Remove Jenkins GH and Appveyor configs, this branch has no active development and no longer needs any CI (#2340)

--- .jenkins/insert-fla-version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# look for "INSERT INTO metadata (id"... in ./viewer-config-persistence/src/test/resources/nl/b3p/viewer/util/testdata.sql
-#  to get the current database version to be inserted
-#
-fladbversion=$(grep 'INSERT INTO metadata (id' ./viewer-config-persistence/src/test/resources/nl/b3p/viewer/util/testdata.sql)"
-sqlplus JENKINS_FLAMINGO/jenkins_flamingo@192.168.1.11:1521/orcl <<< ${fladbversion}"
