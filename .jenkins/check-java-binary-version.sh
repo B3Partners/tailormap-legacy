3e74a54c46f0f228eba1c443972908507802f01f
Mark Prins <1165786+mprins@users.noreply.github.com>
2021-02-20 13:33:35 +0100

    Update check-java-binary-version.sh

--- .jenkins/check-java-binary-version.sh
+++ .jenkins/check-java-binary-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# check of class file is java 8 == 52
+# check class file is java 8 == 52
 set -eE
-if [ $(javap -verbose -cp viewer-audit/target/classes/ nl.b3p.viewer.audit.AuditMessageObject | grep -c 'major version: 52') == 1 ]; then
+if [ $(javap -verbose -cp viewer-audit/target/classes/ nl.b3p.viewer.audit.LoggingServiceFactory | grep -c 'major version: 52') == 1 ]; then
   printf "\nOK: Java 8 classfile\n\n"
   exit 0
 else
