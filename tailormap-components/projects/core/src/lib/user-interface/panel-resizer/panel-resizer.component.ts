38da84ba4080a923e54005f6d1e59d9fd7095ee4
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-04-07 13:14:20 +0200

    16019: Fixes resizing of Attribute list (#2546)

--- tailormap-components/projects/core/src/lib/user-interface/panel-resizer/panel-resizer.component.ts
+++ tailormap-components/projects/core/src/lib/user-interface/panel-resizer/panel-resizer.component.ts
@@ -1,42 +1,69 @@
-import { Component, HostListener } from '@angular/core';
-import { LayoutService } from '../layout.service';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { finalize, map, switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'tailormap-panel-resize',
   templateUrl: './panel-resizer.component.html',
   styleUrls: ['./panel-resizer.component.css'],
 })
-export class PanelResizerComponent {
+export class PanelResizerComponent implements OnInit {
 
-  public visible = true;
-  private dragStartY = 0;
-  private isMouseDown = false;
+  @ViewChild('resizer', { static: true })
+  public resizer: ElementRef<HTMLDivElement>;
 
-  constructor(
-    private layoutService: LayoutService,
-  ) {}
+  @Input()
+  public orientation: 'horizontal' | 'vertical' = 'vertical';
 
-  public onHandleMouseDown(event: MouseEvent): void {
-    this.isMouseDown = true;
-    this.dragStartY = event.screenY;
+  @Output()
+  public positionChanged: EventEmitter<number> = new EventEmitter<number>();
+
+  private position: number;
+
+  public resizing: boolean;
+
+  constructor() {}
+
+  public ngOnInit() {
+    fromEvent(this.resizer.nativeElement.querySelector('.resize-handle'), 'mousedown')
+      .pipe(
+        map((event: MouseEvent) => this.orientation === 'horizontal' ? event.pageX : event.pageY),
+        switchMap((initialPosition: number) => {
+          this.startResize();
+          return fromEvent(document, 'mousemove')
+            .pipe(
+              map((event: MouseEvent) => {
+                if (this.orientation === 'horizontal') {
+                  return event.pageX - initialPosition;
+                }
+                return event.pageY - initialPosition;
+              }),
+              takeUntil(fromEvent(document, 'mouseup')),
+              finalize(() => this.resizeComplete()),
+            );
+        }),
+      )
+      .subscribe(result => {
+        this.position = result;
+        this.updateResizeIndicatorPosition(result);
+      });
   }
 
-  @HostListener('window:mousemove', ['$event'])
-  public onWindowMouseMove(event: MouseEvent): void {
-    if (this.isMouseDown) {
-      const deltaY = event.screenY - this.dragStartY;
-      this.layoutService.updateComponent(this, deltaY);
-      this.dragStartY += deltaY;
-    }
+  private startResize() {
+    this.resizing = true;
+    this.position = 0;
+    document.body.classList.add('resize-active');
+    this.updateResizeIndicatorPosition(0);
   }
 
-  @HostListener('window:mouseup', ['$event'])
-  public onWindowMouseUp(): void {
-    this.isMouseDown = false;
+  private resizeComplete() {
+    document.body.classList.remove('resize-active');
+    this.positionChanged.emit(this.position);
+    this.resizing = false;
   }
 
-  public showHandle(show: boolean): void {
-    this.visible = show;
+  private updateResizeIndicatorPosition(position: number) {
+    this.resizer.nativeElement.style.setProperty('--translate-pos', position + 'px');
   }
 
 }
