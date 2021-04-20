44679237269cad6edb28c63b0a60a188acb8b76c
mvdstruijk <martijnvdstruijk@b3partners.nl>
2021-04-20 10:59:28 +0200

    Show message for creating userlayer from userlayer, this is not valid

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/attribute-list-tab-toolbar/attribute-list-tab-toolbar.component.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/attribute-list-tab-toolbar/attribute-list-tab-toolbar.component.ts
@@ -106,6 +106,10 @@ export class AttributeListTabToolbarComponent implements OnInit, OnDestroy {
   }
 
   public createUserLayer(): void {
+    if (this.tailorMapService.getApplayerById(+(this.layerId)).userlayer) {
+      this.snackbar.open('Er kunnen geen selectielagen op basis van andere selectielagen gemaakt worden', '', {duration: 5000});
+      return;
+    }
     const query = this.tailorMapService.getFilterString(+(this.layerId), false);
     if (!query) {
       this.snackbar.open('Stel eerst een filter in op de attributenlijst in om een laag te kunnen publiceren', '', {duration: 5000});
