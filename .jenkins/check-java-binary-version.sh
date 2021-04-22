739e9840cfbce84ceedc64c4256427d2e9bb809e
Matthijs Laan <matthijsln@gmail.com>
2021-04-22 14:35:57 +0200

    controleer alle .class bestanden met 'file' ipv 'javap' voor performance

--- .jenkins/check-java-binary-version.sh
+++ .jenkins/check-java-binary-version.sh
@@ -1,29 +1,16 @@
 #!/usr/bin/env bash
 # check of class file is java 8 == 52
-set -eE
 set _exitCode=0
 
-# van iedere module 1 classfile testen
-for clazz in 'form/target/classes/ nl.b3p.gbi.converter.Formulier' \
-            'solr-commons/target/classes/ nl.b3p.viewer.solr.SolrUpdateJob' \
-            'viewer/target/classes/ nl.b3p.viewer.util.FlamingoCQL' \
-            'viewer-admin/target/classes/ nl.b3p.viewer.admin.stripes.ApplicationActionBean' \
-            'viewer-audit/target/classes/ nl.b3p.viewer.audit.LoggingServiceFactory' \
-            'viewer-commons/target/classes/ nl.b3p.i18n.LocalizableActionBean' \
-            'viewer-config-persistence/target/classes/ nl.b3p.viewer.config.security.User' \
-            'web-commons/target/classes/ nl.b3p.web.stripes.ErrorMessageResolution'
+echo Controleer of alle .class bestanden Java 8 versie zijn...
 
-do
-  testvalues=($clazz)
-  printf "\nControleer classfile versie van ${testvalues[1]} in ${testvalues[0]}."
-  if [ $(javap -verbose -cp ${testvalues[0]} ${testvalues[1]} | grep -c 'major version: 52') == 1 ]; then
-    printf "\nOK: ${testvalues[1]} is een Java 8 classfile.\n"
-  else
-    printf "\nFAILED: ${testvalues[1]} is geen Java 8 classfile.\n"
-    _exitCode=1
-    _msg=' niet'
-  fi
-done
+while IFS= read -r -d '' clazz; do
+	file "$clazz" | grep -v 'version 52.0'
+	if [[ $? == 0 ]]; then
+		_exitCode=1
+		_msg=' niet'
+	fi
+done < <(find . -name "*.class" -print0)
 
 printf "\nControle$_msg succesvol afgerond.\n\n"
 exit $_exitCode
