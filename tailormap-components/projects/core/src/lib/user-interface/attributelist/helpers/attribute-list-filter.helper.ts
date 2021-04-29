b637061cb90f4392dbfa2f40a93d4660d16def19
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-04-29 14:58:53 +0200

    TM-68: Apply filters recursively

--- tailormap-components/projects/core/src/lib/user-interface/attributelist/helpers/attribute-list-filter.helper.ts
+++ tailormap-components/projects/core/src/lib/user-interface/attributelist/helpers/attribute-list-filter.helper.ts
@@ -3,6 +3,8 @@ import { AttributeListFeatureTypeData } from '../models/attribute-list-feature-t
 import { AttributeFilterHelper } from '../../../shared/helpers/attribute-filter.helper';
 import { RelatedFilterHelper } from '../../../shared/helpers/related-filter.helper';
 import { RelatedFilterDataModel } from '../../../shared/models/related-filter-data.model';
+import { AttributeTypeHelper } from '../../../application/helpers/attribute-type.helper';
+import { AttributeTypeEnum } from '../../../shared/models/attribute-type.enum';
 
 export class AttributeListFilterHelper {
 
@@ -17,83 +19,46 @@ export class AttributeListFilterHelper {
       if (featureData.featureType === tab.featureType && extraLayerFilters) {
         filter.push(extraLayerFilters);
       }
+      const checkedRowsFilter = AttributeListFilterHelper.getQueryForCheckedRows(featureData, tabFeatureData);
+      if (checkedRowsFilter) {
+        filter.push(checkedRowsFilter);
+      }
       return {
         featureType: featureData.featureType,
         parentFeatureType: featureData.parentFeatureType,
-        filter: filter.join(' AND '),
+        filter: filter.filter(f => !!f).join(' AND '),
       };
     });
-    return RelatedFilterHelper.getFilter(tab.featureType, filterData).join(' AND ');
+    return RelatedFilterHelper.getFilter(featureType, filterData).filter(f => !!f).join(' AND ');
   }
 
-  // private static getQueryForFeatureType(
-  //   tab: AttributeListTabModel,
-  //   featureType: number,
-  //   filters: Map<number, string>,
-  //   isRelatedFeature: boolean,
-  //   mainFeatureData: AttributeListFeatureTypeData,
-  //   extraLayerFilters?: string,
-  // ) {
-  //   const featureFilter: string[] = [];
-  //   tab.relatedFeatures.map<string>(relation => {
-  //     if (relation.foreignFeatureType === featureType) {
-  //       return '';
-  //     }
-  //     const relationFilter = filters.get(relation.foreignFeatureType);
-  //     if (relationFilter) {
-  //       const filter = `RELATED_FEATURE(${tab.featureType},${relation.foreignFeatureType},(${relationFilter}))`;
-  //       if (isRelatedFeature) {
-  //         return `RELATED_FEATURE(${featureType},${tab.featureType},(${filter}))`;
-  //       }
-  //       return filter;
-  //     }
-  //     return '';
-  //   });
-  //   if (filters.has(featureType)) {
-  //     featureFilter.push(filters.get(featureType));
-  //   }
-  //   if (isRelatedFeature && filters.has(tab.featureType)) {
-  //     featureFilter.push(`RELATED_FEATURE(${featureType},${tab.featureType},(${filters.get(tab.featureType)}))`);
-  //   }
-  //   if (isRelatedFeature && extraLayerFilters) {
-  //     featureFilter.push(`RELATED_FEATURE(${featureType},${tab.featureType},(${extraLayerFilters})`);
-  //   }
-  //   if (!isRelatedFeature && extraLayerFilters) {
-  //     featureFilter.push(extraLayerFilters);
-  //   }
-  //   if (isRelatedFeature && mainFeatureData.checkedFeatures.length > 0) {
-  //     const checkedRowsFilter = AttributeListFilterHelper.getQueryForCheckedRows(tab, featureType, mainFeatureData);
-  //     if (checkedRowsFilter) {
-  //       featureFilter.push(checkedRowsFilter);
-  //     }
-  //   }
-  //   return featureFilter.filter(f => !!f).join(' AND ');
-  // }
-
-  // private static getQueryForCheckedRows(
-  //   tab: AttributeListTabModel,
-  //   featureType: number,
-  //   mainFeatureData: AttributeListFeatureTypeData,
-  // ) {
-  //   const currentRelation = tab.relatedFeatures.find(r => r.foreignFeatureType === featureType);
-  //   const selectedRowsFilter = [];
-  //   if (currentRelation) {
-  //     currentRelation.relationKeys.forEach(relation => {
-  //       const checkedFeatureKeys = new Set<string>();
-  //       mainFeatureData.checkedFeatures.forEach(checkedFeature => {
-  //         if (checkedFeature[relation.leftSideName]) {
-  //           checkedFeatureKeys.add(AttributeTypeHelper.getExpression(`${checkedFeature[relation.leftSideName]}`, AttributeTypeEnum.STRING));
-  //         }
-  //       });
-  //       if (checkedFeatureKeys.size !== 0) {
-  //         selectedRowsFilter.push(`${relation.rightSideName} IN (${Array.from(checkedFeatureKeys).join(',')})`);
-  //       }
-  //     });
-  //   }
-  //   if (selectedRowsFilter.length === 0) {
-  //     return '';
-  //   }
-  //   return `(${selectedRowsFilter.join(' AND ')})`;
-  // }
+  private static getQueryForCheckedRows(
+    featureData: AttributeListFeatureTypeData,
+    parentsFeatureData: AttributeListFeatureTypeData[],
+  ): string {
+    if (typeof featureData.parentFeatureType === 'undefined' || typeof featureData.parentAttributeRelationKeys === 'undefined') {
+      return '';
+    }
+    const selectedRowsFilter = [];
+    const parentFeature = parentsFeatureData.find(f => f.featureType === featureData.parentFeatureType);
+    if (!parentFeature) {
+      return '';
+    }
+    featureData.parentAttributeRelationKeys.forEach(relationKey => {
+      const checkedFeatureKeys = new Set<string>();
+      parentFeature.checkedFeatures.forEach(checkedFeature => {
+        if (checkedFeature[relationKey.parentAttribute]) {
+          checkedFeatureKeys.add(AttributeTypeHelper.getExpression(`${checkedFeature[relationKey.parentAttribute]}`, AttributeTypeEnum.STRING));
+        }
+      });
+      if (checkedFeatureKeys.size !== 0) {
+        selectedRowsFilter.push(`${relationKey.childAttribute} IN (${Array.from(checkedFeatureKeys).join(',')})`);
+      }
+    });
+    if (selectedRowsFilter.length === 0) {
+      return '';
+    }
+    return `(${selectedRowsFilter.join(' AND ')})`;
+  }
 
 }
