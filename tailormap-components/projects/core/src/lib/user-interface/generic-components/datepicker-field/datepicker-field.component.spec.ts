dfaa68879404c562c31bf9bdfc77c0ffca82d14f
mvdstruijk <m_struijk1990@hotmail.nl>
2021-06-16 11:01:56 +0200

    test-file for datepicker-field

--- tailormap-components/projects/core/src/lib/user-interface/generic-components/datepicker-field/datepicker-field.component.spec.ts
+++ tailormap-components/projects/core/src/lib/user-interface/generic-components/datepicker-field/datepicker-field.component.spec.ts
@@ -1,25 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DatepickerFieldComponent } from './datepicker-field.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { SharedModule } from '../../../shared/shared.module';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { FeatureAttribute, FormFieldType } from '../../../feature-form/form/form-models';
+import { FormControl, FormGroup } from '@angular/forms';
+
+const attribute: FeatureAttribute = {
+  dateFormat: 'YYYY-MM-DD',
+  value: '2021-06-16',
+  key: 'test',
+  label: 'test',
+  column: 1,
+  tab: 1,
+  type: FormFieldType.DATE,
+};
+
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'YYYY',
+  },
+};
 
 describe('DatepickerFieldComponent', () => {
-  let component: DatepickerFieldComponent;
-  let fixture: ComponentFixture<DatepickerFieldComponent>;
+  let spectator: Spectator<DatepickerFieldComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DatepickerFieldComponent ],
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: DatepickerFieldComponent,
+    imports: [ SharedModule ],
+    providers: [{provide: MAT_DATE_FORMATS, useValue: MY_FORMATS } ],
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DatepickerFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent({props: {
+        attribute,
+        groep: new FormGroup({ test: new FormControl('the value') })}});
+    spectator.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 });
