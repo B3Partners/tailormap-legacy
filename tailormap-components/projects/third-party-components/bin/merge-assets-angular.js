6e28b978562730bc20f39b25a1c8a0c3901a0054
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-07-08 14:59:44 +0200

    Added node script to merge assets into angular.json

---
+++ tailormap-components/projects/third-party-components/bin/merge-assets-angular.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const assets = require('../third-party-components-assets.json');
+const angular = require('../../../angular.json');
+
+function mergeAssets(angularAssets) {
+  assets.forEach(asset => {
+    if (angularAssets.findIndex(a => a.input === asset.input) === -1) {
+      angularAssets.push(asset);
+    }
+  });
+}
+
+function addAssets(project, step) {
+  if (project.architect[step] && project.architect[step].options && project.architect[step].options.assets) {
+    mergeAssets(project.architect[step].options.assets);
+  }
+}
+
+for (const project in angular.projects) if(angular.projects.hasOwnProperty(project)) {
+  if (angular.projects[project].architect) {
+    addAssets(angular.projects[project], 'build');
+    addAssets(angular.projects[project], 'test');
+  }
+}
+
+fs.writeFile('../../angular.json', JSON.stringify(angular, null, 2), function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('JSON saved to angular.json');
+  }
+});
