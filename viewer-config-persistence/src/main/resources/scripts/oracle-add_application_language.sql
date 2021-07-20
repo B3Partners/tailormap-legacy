63269518c2625d20f18a01fbdd125346e2fc991f
Mark Prins <mark@b3partners.nl>
2021-07-20 17:01:39 +0200

    [TM-182] Remove any Oracle upgrade procedures from the persistence module

--- viewer-config-persistence/src/main/resources/scripts/oracle-add_application_language.sql
+++
@@ -1,2 +0,0 @@
-alter table application
-  add lang varchar2(5 char);
--- viewer-config-persistence/src/main/resources/scripts/oracle-remove_selectedIndexChecked.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE application_layer
-  DROP COLUMN selected_index;
-ALTER TABLE application_layer
-  DROP COLUMN checked;
-
-  ALTER TABLE level_
-  DROP COLUMN selected_index;
-
