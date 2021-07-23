c8a2b6ef24e6bd40366cbd18d569018eb5144145
Meine Toonen <toonen.meine@gmail.com>
2021-07-23 13:40:26 +0200

    load correct deps and show editor

--- tailormap-admin-config/src/app/app.module.ts
+++ tailormap-admin-config/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormEditorModule, SharedModule} from "@tailormap/admin-config";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {DragDropModule} from "@angular/cdk/drag-drop";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    FormEditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
