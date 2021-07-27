022ae7ffca0fc1fa0861043f290f7cbae528a790
Mark Prins <mark@b3partners.nl>
2021-07-27 10:55:04 +0200

    [TM-252] add upgrade script to remove Layar tables from database

---
+++ viewer-config-persistence/src/main/resources/scripts/hsqldb-drop_layar.sql
@@ -0,0 +1,7 @@
+-- try to drop Layar tables from existing database
+-- ALTER TABLE layar_source_details DROP CONSTRAINT FKNJX9DMYCR5RVBHCQEV63S9PHQ;
+-- ALTER TABLE layar_source DROP CONSTRAINT FK59GAWRWEQPPCQIP1K4NP9ATTD;
+-- ALTER TABLE layar_source DROP CONSTRAINT FKDJIJAAVTOB7KT165QFO42WA5X;
+DROP TABLE layar_source_details;
+DROP TABLE layar_source;
+DROP TABLE layar_service;
