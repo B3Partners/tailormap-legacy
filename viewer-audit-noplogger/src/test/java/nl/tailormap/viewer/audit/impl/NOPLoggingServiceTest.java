85925d7c29d6a087224db9ac550e69367d34c325
Mark Prins <mark@b3partners.nl>
2021-08-02 13:52:44 +0200

    TM-259 migrate viewer-audit-noplogger module to junit 5

--- viewer-audit-noplogger/src/test/java/nl/tailormap/viewer/audit/impl/NOPLoggingServiceTest.java
+++ viewer-audit-noplogger/src/test/java/nl/tailormap/viewer/audit/impl/NOPLoggingServiceTest.java
@@ -1,12 +1,12 @@
 package nl.tailormap.viewer.audit.impl;
 
 import nl.tailormap.viewer.audit.LoggingServiceFactory;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class NOPLoggingServiceTest {
     @Test
     public void testInstance() {
-        Assert.assertEquals("We should have gotten the NOP logging implementation", NOPLoggingService.class, LoggingServiceFactory.getInstance().getClass());
+        Assertions.assertEquals(NOPLoggingService.class, LoggingServiceFactory.getInstance().getClass(), "We should have gotten the NOP logging implementation");
     }
 }
