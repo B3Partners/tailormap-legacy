fc54420ed0bf60415308d0f317ff5cc2934d6313
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-10-01 16:11:13 +0200

    Prefix hyperlinks with // in case it does not start with http

--- tailormap-components/projects/core/src/lib/user-interface/generic-components/label-field/label-field.component.spec.ts
+++ tailormap-components/projects/core/src/lib/user-interface/generic-components/label-field/label-field.component.spec.ts
@@ -1,7 +1,8 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
 
 import { LabelFieldComponent } from './label-field.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { FormFieldType } from '../../../feature-form/form/form-models';
 
 describe('LabelFieldComponent', () => {
   let spectator: Spectator<LabelFieldComponent>;
@@ -12,7 +13,33 @@ describe('LabelFieldComponent', () => {
 
   it('should create', () => {
     spectator = createComponent();
-
     expect(spectator.component).toBeTruthy();
   });
+
+  it('renders hyperlink', () => {
+    spectator = createComponent({
+      props: {
+        fieldType: FormFieldType.HYPERLINK,
+        value: 'https://test.nl',
+      },
+    });
+    expect(spectator.component).toBeTruthy();
+    const href = spectator.query('a');
+    expect(href).toBeTruthy();
+    expect(href.getAttribute('href')).toEqual('https://test.nl');
+  });
+
+  it('renders hyperlink without http', () => {
+    spectator = createComponent({
+      props: {
+        fieldType: FormFieldType.HYPERLINK,
+        value: 'test.nl',
+      },
+    });
+    expect(spectator.component).toBeTruthy();
+    const href = spectator.query('a');
+    expect(href).toBeTruthy();
+    expect(href.getAttribute('href')).toEqual('//test.nl');
+  });
+
 });
