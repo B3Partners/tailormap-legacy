b29631181dbf5283ffd7bdcbf1b0a18442757826
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-10-15 16:03:15 +0200

    GBIM-194: Fixes creating new object with datefield

--- tailormap-components/projects/core/src/lib/user-interface/generic-components/datepicker-field/datepicker-field.component.ts
+++ tailormap-components/projects/core/src/lib/user-interface/generic-components/datepicker-field/datepicker-field.component.ts
@@ -1,40 +1,52 @@
-import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
-import { BaseFieldComponent } from '../base-field/base-field.component';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import * as momentNs from 'moment';
 import { FeatureAttribute } from '../../../feature-form/form/form-models';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'tailormap-datepicker-field',
   templateUrl: './datepicker-field.component.html',
   styleUrls: ['./datepicker-field.component.css'],
 })
-export class DatepickerFieldComponent extends BaseFieldComponent implements OnInit {
+export class DatepickerFieldComponent implements OnInit {
+
+  @Input()
+  public groep: FormGroup;
 
   @Input()
   public attribute: FeatureAttribute;
 
-  @Output()
-  public dateChange: EventEmitter< MatDatepickerInputEvent< any>>;
-
-  constructor(@Inject(MAT_DATE_FORMATS) private dateFormats) {super(); }
+  constructor(
+    @Inject(MAT_DATE_FORMATS) private dateFormats,
+  ) {}
 
   public ngOnInit(): void {
-    this.dateFormats.display.dateInput = this.attribute.dateFormat;
+    this.dateFormats.display.dateInput = this.getDateFormat();
   }
 
   public getDate(): Date {
+    if (!this.attribute.value) {
+      return null;
+    }
     return new Date(this.attribute.value.toString());
   }
 
-  public changeDate(date: any) {
-    this.attribute.value = new Date(date.value._d).toString();
-    this.groep.get(this.attribute.key).setValue(new Date(date.value._d), {
+  public changeDate(date: MatDatepickerInputEvent<momentNs.Moment>) {
+    this.attribute.value = date.value.toDate().toString();
+    const formattedDate = date.value.format(this.getDateFormat());
+    this.groep.get(this.attribute.key).setValue(formattedDate, {
       emitEvent: true,
       onlySelf: false,
       emitModelToViewChange: true,
       emitViewToModelChange: true,
     });
+    this.groep.get(this.attribute.key).markAsDirty({ onlySelf: true });
+  }
+
+  private getDateFormat() {
+    return this.attribute.dateFormat || 'YYYY-MM-DD';
   }
 
 }
