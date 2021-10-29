8bd3fa0c2610aa022e47888d7b74708ea83b6fad
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-10-29 12:13:46 +0200

    Fix lint

--- tailormap-components/projects/core/src/lib/shared/export-service/export.service.ts
+++ tailormap-components/projects/core/src/lib/shared/export-service/export.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {
   HttpClient,
-  HttpParams, HttpResponse,
+  HttpResponse,
 } from '@angular/common/http';
 import {
   ExportFeaturesParameters,
