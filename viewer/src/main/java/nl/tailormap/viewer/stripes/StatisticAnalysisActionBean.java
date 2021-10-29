f65306cb1e04604f310ad3d7e1dd0320c6b575a3
mvdstruijk <martijnvdstruijk@b3partners.nl>
2021-10-29 12:40:49 +0200

    use sql filtering for getting analysis when store is a JDBCstore

--- viewer/src/main/java/nl/tailormap/viewer/stripes/StatisticAnalysisActionBean.java
+++ viewer/src/main/java/nl/tailormap/viewer/stripes/StatisticAnalysisActionBean.java
@@ -18,15 +18,20 @@ import nl.tailormap.viewer.config.app.ApplicationLayer;
 import nl.tailormap.viewer.config.services.Layer;
 import nl.tailormap.viewer.config.services.SimpleFeatureType;
 import nl.tailormap.viewer.helpers.featuresources.FeatureSourceFactoryHelper;
+import nl.tailormap.viewer.userlayer.TMFilterToSQL;
 import nl.tailormap.viewer.util.ChangeMatchCase;
 import nl.tailormap.viewer.util.FilterHelper;
 import nl.tailormap.viewer.util.TailormapCQL;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.geotools.data.DefaultTransaction;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.Query;
+import org.geotools.data.jdbc.FilterToSQLException;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.text.cql2.CQLException;
+import org.geotools.jdbc.JDBCDataStore;
 import org.json.JSONObject;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
@@ -34,7 +39,11 @@ import org.opengis.filter.expression.Function;
 import org.stripesstuff.stripersist.Stripersist;
 
 import javax.persistence.EntityManager;
+import java.io.IOException;
 import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.MessageFormat;
 
 @UrlBinding("/action/statisticanalysis")
@@ -175,24 +184,40 @@ public class StatisticAnalysisActionBean extends LocalizableApplicationActionBea
 
                 fs = FeatureSourceFactoryHelper.openGeoToolsFeatureSource(ft);
 
+                if(fs.getDataStore() instanceof JDBCDataStore) {
+                    log.debug("trying to get Analysis with SQL for JDBCDatastore");
+                    json.put("type", "sql");
+                    EntityManager em = Stripersist.getEntityManager();
+                    JDBCDataStore da = (JDBCDataStore) fs.getDataStore();
 
-                final Query q = new Query(fs.getName().toString());
-              //  q.setMaxFeatures(getMaxFeatures());
+                    String whereStatement = "";
+                    if(this.filter != null) {
+                        whereStatement = this.getSQLQuery(da, ft.getTypeName(), em);
+                    }
 
-                setFilter(filter, q, ft, Stripersist.getEntityManager());
+                    json.put("result", getValueWithSQL(da, ft, whereStatement));
+                    json.put("success", true);
+                    this.auditMessageObject.addMessage(whereStatement);
+                } else {
+
+                    final Query q = new Query(fs.getName().toString());
+                    //  q.setMaxFeatures(getMaxFeatures());
+
+                    setFilter(filter, q, ft, Stripersist.getEntityManager());
 
 
-                SimpleFeatureCollection fc =(SimpleFeatureCollection) fs.getFeatures(q);
+                    SimpleFeatureCollection fc = (SimpleFeatureCollection) fs.getFeatures(q);
 
-                FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
-                Function unique = ff.function(analysis.geotoolsFunction, ff.property(column));
+                    FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
+                    Function unique = ff.function(analysis.geotoolsFunction, ff.property(column));
 
-                Object value = unique.evaluate( fc );
-                json.put("result", value);
-                json.put("success", true);
+                    Object value = unique.evaluate(fc);
+                    json.put("result", value);
+                    json.put("success", true);
+                    this.auditMessageObject.addMessage(q);
+                }
 
                 this.auditMessageObject.addMessage(ft);
-                this.auditMessageObject.addMessage(q);
                 this.auditMessageObject.addMessage(fs);
             }
         } catch (Exception e) {
@@ -242,5 +267,55 @@ public class StatisticAnalysisActionBean extends LocalizableApplicationActionBea
         AnalysisType(String geotoolsFunction) {
             this.geotoolsFunction = geotoolsFunction;
         }
+    }
+
+    private String getSQLQuery(JDBCDataStore dataStore, String tableName, EntityManager em) throws CQLException, FilterToSQLException, IOException {
+        TMFilterToSQL f = new TMFilterToSQL(dataStore, tableName);
+        f.createFilterCapabilities();
+        return f.encodeToString(TailormapCQL.toFilter(this.filter, em, false));
+    }
+
+    private double getValueWithSQL(JDBCDataStore da, SimpleFeatureType ft, String whereStatement) throws SQLException {
+        double value = 0;
+        Connection con = null;
+
+        try {
+            con = da.getConnection(new DefaultTransaction("analysis"));
+            String sql  = buildAnalysisQuery(ft.getTypeName(), whereStatement);
+            ResultSet rs = con.prepareStatement(sql).executeQuery();
+            rs.next();
+            value = rs.getInt(1);
+        } catch (IOException | SQLException e) {
+            log.error("Can't get feature Analysis with sql for JDBCDatastore: " + e.getMessage());
+        } finally {
+            if (con != null) {
+                con.close();;
+            }
+        }
+        return value;
+    }
+
+    private String buildAnalysisQuery(String tableName, String whereStatement) {
+        String sql = "SELECT ";
+        switch (type) {
+            case "SUM":
+                sql += "SUM (" + column + ") FROM " + tableName + " " + whereStatement;
+                break;
+            case "MIN":
+                sql += "MIN (" + column + ") FROM " + tableName + " " + whereStatement;
+                break;
+            case "MAX":
+                sql += "MAX (" + column + ") FROM " + tableName + " " + whereStatement;
+                break;
+            case "COUNT":
+                sql += "COUNT (" + column + ") FROM " + tableName + " " + whereStatement;
+                break;
+            case "AVERAGE":
+                sql += "AVG (" + column + ") FROM " + tableName + " " + whereStatement;
+                break;
+            default:
+                sql = "";
+        }
+        return sql;
     }
 }
