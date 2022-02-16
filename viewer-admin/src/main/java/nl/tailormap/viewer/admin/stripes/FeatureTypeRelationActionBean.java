d9d9dfe81976ab9ec8b868b1f0e75fa49d5aacd9
mvdstruijk <m_struijk1990@hotmail.nl>
2022-02-16 11:02:55 +0100

    set boolean for searching for more relations

--- viewer-admin/src/main/java/nl/tailormap/viewer/admin/stripes/FeatureTypeRelationActionBean.java
+++ viewer-admin/src/main/java/nl/tailormap/viewer/admin/stripes/FeatureTypeRelationActionBean.java
@@ -94,7 +94,8 @@ public class FeatureTypeRelationActionBean extends LocalizableActionBean {
     @ValidateNestedProperties({
         @Validate(field="featureType", required=true, on="save"),
         @Validate(field="foreignFeatureType", required=true, on="save"),
-        @Validate(field="type", required=true, on="save")
+        @Validate(field="type", required=true, on="save"),
+        @Validate(field="searchNextRelation", on="save")
     })
     private FeatureTypeRelation relation;
         
