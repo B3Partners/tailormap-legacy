34cec4465bebb0afb25ebfc075e7e176db41f03a
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-05-06 05:40:01 +0200

    Fix layercount (#3233)
    (cherry picked from commit 34e031f8259f6d23829954b4d1728ebe93d9b0c9)

--- viewer-config-persistence/src/test/java/nl/b3p/viewer/config/services/TileServiceTest.java
+++ viewer-config-persistence/src/test/java/nl/b3p/viewer/config/services/TileServiceTest.java
@@ -48,7 +48,7 @@ public class TileServiceTest extends TestUtil{
     private TileService instance = new TileService();
 
     private static final String PDOK_WMTS = "http://geodata.nationaalgeoregister.nl/tiles/service/wmts?request=getcapabilities";
-    private static final int PDOK_WMTS_LAYERCOUNT = 37;
+    private static final int PDOK_WMTS_LAYERCOUNT = 32;
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     DocumentBuilder builder;
     XPathFactory xPathfactory = XPathFactory.newInstance();
